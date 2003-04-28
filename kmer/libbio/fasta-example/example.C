7ec69dca09e3f4089cdd1df84edd738dcfbe1801
Brian P. Walenz <thebri@gmail.com>
2003-04-28 18:20:33 +0000

    Example application that uses the fasta-reader

---
+++ kmer/libbio/fasta-example/example.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fasta.H"
+
+
+int
+main(int argc, char **argv) {
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <query-filename>\n", argv[0]);
+    exit(1);
+  }
+
+  FastAWrapper  seqs(argv[1]);
+
+  while (!seqs.eof()) {
+    FastASequenceInCore *aSeq = seqs.getSequence();
+
+    fprintf(stderr, "%8d  '%s'\n", aSeq->getIID(), aSeq->header());
+
+    delete aSeq;
+  }
+
+  fprintf(stderr, "\n");
+
+  seqs.openIndex();
+  for (int id=seqs.getNumberOfSequences()-1; id>=0; id--) {
+    seqs.find(id);
+
+    FastASequenceInCore *aSeq = seqs.getSequence();
+
+    fprintf(stderr, "%8d  '%s'\n", aSeq->getIID(), aSeq->header());
+
+    delete aSeq;
+  }
+}
+
