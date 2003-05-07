e1c8cb492324bd151800cb2dccd1eeef6fd75499
Brian P. Walenz <thebri@gmail.com>
2003-05-07 17:42:48 +0000

    These used to be automagically generated during make.  Figuring out dependencies (and creating alphabet.h if it didn't exist) was trouble. Now we just include both the generator and the generated.

---
+++ kmer/libbio/alphabet.h
@@ -0,0 +1,17 @@
+//
+//  Automagically generated -- DO NOT EDIT!
+//  See alphabet-generate.c for details.
+//
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern unsigned char const   compressSymbol[256];
+extern const unsigned char   validSymbol[256];
+extern const unsigned char   decompressSymbol[256];
+extern const unsigned char   complementSymbol[256];
+extern const unsigned char   validCompressedSymbol[256];
+#ifdef __cplusplus
+}
+#endif
