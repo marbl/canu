3180c26810b4390ee25dd68b8ddda9e0f629cea8
Brian P. Walenz <thebri@gmail.com>
2003-05-15 18:44:06 +0000

    Added support for checking command line arguments.  Should report invalid options, and missing parameters for known options (e.g., giving two values when three are needed).

--- kmer/scripts/ESTmapper.pl
+++ kmer/scripts/ESTmapper.pl
@@ -77,8 +77,75 @@ use strict;
 #
 
 
-my $scriptVersion = "6";           # analogous to a buildNumber
-my $scriptDate    = "03dec02";
+
+
+#  Global arg list.  New command line options MUST be added here, but
+#  there is no mechanism to enforce this rule.
+#
+#  The value of the hash is the number of extra args needed
+#
+my %validArgs = ("-help"            => "0",
+                 "-restart"         => "1",
+                 "-configure"       => "1",
+                 "-searchest"       => "1",
+                 "-searchmrna"      => "1",
+                 "-filterest"       => "1",
+                 "-filtermrna"      => "1",
+                 "-filternone"      => "1",
+                 "-polish"          => "1",
+                 "-assembleoutput"  => "1",
+                 "-mapest"          => "3",
+                 "-mapest-nofilter" => "3",
+                 "-mapsnp"          => "3",
+                 "-mapesttoest"     => "3",
+                 "-mapmrna"         => "3",
+
+                 "-snpdelimiter"    => "1",
+                 "-snpoffset"       => "1",
+                 "-snppostag"       => "1",
+                 "-snpsizetag"      => "1",
+                 "-abort"           => "1",
+                 "-aligns"          => "0",
+                 "-alwaysprint"     => "1",
+                 "-batchsize"       => "1",
+                 "-cdna"            => "1",
+                 "-cleanup"         => "0",
+                 "-farmpolishes"    => "2",
+                 "-farmsearches"    => "2",
+                 "-genomic"         => "1",
+                 "-hitsortmemory"   => "1",
+                 "-localpolishes"   => "1",
+                 "-localsearches"   => "1",
+                 "-longintron"      => "1",
+                 "-maskmers"        => "1",
+                 "-maxintron"       => "1",
+                 "-memory"          => "1",
+                 "-mersize"         => "1",
+                 "-mincoverage"     => "1",
+                 "-minidentity"     => "1",
+                 "-minlength"       => "1",
+                 "-minsim4coverage" => "1",
+                 "-minsim4identity" => "1",
+                 "-minsim4length"   => "1",
+                 "-noaligns"        => "0",
+                 "-nocleanup"       => "0",
+                 "-nomaskmers"      => "0",
+                 "-nostats"         => "0",
+                 "-numbatches"      => "1",
+                 "-relink"          => "1",
+                 "-runlater"        => "0",
+                 "-searchthreads"   => "1",
+                 "-segments"        => "1",
+                 "-species"         => "1",
+                 "-stats"           => "0",
+                 "-verbose"         => "0" );
+
+
+
+
+
+my $scriptVersion = "7";           # analogous to a buildNumber
+my $scriptDate    = "14may03";
 
 #  We use the libBri package of usefull stuff.  It's located in the same place
 #  as the ESTmapper.pl script.  That's what FindBin tells us.
@@ -98,7 +165,7 @@ while ($exechome =~ m!^/member\w+(/.*)$!) {
     $exechome = $1;
 }
 
-print STDERR "Using exechome of '$exechome'\n";
+#print STDERR "Using exechome of '$exechome'\n";
 
 my $searchGENOME  = "$exechome/searchGENOME";
 my $mergeCounts   = "$exechome/mergeCounts";
@@ -122,6 +189,31 @@ die "Can't find/execute $cleanPolishes\n" if (! -x $cleanPolishes);
 die "Can't find/execute $sortHits\n"      if (! -x $sortHits);
 die "Can't find/execute $sortPolishes\n"  if (! -x $sortPolishes);
 die "Can't find/execute $parseSNPs\n"     if (! -x $parseSNPs);
+
+sub checkArgs {
+    my $fail = 0;
+
+    while (scalar(@_)) {
+        my $a = shift @_;
+        my $n = $validArgs{$a};
+        if (defined($n)) {
+            while ($n > 0) {
+                my $b = shift @_;
+                if (!defined($b) || defined($validArgs{$b})) {
+                    print "ERROR: Too few parameters given to option '$a'\n";
+                    $fail = 1;
+                    last;
+                }
+                $n--;
+            }
+        } else {
+            print "ERROR: Invalid option '$a' supplied.\n";
+            $fail = 1;
+        }
+    }
+
+    exit(1) if ($fail);
+}
 
 sub showHelp {
     print STDERR "Basic help:\n";
@@ -241,6 +333,7 @@ if ($personality eq "-restart") {
 
 my $runInformationFile;
 
+checkArgs(@ARGV);
 
 if ($personality eq "-mapest") {
     shift @ARGV;
