48212fdb98dc8fd7c9e599f5b677331903b9f400
Brian P. Walenz <thebri@gmail.com>
2003-05-19 15:50:41 +0000

    Isolated a memory issue with loading sequences -- the default size of sequences (not squeezed, not fixedwidth) was 16MB.  We tried to load 16k of them, resulting in 256GB of allocations.  The default size has been changed (in fasta reader, not here).  The try/catch was narrowed here.  XXX:  We should do more intelligent handling of failure to load sequences.

--- kmer/seagen/thr-loader.C
+++ kmer/seagen/thr-loader.C
@@ -23,52 +23,51 @@ char const *loadDesc = "WARNING: Loader is at %3.0f%% capacity. (cDNA %7lu out o
 
 void*
 loaderThread(void *) {
+  u32bit               LOADER_HIGH_WATER_MARK = 16384;
+  u32bit               waterLevel             = 0;
+  FastASequenceInCore *B;
 
-  try {
-    u32bit               LOADER_HIGH_WATER_MARK = 16384;
-    u32bit               waterLevel             = 0;
-    FastASequenceInCore *B;
+  struct timespec loaderSleep = { 0, 25000000 };
 
-    struct timespec loaderSleep = { 0, 25000000 };
+  while (inputHead < numberOfQueries) {
 
-    while (inputHead < numberOfQueries) {
+    //  We fill the input as fast as we can, up to the high water
+    //  mark, then we take a little snooze to let the workers catch up.
+    //
+    pthread_mutex_lock(&inputTailMutex);
+    waterLevel = inputHead - inputTail;
+    pthread_mutex_unlock(&inputTailMutex);
 
-      //  We fill the input as fast as we can, up to the high water
-      //  mark, then we take a little snooze to let the workers catch up.
-      //
-      pthread_mutex_lock(&inputTailMutex);
-      waterLevel = inputHead - inputTail;
-      pthread_mutex_unlock(&inputTailMutex);
-
-      //  Now, sleep, if we need to.
-      //
-      if (waterLevel >= LOADER_HIGH_WATER_MARK) {
-        nanosleep(&loaderSleep, 0L);
+    //  Now, sleep, if we need to.
+    //
+    if (waterLevel >= LOADER_HIGH_WATER_MARK) {
+      nanosleep(&loaderSleep, 0L);
 
 #ifdef WATERLEVELWARNINGS
-        //  Warn if we're too small.
-        //
-        if (waterLevel < (LOADER_HIGH_WATER_MARK >> 1))
-          fprintf(stderr, loadDesc,
-                  100.0 * waterLevel / LOADER_HIGH_WATER_MARK,
-                  inputHead, numberOfQueries);
+      //  Warn if we're too small.
+      //
+      if (waterLevel < (LOADER_HIGH_WATER_MARK >> 1))
+        fprintf(stderr, loadDesc,
+                100.0 * waterLevel / LOADER_HIGH_WATER_MARK,
+                inputHead, numberOfQueries);
 #endif
-      } else {
+    } else {
 
-        //  Get the next cDNA and push it onto the input list at
-        //  inputHead.  This alloc is deleted by the output thread.
-        //
+      //  Get the next cDNA and push it onto the input list at
+      //  inputHead.  This alloc is deleted by the output thread.
+      //
+      try {
         B = qsFASTA->getSequence();
-
-        pthread_mutex_lock(&inputTailMutex);
-        input[inputHead] = B;
-        inputHead++;
-        pthread_mutex_unlock(&inputTailMutex);
+      } catch (std::bad_alloc) {
+        fprintf(stderr, "loaderThread()-- Failed to load next query sequence\ncaught bad_alloc in %s at line %d\n", __FILE__, __LINE__);
+        exit(1);
       }
+
+      pthread_mutex_lock(&inputTailMutex);
+      input[inputHead] = B;
+      inputHead++;
+      pthread_mutex_unlock(&inputTailMutex);
     }
-  } catch (std::bad_alloc) {
-    fprintf(stderr, "loaderThread()-- caught bad_alloc in %s at line %d\n", __FILE__, __LINE__);
-    exit(1);
   }
 
   return(0L);
