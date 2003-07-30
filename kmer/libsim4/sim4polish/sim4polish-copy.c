edf64f2e6fc38ffa8a2de78aad50332bcb01cffb
Brian P. Walenz <thebri@gmail.com>
2003-07-30 00:15:29 +0000

    Check that deflines and alignments exist before copying them.

--- kmer/libsim4/sim4polish/sim4polish-copy.c
+++ kmer/libsim4/sim4polish/sim4polish-copy.c
@@ -11,14 +11,20 @@ s4p_copyPolish(sim4polish *orig) {
 
   copy = (sim4polish *)memdup(orig, sizeof(sim4polish));
 
-  copy->estDefLine = (char *)memdup(orig->estDefLine, sizeof(char) * (strlen(orig->estDefLine) + 1));
-  copy->genDefLine = (char *)memdup(orig->genDefLine, sizeof(char) * (strlen(orig->genDefLine) + 1));
+  if (orig->estDefLine)
+    copy->estDefLine = (char *)memdup(orig->estDefLine, sizeof(char) * (strlen(orig->estDefLine) + 1));
+
+  if (orig->genDefLine)
+    copy->genDefLine = (char *)memdup(orig->genDefLine, sizeof(char) * (strlen(orig->genDefLine) + 1));
 
   copy->exons = (sim4polishExon *)memdup(orig->exons, sizeof(sim4polishExon) * copy->numExons);
 
   for (i=0; i<copy->numExons; i++) {
-    copy->exons[i].estAlignment = (char *)memdup(orig->exons[i].estAlignment, sizeof(char) * (strlen(copy->exons[i].estAlignment) + 1));
-    copy->exons[i].genAlignment = (char *)memdup(orig->exons[i].genAlignment, sizeof(char) * (strlen(copy->exons[i].genAlignment) + 1));
+    if (orig->exons[i].estAlignment)
+      copy->exons[i].estAlignment = (char *)memdup(orig->exons[i].estAlignment, sizeof(char) * (strlen(copy->exons[i].estAlignment) + 1));
+
+    if (orig->exons[i].genAlignment)
+      copy->exons[i].genAlignment = (char *)memdup(orig->exons[i].genAlignment, sizeof(char) * (strlen(copy->exons[i].genAlignment) + 1));
   }
 
   return(copy);
