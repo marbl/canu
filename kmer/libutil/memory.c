5962c892b0b11005c96af4c774ac8059ca7ee7f9
Brian P. Walenz <thebri@gmail.com>
2003-08-07 16:02:51 +0000

    64bit/32bit printing changes

--- kmer/libutil/memory.c
+++ kmer/libutil/memory.c
@@ -1,4 +1,5 @@
 #include "memory.h"
+#include "libbritypes.h"
 
 #include <errno.h>
 #include <string.h>
@@ -11,7 +12,11 @@ memdup(const void *orig, size_t size) {
     errno = 0;
     rslt = malloc(size);
     if (errno) {
+#ifdef TRUE64BIT
+      fprintf(stderr, "memdup()-- can't allocate %lu bytes.\n%s\n", size, strerror(errno));
+#else
       fprintf(stderr, "memdup()-- can't allocate %d bytes.\n%s\n", size, strerror(errno));
+#endif
       exit(1);
     }
     memcpy(rslt, orig, size);
