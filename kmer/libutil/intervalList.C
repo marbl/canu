c2a6bde9ab6cdbd3f0081d8c222b29be8231216b
Brian P. Walenz <thebri@gmail.com>
2003-08-11 21:49:27 +0000

    Initial implementation of an interval list.  This stores an unordered set of intervals, with operations of sort() and merge().  It doesn't allow for much more than access by iterating the set of intervals.

---
+++ kmer/libutil/intervalList.C
@@ -0,0 +1,149 @@
+#include "intervalList.H"
+
+
+
+//  Define this to print some debugging information
+//
+//#define DEBUG_LIST
+
+
+intervalList::intervalList() {
+  _isSorted = false;
+  _listLen  = 0;
+  _listMax  = 16;
+  _list     = new _intervalPair [_listMax];
+}
+
+
+intervalList::~intervalList() {
+  delete [] _list;
+}
+
+
+void
+intervalList::add(u32bit position, u32bit length) {
+
+#ifdef DEBUG_LIST
+  fprintf(stderr, "Adding %u - %u\n", position, position+length);
+#endif
+
+  if (_listLen >= _listMax) {
+    _listMax *= 2;
+    _intervalPair *l = new _intervalPair [_listMax];
+    memcpy(l, _list, sizeof(_intervalPair) * _listLen);
+    delete [] _list;
+    _list = l;
+  }
+
+  _list[_listLen].lo = position;
+  _list[_listLen].hi = position + length;
+
+  if ((_listLen > 0) &&
+      (_list[_listLen-1].lo > _list[_listLen].lo)) {
+#ifdef DEBUG_LIST
+    fprintf(stderr, "list isn't sorted\n");
+#endif
+    _isSorted = false;
+  }
+
+  _listLen++;
+}
+
+
+static
+int
+intervalList_sort_helper(const void *a, const void *b) {
+  _intervalPair *A = (_intervalPair *)a;
+  _intervalPair *B = (_intervalPair *)b;
+
+  if (A->lo < B->lo)
+    return(-1);
+  if (A->lo > B->lo)
+    return(1);
+  return(0);
+}
+
+
+void
+intervalList::sort(void) {
+
+  if (_isSorted) {
+#ifdef DEBUG_LIST
+    fprintf(stderr, "List is already sorted!\n");
+#endif
+    return;
+  }
+
+  qsort(_list, _listLen, sizeof(_intervalPair), intervalList_sort_helper);
+
+  _isSorted = true;
+}
+
+
+void
+intervalList::merge(void) {
+  u32bit  targetInterval  = 0;
+  u32bit  examineInterval = 1;
+
+  if (_listLen < 2)
+    return;
+
+  sort();
+
+  while (examineInterval < _listLen) {
+
+    if ((_list[targetInterval].lo == 0) &&
+        (_list[targetInterval].hi == 0)) {
+
+      //  Our target interval is empty.  Copy in the interval we are
+      //  examining and move to the next.
+
+      _list[targetInterval].lo = _list[examineInterval].lo;
+      _list[targetInterval].hi = _list[examineInterval].hi;
+      examineInterval++;
+    } else {
+
+      //  We have a valid targetInterval.  See if it overlaps
+      //  with the interval we are examining.
+
+      if (_list[examineInterval].lo <= _list[targetInterval].hi) {
+
+        //  Got an intersection.
+
+        //  Merge examineInterval into targetInterval -- the hi range
+        //  is extended if the examineInterval range is larger.
+        //
+        if (_list[targetInterval].hi < _list[examineInterval].hi)
+          _list[targetInterval].hi = _list[examineInterval].hi;
+        
+        //  Clear the just merged examineInterval
+        //
+        _list[examineInterval].lo = 0;
+        _list[examineInterval].hi = 0;
+
+        //  Move to the next interval to examine.
+        //
+        examineInterval++;
+      } else {
+
+        //  No intersection.  Move along.  Nothing to see here.
+
+        //  If there is a gap between the target and the examine (we already
+        //  merged sometime in the past), copy examine to the next target.
+
+        targetInterval++;
+
+        if (targetInterval != examineInterval) {
+          _list[targetInterval].lo = _list[examineInterval].lo;
+          _list[targetInterval].hi = _list[examineInterval].hi;
+        }
+
+        examineInterval++;
+      }
+    }
+  }
+
+  if (targetInterval+1 < _listLen)
+    _listLen = targetInterval + 1;
+}
+
