ccb64f5d419f3fec944b90354016debff6492797
Brian P. Walenz <thebri@gmail.com>
2003-09-09 13:25:02 +0000

    Updated to test fibonacci encoding.

--- kmer/libutil/test/testpackedfile.C
+++ kmer/libutil/test/testpackedfile.C
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <math.h>
+#include "bitPackedFile.H"
 #include "bit-packing.H"
 
-#define TESTSIZE 4929482
+
+//  Two tests are defined (OK, three): one that tests fixed-width bit
+//  packing, one that tests the fibonacci numbers, and one that tests
+//  both.
+//
+#define TESTSIZE         4929482
+#define TEST_BITS
+#define TEST_NUMBERS
 
 void
 main(void) {
@@ -21,28 +29,44 @@ main(void) {
   }
 
   bitPackedFileWriter *W = new bitPackedFileWriter("bittest.junk");
-  for (i=0; i<TESTSIZE; i++)
+  for (i=0; i<TESTSIZE; i++) {
+#ifdef TEST_BITS
     W->putBits(val[i], siz[i]);
+#endif
+
+#ifdef TEST_NUMBERS
+    W->putNumber(val[i]);
+#endif
+  }
   delete W;
 
   bitPackedFileReader *R = new bitPackedFileReader("bittest.junk");
   for (i=0; i<TESTSIZE; i++) {
+#ifdef TEST_BITS
     u64bit r = R->getBits(siz[i]);
-
     if (r != val[i]) {
-      fprintf(stderr, "%6d] ERROR -- 0x%016lx != 0x%016lx (%2d).\n", i, r, val[i], siz[i]);
+      fprintf(stderr, "%6d] ERROR in getBits()   -- retrieved 0x%016lx != expected 0x%016lx (%2d bits).\n", i, r, val[i], siz[i]);
       errs++;
+#if 0
+    } else {
+      fprintf(stderr, "%6d]       in getBits()   -- retrieved 0x%016lx != expected 0x%016lx (%2d bits).\n", i, r, val[i], siz[i]);
+#endif
     }
+#endif
+
+#ifdef TEST_NUMBERS
+    u64bit v = R->getNumber();
+    if (v != val[i]) {
+      fprintf(stderr, "%6d] ERROR in getNumber() -- retrieved 0x%016lx != expected 0x%016lx.\n", i, v, val[i]);
+      errs++;
+#if 0
+    } else {
+      fprintf(stderr, "%6d]       in getNumber() -- retrieved 0x%016lx != expected 0x%016lx.\n", i, v, val[i]);
+#endif
+    }
+#endif
   }
   delete R;
 
   fprintf(stderr, "There are %u errors.\n", errs);
-  for (u32bit i=0; i<=64; i++) {
-    u32bit c=0;
-    for (u32bit j=0; j<TESTSIZE; j++) {
-      if (siz[j] == i)
-        c++;
-    }
-    fprintf(stderr, "%8d at size = %d\n", c, i);
-  }
 }
