84a6e0c20ccb7fc0eb03f5c0908a008d86aeca14
Brian P. Walenz <thebri@gmail.com>
2004-02-20 20:48:32 +0000

    Added incremental md5 computation.

--- kmer/libutil/md5.c
+++ kmer/libutil/md5.c
@@ -73,6 +73,105 @@ md5_string(md5_s *m, char *s, u32bit l) {
 
 
 
+static
+md5_increment_s*
+md5_increment_initialize(void) {
+  md5_increment_s *m;
+
+  errno = 0;
+  m = (md5_increment_s *)malloc(sizeof(md5_increment_s));
+  if (errno) {
+    fprintf(stderr, "md5_increment_*()-- Can't allocate a md5_increment_s.\n%s\n", strerror(errno));
+    exit(1);
+  }
+
+  m->context = (MD5_CTX *)malloc(sizeof(MD5_CTX));
+  if (errno) {
+    fprintf(stderr, "md5_increment_*()-- Can't allocate a md5 context.\n%s\n", strerror(errno));
+    exit(1);
+  }
+  MD5Init((MD5_CTX *)m->context);
+
+  m->bufferPos = 0;
+
+  return(m);
+}
+
+
+md5_increment_s*
+md5_increment_char(md5_increment_s *m, char s) {
+
+  if (m == NULL)
+    m = md5_increment_initialize();
+
+  m->buffer[m->bufferPos++] = s;
+
+  if (m->bufferPos == MD5_BUFFER_SIZE) {
+    MD5Update((MD5_CTX *)m->context, m->buffer, m->bufferPos);
+    m->bufferPos = 0;
+  }
+
+  return(m);
+}
+
+
+md5_increment_s*
+md5_increment_block(md5_increment_s *m, char *s, u32bit l) {
+
+  if (m == NULL)
+    m = md5_increment_initialize();
+
+  MD5Update((MD5_CTX *)m->context, (unsigned char*)s, l);
+
+  return(m);
+}
+
+
+void
+md5_increment_finalize(md5_increment_s *m) {
+  MD5_CTX        *ctx = (MD5_CTX *)m->context;
+  unsigned char   dig[16];
+  int             i = 0;
+
+  if (m->bufferPos > 0) {
+    MD5Update((MD5_CTX *)m->context, m->buffer, m->bufferPos);
+    m->bufferPos = 0;
+  }
+
+  MD5Final(dig, ctx);
+
+  m->a = dig[0];
+  while (i<8) {
+    m->a <<= 8;
+    m->a |= dig[i++];
+  }
+
+  m->b  = dig[i++];
+  while (i<16) {
+    m->b <<= 8;
+    m->b |= dig[i++];
+  }
+
+  m->context = 0L;
+
+  free(ctx);
+}
+
+void
+md5_increment_destroy(md5_increment_s *m) {
+  free(m);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 #ifdef MAIN
 
