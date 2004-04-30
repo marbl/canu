2e39503a56e6df08389048e14000330d2b8ae3a1
Brian P. Walenz <thebri@gmail.com>
2004-04-30 16:58:25 +0000

    Enable writing the magic number after writing the data.  Fixes to printf() formatting.

--- kmer/libkmer/positionDB-file.C
+++ kmer/libkmer/positionDB-file.C
@@ -6,12 +6,9 @@
 
 static
 char     magic[16] = { 'p', 'o', 's', 'i', 't', 'i', 'o', 'n', 'D', 'B', '1', ' ', ' ', ' ', ' ', ' '  };
+static
+char     faild[16] = { 'p', 'o', 's', 'i', 't', 'i', 'o', 'n', 'D', 'B', 'f', 'a', 'i', 'l', 'e', 'd'  };
 
-//  XXX: Includes ifdef'd support for writing the magic AFTER the data
-//  has been written, ensuring that the whole write is complete.  Not
-//  tested much, so not enabled.
-//
-//#define MAGIC_AFTER_DATA
 
 void
 positionDB::saveState(char const *filename) {
@@ -23,19 +20,12 @@ positionDB::saveState(char const *filename) {
     exit(1);
   }
 
-  u64bit  hs = _tableSizeInEntries * _hashWidth / 64 + 1;
-  u64bit  bs = _numberOfDistinct   * _wFin      / 64 + 1;
-  u64bit  ps = _numberOfEntries    * _posnWidth / 64 + 1;
+  bool    magicFirst = false;
 
-#ifndef MAGIC_AFTER_DATA
-  fwrite(magic,      sizeof(char),       16, F);
-#else
   //  Test if this is a pipe.  If so, we write the magic first,
   //  otherwise we write the magic last.
   //
-  char  cigam[16] = { 0 };
-  bool  magicFirst = false;
-
+  errno      = 0;
   lseek(fileno(F), 0, SEEK_SET);
   if (errno == ESPIPE)
     magicFirst = true;
@@ -43,15 +33,19 @@ positionDB::saveState(char const *filename) {
   if (magicFirst)
     fwrite(magic,    sizeof(char),       16, F);
   else
-    fwrite(cigam,    sizeof(char),       16, F);
-#endif
-  fwrite(this,       sizeof(positionDB), 1,  F);
-  fwrite(_hashTable, sizeof(u64bit),     hs, F);
-  fwrite(_buckets,   sizeof(u64bit),     bs, F);
-  fwrite(_positions, sizeof(u64bit),     ps, F);
+    fwrite(faild,    sizeof(char),       16, F);
 
-#ifdef MAGIC_AFTER_DATA
-  if (!magicFirst) {
+  fwrite(this,       sizeof(positionDB), 1,  F);
+  fwrite(_hashTable, sizeof(u64bit),     _tableSizeInEntries * _hashWidth / 64 + 1, F);
+  fwrite(_buckets,   sizeof(u64bit),     _numberOfDistinct   * _wFin      / 64 + 1, F);
+  fwrite(_positions, sizeof(u64bit),     _numberOfEntries    * _posnWidth / 64 + 1, F);
+
+  if (errno) {
+    fprintf(stderr, "positionDB::saveState()-- Write failure.\n%s\n", strerror(errno));
+    exit(1);
+  }
+
+  if (magicFirst == false) {
     lseek(fileno(F), 0, SEEK_SET);
     if (errno) {
       fprintf(stderr, "positionDB::saveState()-- Failed to seek to start of file -- write failed.\n%s\n", strerror(errno));
@@ -59,7 +53,6 @@ positionDB::saveState(char const *filename) {
     }
     fwrite(magic, sizeof(char), 16, F);
   }
-#endif
 
   fclose(F);
 
@@ -83,7 +76,12 @@ positionDB::loadState(char const *filename, bool beNoisy, bool loadData) {
 
   fread(cigam, sizeof(char), 16, F);
 
-  if (strncmp(magic, cigam, 16) != 0) {
+  if        (strncmp(faild, cigam, 16) == 0) {
+    if (beNoisy)
+      fprintf(stderr, "positionDB::loadState()-- Incomplete positionDB binary file.\n");
+    fclose(F);
+    return(false);
+  } else if (strncmp(magic, cigam, 16) != 0) {
     if (beNoisy) {
       fprintf(stderr, "positionDB::loadState()-- Not a positionDB binary file, maybe a sequence file?\n");
       fprintf(stderr, "positionDB::loadState()-- Read     '%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c'\n",
@@ -136,37 +134,18 @@ positionDB::loadState(char const *filename, bool beNoisy, bool loadData) {
 
 void
 positionDB::printState(FILE *stream) {
-
-#ifdef TRUE64BIT
-  fprintf(stream, "merSizeInBases:       %u\n",  _merSizeInBases);
-  fprintf(stream, "merSkipInBases:       %u\n",  _merSkipInBases);
-  fprintf(stream, "tableSizeInBits:      %u\n",  _tableSizeInBits);
-  fprintf(stream, "tableSizeInEntries:   %lu\n", _tableSizeInEntries);
-  fprintf(stream, "hashWidth:            %u\n",  _hashWidth);
-  fprintf(stream, "chckWidth:            %u\n",  _chckWidth);
-  fprintf(stream, "posnWidth:            %u\n",  _posnWidth);
-  fprintf(stream, "numberOfMers:         %lu\n", _numberOfMers);
-  fprintf(stream, "numberOfPositions:    %lu\n", _numberOfPositions);
-  fprintf(stream, "numberOfDistinct:     %lu\n", _numberOfDistinct);
-  fprintf(stream, "numberOfUnique:       %lu\n", _numberOfUnique);
-  fprintf(stream, "numberOfEntries:      %lu\n", _numberOfEntries);
-  fprintf(stream, "maximumEntries:       %lu\n", _maximumEntries);
-#else
-  fprintf(stream, "merSizeInBases:       %lu\n",  _merSizeInBases);
-  fprintf(stream, "merSkipInBases:       %lu\n",  _merSkipInBases);
-  fprintf(stream, "tableSizeInBits:      %lu\n",  _tableSizeInBits);
-  fprintf(stream, "tableSizeInEntries:   %llu\n", _tableSizeInEntries);
-  fprintf(stream, "hashWidth:            %lu\n",  _hashWidth);
-  fprintf(stream, "chckWidth:            %lu\n",  _chckWidth);
-  fprintf(stream, "posnWidth:            %lu\n",  _posnWidth);
-  fprintf(stream, "numberOfMers:         %llu\n", _numberOfMers);
-  fprintf(stream, "numberOfPositions:    %llu\n", _numberOfPositions);
-  fprintf(stream, "numberOfDistinct:     %llu\n", _numberOfDistinct);
-  fprintf(stream, "numberOfUnique:       %llu\n", _numberOfUnique);
-  fprintf(stream, "numberOfEntries:      %llu\n", _numberOfEntries);
-  fprintf(stream, "maximumEntries:       %llu\n", _maximumEntries);
-#endif
-
-
+  fprintf(stream, "merSizeInBases:       "u32bitFMT"\n",  _merSizeInBases);
+  fprintf(stream, "merSkipInBases:       "u32bitFMT"\n",  _merSkipInBases);
+  fprintf(stream, "tableSizeInBits:      "u32bitFMT"\n",  _tableSizeInBits);
+  fprintf(stream, "tableSizeInEntries:   "u64bitFMT"\n", _tableSizeInEntries);
+  fprintf(stream, "hashWidth:            "u32bitFMT"\n",  _hashWidth);
+  fprintf(stream, "chckWidth:            "u32bitFMT"\n",  _chckWidth);
+  fprintf(stream, "posnWidth:            "u32bitFMT"\n",  _posnWidth);
+  fprintf(stream, "numberOfMers:         "u64bitFMT"\n", _numberOfMers);
+  fprintf(stream, "numberOfPositions:    "u64bitFMT"\n", _numberOfPositions);
+  fprintf(stream, "numberOfDistinct:     "u64bitFMT"\n", _numberOfDistinct);
+  fprintf(stream, "numberOfUnique:       "u64bitFMT"\n", _numberOfUnique);
+  fprintf(stream, "numberOfEntries:      "u64bitFMT"\n", _numberOfEntries);
+  fprintf(stream, "maximumEntries:       "u64bitFMT"\n", _maximumEntries);
 }
 
