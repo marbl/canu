cd87e17b2160b305bb4f514e483ce62f735f28c6
Brian P. Walenz <thebri@gmail.com>
2004-08-31 10:23:18 +0000

    Much better, but still not symmetric for the multiple cases.

--- kmer/sim4dbutils/comparePolishes.C
+++ kmer/sim4dbutils/comparePolishes.C
@@ -11,24 +11,32 @@
 //  Matches two sets of polishes to each other using a simple overlap
 //  heuristic.
 //
-//  Output is:
-//    AmatchIID BmatchIID  ESTlen  A%id A%cov  B%id B%cov  overlap
+//  Arguments (are horrible, whatcha gonna do about it?):
 //
-//  Arguments:
-//   
-//    -i min-percent-id (default 95)
-//    -c min-percent-coverage (default 50)
-//    -a polishes-file-1
-//    -b polishes-file-1
+//    -i min percent id (default 95)
+//    -c min percent coverage (default 50)
+//    -a polishes file 1
+//    -b polishes file 2
 //
-//    -O output-file
-//    -A output-polishes-from-file-a
-//    -B output-polishes-from-file-b
-//    -L log-output-file
+//    -O output file
+//    -A polishes from file a
+//    -B polishes from file b
+//    -L log output file
 //
 //  The default is to output on stdout.  -A, -B write the overlapped
-//  matches in order.  -L reports a log of weird stuff.
+//  matches in the same order as the output-file.  -L reports a log of
+//  weird stuff.
 //
+//  Output is tab-delimited:
+//    ESTiid ESTlen  overlap  A%id A%cov #cdnagaps #exons  B%id B%cov #cdnagaps #exons
+//
+
+//  e,b good:113408 Anovel:1547 Amulti: 216 Bnovel:83220 Bmulti:  13 hairy: 270
+//  b,e good:113408 Bnovel:1547 Bmulti:  76 Anovel:83220 Amulti:  69 hairy: 129
+
+//  b,e good:113408 Anovel:1547 Amulti: 216 Bnovel:83220 Bmulti:  13 hairy:  54
+//  e,b good:113408 Bnovel:1547 Bmulti:  76 Anovel:83220 Amulti:  69 hairy:  60
+
 
 u32bit  findOverlap(sim4polish *A, sim4polish *B);
 
@@ -42,10 +50,25 @@ main(int argc, char **argv) {
   FILE            *Aout = 0L;
   FILE            *Bout = 0L;
 
-  //  Stats
-  u32bit           goodOverlap = 0;
-  u32bit           multipleInA = 0;
-  u32bit           multipleInB = 0;
+  //  goodOverlap   -- match in A maps uniquely to B and likewise.
+  //
+  //  novelInA      -- a match in A has no counterpart in B.
+  //  novelInB      -- similar for B.
+  //
+  //  multipleInA   -- a match in B maps to multiple things in A.
+  //  multipleInB   -- similar for A.
+  //
+  //  multipleInA requires that the matches in A map only to the single
+  //  match in B.
+  //
+  //  hairyOverlap  -- multiple matches in both.
+  //
+  u32bit           goodOverlap  = 0;  //  the number of lines in the output
+  u32bit           novelInA     = 0;
+  u32bit           novelInB     = 0;
+  u32bit           multipleInA  = 0;
+  u32bit           multipleInB  = 0;
+  u32bit           hairyOverlap = 0;
 
   int arg=1;
   while(arg < argc) {
@@ -81,23 +104,27 @@ main(int argc, char **argv) {
     exit(1);
   }
 
+
   //  Force index builds
   //
   Afile->setPosition(0);
   Bfile->setPosition(0);
 
 
-  //  Ask both for the largest EST iid seen, then iterate over those.
+  //  Find the largest IID
   //
   u32bit  largestIID = Afile->maxIID();
   if (largestIID < Bfile->maxIID())
     largestIID = Bfile->maxIID();
 
+
+  //  Iterate over all the ESTs.
+
   for (u32bit iid=0; iid<largestIID; iid++) {
     sim4polishList *A = Afile->getEST(iid);
     sim4polishList *B = Bfile->getEST(iid);
 
-    //  Filter by quality
+    //  Filter by quality.
     A->filterByQuality(minI, minC);
     B->filterByQuality(minI, minC);
 
@@ -109,105 +136,106 @@ main(int argc, char **argv) {
       overlap[i] = overlap[i-1] + B->length();
 
     for (u32bit a=0; a<A->length(); a++)
-      for (u32bit b=0; b<B->length(); b++) {
+      for (u32bit b=0; b<B->length(); b++)
         overlap[a][b] = findOverlap((*A)[a], (*B)[b]);
 
-#if 0
-        if (overlap[a][b] > 0) {
-          fprintf(stderr, "overlap[%2d][%2d] = "u32bitFMT"\n", a, b, overlap[a][b]);
-          //s4p_printPolish(stderr, (*A)[a], S4P_PRINTPOLISH_NORMALIZED | S4P_PRINTPOLISH_MINIMAL);
-          //s4p_printPolish(stderr, (*B)[b], S4P_PRINTPOLISH_NORMALIZED | S4P_PRINTPOLISH_MINIMAL);
-        }
-#endif
-      }
 
-    //  For each match in A, find the one in B that overlaps it.  If
-    //  there is more than one, record an error, and merge the B's
-    //  into one match.
-    //
+
+    //  For each match in A, find the one in B that overlaps it.
 
     for (u32bit a=0; a<A->length(); a++) {
 
-      //  Count the number of things we overlap.  We count the number
-      //  of times that this A overlaps any B, and if exactly once,
-      //  then we count the number of times that B overlaps any A.  If
-      //  also once, we declare a match.
-
-      u32bit Aoverlaps = 0;
-      u32bit theAovl   = 0;
       u32bit Boverlaps = 0;
       u32bit theBovl   = 0;
 
+      //  Count the number of things we overlap in B.
       for (u32bit b=0; b<B->length(); b++) {
         if (overlap[a][b]) {
-          Aoverlaps++;
-          theAovl = b;
+          Boverlaps++;
+          theBovl = b;
         }
       }
 
-      //  Continue only if we found an overlap in B.  If we didn't there is no match, period.
+      //  No overlaps --> novelInA
+      //
+      if (Boverlaps == 0) {
+        novelInA++;
+          //  XXX OUTPUT
+      }
 
-      if (Aoverlaps > 0) {
+      //  One overlap?  Possibly a goodOverlap, possibly multipleInA.
+      //
+      else if (Boverlaps == 1) {
 
-        for (u32bit i=0; i<A->length(); i++) {
-          if (overlap[i][theAovl]) {
-            Boverlaps++;
-            theBovl = i;
-          }
-        }
+        //  Count the number of overlaps the guy in B has with A.  If
+        //  1, it's a goodOverlap, else it's a multipleInA.
 
-        //  By definition if there is an A overlap, there will be a(t
-        //  least one) B overlap.
+        u32bit Aoverlaps = 0;
+        for (u32bit x=0; x<A->length(); x++)
+          if (overlap[x][theBovl])
+            Aoverlaps++;
 
-        if ((Aoverlaps == 1) && (Boverlaps == 1)) {
-          //  Got a good overlap!  match 'theAovl' and match 'theBovl'
-          //  are probably the same.
-          //
+        if (Aoverlaps == 1) {
           goodOverlap++;
-        } else if ((Aoverlaps > 1) || (Boverlaps > 1)) {
-          //  Dang!  Somebody matched more than once.
-          //
-          if (Aoverlaps > 1)
-            multipleInB++;
-          if (Boverlaps > 1)
-            multipleInA++;
-
-#if 0
-          //  This will print out details for the multiple match.
-
-          fprintf(stderr, "Multiple overlaps!\n");
-
-          fprintf(stderr, "A="u32bitFMT" with overlaps of ", a);
-          for (u32bit x=0; x<B->length(); x++)
-            fprintf(stderr, " B="u32bitFMT":"u32bitFMT, x, overlap[a][x]);
-          fprintf(stderr, "\n");
-
-          s4p_printPolish(stderr, (*A)[a], S4P_PRINTPOLISH_NORMALIZED | S4P_PRINTPOLISH_MINIMAL);
-
-          for (u32bit x=0; x<B->length(); x++) {
-            if (overlap[a][x]) {
-              fprintf(stderr, "B="u32bitFMT" with overlaps of ", x);
-              for (u32bit y=0; y<A->length(); y++)
-                fprintf(stderr, " A="u32bitFMT":"u32bitFMT, y, overlap[y][x]);
-              fprintf(stderr, "\n");
-
-              s4p_printPolish(stderr, (*B)[x], S4P_PRINTPOLISH_NORMALIZED | S4P_PRINTPOLISH_MINIMAL);
-            }
-          }
-#endif
-
-
+          //  XXX OUTPUT
         } else {
-          fprintf(stderr, "Huh?!  I shouldn't be printing this!\n");
-          exit(1);
+          multipleInA++;
+          //  XXX OUTPUT
+        }
+      }
+
+      //  More than one overlap?  Possibly multipleInB, possilby hairyOverlap.
+      //
+      else if (Boverlaps > 1) {
+
+        //  Examine the overlaps the guys in B have with A.  If any
+        //  are to something other than a, it's hairy, otherwise it's
+        //  multipleInB.
+
+        bool hairy = false;
+
+        for (u32bit b=0; b<B->length(); b++)
+          if (overlap[a][b]) {
+
+            //  Found an overlap, check all of A and see if this b overlaps
+            //  anything other than this a
+
+            for (u32bit x=0; x<A->length(); x++)
+              if ((overlap[x][b]) && (a != x))
+                hairy = true;
+          }
+
+        if (hairy == false) {
+          multipleInB++;
+          //  XXX OUTPUT
+        } else {
+          hairyOverlap++;
+          //  XXX OUTPUT
         }
       }
     }
 
-    fprintf(stderr, "IID:"u32bitFMTW(8)" A:"u32bitFMTW(4)" B:"u32bitFMTW(4)"  multiInA:"u32bitFMT" multiInB:"u32bitFMT" good:"u32bitFMT"\n",
-            iid,
-            A->length(), B->length(),
-            multipleInA, multipleInB, goodOverlap);
+    //  Look for novelInB separately.
+    //
+    for (u32bit b=0; b<B->length(); b++) {
+      u32bit Boverlaps = 0;
+
+      for (u32bit a=0; a<A->length(); a++)
+        if (overlap[a][b])
+          Boverlaps++;
+
+      if (Boverlaps == 0)
+        novelInB++;
+    }
+
+
+    if ((iid % 1234) == 0) {
+      fprintf(stderr, "IID:"u32bitFMTW(8)" A:"u32bitFMTW(4)" B:"u32bitFMTW(4)"  good:"u32bitFMTW(4)" Anovel:"u32bitFMTW(4)" Amulti:"u32bitFMTW(4)" Bnovel:"u32bitFMTW(4)" Bmulti:"u32bitFMTW(4)" hairy:"u32bitFMTW(4)"\r",
+              iid,
+              A->length(), B->length(),
+              goodOverlap, novelInA, multipleInA, novelInB, multipleInB, hairyOverlap);
+      fflush(stderr);
+    }
 
     //  Do it again for B -> A, iterate until it's stable.
 
@@ -224,8 +252,10 @@ main(int argc, char **argv) {
     delete B;
   }
 
+  fprintf(stderr, "\ngood:"u32bitFMTW(4)" Anovel:"u32bitFMTW(4)" Amulti:"u32bitFMTW(4)" Bnovel:"u32bitFMTW(4)" Bmulti:"u32bitFMTW(4)" hairy:"u32bitFMTW(4)"\n",
+          goodOverlap, novelInA, multipleInA, novelInB, multipleInB, hairyOverlap);
+
+
   delete Afile;
   delete Bfile;
 }
-
-
