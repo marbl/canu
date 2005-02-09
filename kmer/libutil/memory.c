b2f1cc5c1730be4467a135789b30b1fc726014b5
Brian P. Walenz <thebri@gmail.com>
2005-02-09 05:46:20 +0000

    Removed compiler warning.

--- kmer/libutil/memory.c
+++ kmer/libutil/memory.c
@@ -12,13 +12,12 @@ memdup(const void *orig, size_t size) {
     rslt = malloc(size);
     if (errno) {
       //  Some ugliness to print out a size_t.  This might be useless,
-      //  as it might be determined by TRUE64BIT.  You also get a
-      //  compiler warning here, probably.
+      //  as it might be determined by TRUE64BIT.
       //
       if (sizeof(size_t) == 8)
-        fprintf(stderr, "memdup()-- can't allocate "s64bitFMT" bytes.\n%s\n", size, strerror(errno));
+        fprintf(stderr, "memdup()-- can't allocate "s64bitFMT" bytes.\n%s\n", (s64bit)size, strerror(errno));
       else
-        fprintf(stderr, "memdup()-- can't allocate "u32bitFMT" bytes.\n%s\n", size, strerror(errno));
+        fprintf(stderr, "memdup()-- can't allocate "u32bitFMT" bytes.\n%s\n", (u32bit)size, strerror(errno));
       exit(1);
     }
     memcpy(rslt, orig, size);
