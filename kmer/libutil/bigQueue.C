54d1bebea708a0fd7fbd6309777f6e5b06bcfd1b
Brian P. Walenz <thebri@gmail.com>
2005-03-15 01:33:40 +0000

    1) removed _inputFile, since we couldn't create another file handle from an existing one and have independent file pointers. 2) If only one temporary file, don't do a merge.

--- kmer/libutil/bigQueue.C
+++ kmer/libutil/bigQueue.C
@@ -48,10 +48,12 @@ bigQueue::_initialize(int    (*sortfcn)(const void *a, const void *b),
   //
   _temporaryFiles[_numTemporaryFiles++] = makeTempFile(_tmpPath);
 
-  //  There has got to be a better way of getting a duplicate file(3) handle....
-  //  XXX - indeed!  if you don't flush() before reading this, you die.
+  //  XXX: It would be rather convenient if we could get another file
+  //  handle given an existing handle (no, dup(2) doesn't do that).
+  //  In particular, we want two file pointers, one for read, one for
+  //  write.
   //
-  _inputFile  = fdopen(fileno(_temporaryFiles[0]), "r");
+  //_inputFile  = fdopen(dup(fileno(_temporaryFiles[0])), "w+");
 
   _thingBuffer = new u64bit [_objectSize / 8 + 1];
 
@@ -73,7 +75,7 @@ bigQueue::~bigQueue() {
 
   delete [] _temporaryFiles;
 
-  fclose(_inputFile);
+  //fclose(_inputFile);
 
   clearBuffer();
 }
@@ -157,110 +159,121 @@ bigQueue::clearBuffer(void) {
 
 void
 bigQueue::mergeTemporaryFiles(void) {
-  dict_t    *sorted;
-  dnode_t   *nodes = new dnode_t [_maxOpenFiles];
 
-  //  To be efficient, we need to maintain a sorted queue of the head
-  //  elements of each temporary file.  A red-black tree would do
-  //  nicely, eh?
-  //
-  sorted = dict_create(DICTCOUNT_T_MAX, _sortFunction);
+  if (_numTemporaryFiles > 1) {
+    dict_t    *sorted;
+    dnode_t   *nodes = new dnode_t [_maxOpenFiles];
 
-  //  Grab the first thing off each file, insert it into the dictionary.
-  //  The 'key' is our chunk of data, and the 'value' is the file number
-  //  it came from.
-  //
-  for (u32bit i=0; i<_numTemporaryFiles; i++) {
-    if (_temporaryFiles[i]) {
+    //  To be efficient, we need to maintain a sorted queue of the head
+    //  elements of each temporary file.  A red-black tree would do
+    //  nicely, eh?
+    //
+    sorted = dict_create(DICTCOUNT_T_MAX, _sortFunction);
 
-      //  Rewind all the temporary files.  XXXX This is probably done
-      //  already.
-      //
-      ::rewind(_temporaryFiles[i]);
+    //  Grab the first thing off each file, insert it into the dictionary.
+    //  The 'key' is our chunk of data, and the 'value' is the file number
+    //  it came from.
+    //
+    for (u32bit i=0; i<_numTemporaryFiles; i++) {
+      if (_temporaryFiles[i]) {
 
-      void *thing = malloc(_objectSize);
+        //  Rewind all the temporary files.  XXXX This is probably done
+        //  already.
+        //
+        ::rewind(_temporaryFiles[i]);
 
-      if (_readFunction)
-        (*_readFunction)(_temporaryFiles[i], thing);
-      else
-        fread(thing, _objectSize, 1, _temporaryFiles[i]);
+        void *thing = malloc(_objectSize);
 
-      if (feof(_temporaryFiles[i])) {
-        fclose(_temporaryFiles[i]);
-        _temporaryFiles[i] = 0L;
-      } else {
-        //  initialize the node with the value
-        dnode_init(&nodes[i], (void *)i);
+        if (_readFunction)
+          (*_readFunction)(_temporaryFiles[i], thing);
+        else
+          fread(thing, _objectSize, 1, _temporaryFiles[i]);
 
-        //  insert the node into the tree using the key
-        dict_insert(sorted, &nodes[i], thing);
+        if (feof(_temporaryFiles[i])) {
+          fclose(_temporaryFiles[i]);
+          _temporaryFiles[i] = 0L;
+        } else {
+          //  initialize the node with the value
+          dnode_init(&nodes[i], (void *)i);
+
+          //  insert the node into the tree using the key
+          dict_insert(sorted, &nodes[i], thing);
+        }
       }
     }
-  }
 
-  FILE *mergeFile = makeTempFile(_tmpPath);
+    FILE *mergeFile = makeTempFile(_tmpPath);
 
-  //  while there is stuff in the tree
+    //  while there is stuff in the tree
 
-  while (dict_isempty(sorted) == 0) {
+    while (dict_isempty(sorted) == 0) {
 
-    //  pop the head element off, and print it
-    dnode_t  *head = dict_first(sorted);
+      //  pop the head element off, and print it
+      dnode_t  *head = dict_first(sorted);
 
-    //  XXX: should be const thing
+      //  XXX: should be const thing
 
-    void   *thing  = (void *)dnode_getkey(head);
-    int     fileid = (int)dnode_get(head);
+      void   *thing  = (void *)dnode_getkey(head);
+      int     fileid = (int)dnode_get(head);
 
-    if (_writFunction)
-      (*_writFunction)(mergeFile, thing);
-    else
-      fwrite(thing, _objectSize, 1, mergeFile);
+      if (_writFunction)
+        (*_writFunction)(mergeFile, thing);
+      else
+        fwrite(thing, _objectSize, 1, mergeFile);
 
-    //  delete the node from the tree
-    dict_delete(sorted, head);
+      //  delete the node from the tree
+      dict_delete(sorted, head);
 
-    //  destroy the thing
-    if (_killFunction)
-      (*_killFunction)(thing);
-    else
-      free(thing);
+      //  destroy the thing
+      if (_killFunction)
+        (*_killFunction)(thing);
+      else
+        free(thing);
 
-    //  load the next element from the same file that the head was
-    //  from (that's stored as the value of the head element)
+      //  load the next element from the same file that the head was
+      //  from (that's stored as the value of the head element)
 
-    thing = malloc(_objectSize);
+      thing = malloc(_objectSize);
 
-    if (_readFunction)
-      (*_readFunction)(_temporaryFiles[fileid], thing);
-    else
-      fread(thing, _objectSize, 1, _temporaryFiles[fileid]);
+      if (_readFunction)
+        (*_readFunction)(_temporaryFiles[fileid], thing);
+      else
+        fread(thing, _objectSize, 1, _temporaryFiles[fileid]);
 
-    //  if there was a next element in that file, insert it
-    //  into the tree.  if not, close the temporary file.
-    //
-    if (feof(_temporaryFiles[fileid])) {
-      fclose(_temporaryFiles[fileid]);
-      _temporaryFiles[fileid] = 0;
-      free(thing);
-    } else {
-      //  initialize the node with the value
-      dnode_init(&nodes[fileid], (void *)fileid);
+      //  if there was a next element in that file, insert it
+      //  into the tree.  if not, close the temporary file.
+      //
+      if (feof(_temporaryFiles[fileid])) {
+        fclose(_temporaryFiles[fileid]);
+        _temporaryFiles[fileid] = 0;
+        free(thing);
+      } else {
+        //  initialize the node with the value
+        dnode_init(&nodes[fileid], (void *)fileid);
 
-      //  insert the node into the tree using the key
-      dict_insert(sorted, &nodes[fileid], thing);
+        //  insert the node into the tree using the key
+        dict_insert(sorted, &nodes[fileid], thing);
+      }
     }
+
+    dict_free(sorted);
+    delete [] nodes;
+
+    _numTemporaryFiles = 1;
+    _temporaryFiles[0] = mergeFile;
   }
 
-  dict_free(sorted);
-
-  _numTemporaryFiles = 1;
-  _temporaryFiles[0] = mergeFile;
   ::rewind(_temporaryFiles[0]);
 
+#if 0
   fclose(_inputFile);
-  _inputFile  = fdopen(fileno(_temporaryFiles[0]), "r");
+  errno = 0;
+  _inputFile  = fdopen(dup(fileno(_temporaryFiles[0])), "w+");
+  if (errno)
+    fprintf(stderr, "bigQueue::mergeTemporaryFiles()-- _inputFile = fdopen() failed: %s\n", strerror(errno)), exit(1);
+
   ::rewind(_inputFile);
+#endif
 }
 
 
@@ -268,12 +281,19 @@ bool
 bigQueue::next(void) {
 
   if (_readFunction) {
-    (*_readFunction)(_inputFile, _thingBuffer);
+    //(*_readFunction)(_inputFile, _thingBuffer);
+    (*_readFunction)(_temporaryFiles[0], _thingBuffer);
   } else {
-    fread(_thingBuffer, _objectSize, 1, _inputFile);
+    //fread(_thingBuffer, _objectSize, 1, _inputFile);
+    fread(_thingBuffer, _objectSize, 1, _temporaryFiles[0]);
   }
 
+#if 0
   if (feof(_inputFile))
+    return(false);
+#endif
+
+  if (feof(_temporaryFiles[0]))
     return(false);
 
   return(true);
@@ -287,7 +307,8 @@ bigQueue::get(void) {
 
 void
 bigQueue::rewind(void) {
-  ::rewind(_inputFile);
+  //::rewind(_inputFile);
+  ::rewind(_temporaryFiles[0]);
   next();
 }
 
