d3b74de42ae464baec3d277c9ee162d0aec62a69
Brian P. Walenz <thebri@gmail.com>
2005-05-24 01:11:59 +0000

    Testing of kMer's setBits() and getBits()

---
+++ kmer/libbio/test/test-setbits.C
@@ -0,0 +1,30 @@
+#include "bio++.H"
+
+#if 0
+g++ -o test-setbits test-setbits.C -I../libutil -I. -L../libutil -L. -lbio -lutil
+#endif
+
+int
+main(int argc, char **argv) {
+  
+  kMer   x(96);
+  char   str[256];
+
+  if (KMER_WORDS < 3) {
+    fprintf(stderr, "I need at least KMER_WORDS == 3\n");
+    exit(1);
+  }
+
+  for (u32bit i=0; i<168; i++) {
+    x.clear();
+    x.setBits(i, 24, 0x535);
+    fprintf(stderr, u32bitFMTW(3)" -- %s -- "u64bitHEX"\n", i, x.merToString(str), x.getBits(i, 16));
+
+    if (x.getBits(i, 16) != 0x535) {
+      fprintf(stderr, "decode error.\n");
+      exit(1);
+    }
+  }
+
+  exit(0);
+}
