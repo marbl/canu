e9c646ba2e9bd7ebae508f82deedc1408ba4d886
Eli Venter <invalid@invalid.org>
2005-06-10 17:44:31 +0000

    For high(20x) coverage of a large (1.7MB) contig, consensus was dieing trying to malloc a 2GB chunk of memory.

--- src/AS_UTL/AS_UTL_Var.c
+++ src/AS_UTL/AS_UTL_Var.c
@@ -18,7 +18,7 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
-static char CM_ID[] = "$Id: AS_UTL_Var.c,v 1.5 2005-06-09 21:06:48 brianwalenz Exp $";
+static char CM_ID[] = "$Id: AS_UTL_Var.c,v 1.6 2005-06-10 17:44:31 eliv Exp $";
 /********************************************************************/
 /* Variable Length C Array Package 
  * 
@@ -30,8 +30,8 @@ static char CM_ID[] = "$Id: AS_UTL_Var.c,v 1.5 2005-06-09 21:06:48 brianwalenz E
  * It defines a basic set of operations, and provides a set of
  * macros that expand to support typesafe manipulation of the
  * arrays.
- * Revision: $Revision: 1.5 $
- * Date:     $Date: 2005-06-09 21:06:48 $
+ * Revision: $Revision: 1.6 $
+ * Date:     $Date: 2005-06-10 17:44:31 $
  * CMM, 1999/03/29:  Ported to large arrays on the Digital systems by declaring
  * array sizes using size_t, rather than unit32.
  *
@@ -130,7 +130,8 @@ int MakeRoom_VA
     tentativeNewSize = (((size_t)1) << ceil_log2(newSize));
     
     // If we need to use the end of the block, do it
-    newSize = max(newSize, tentativeNewSize);
+    if ( ! tentativeNewSize >= INT_MAX ) // large mallocs fail, so try a smaller one
+        newSize = max(newSize, tentativeNewSize);
   }
   
   assert(oldSize <= newSize);
