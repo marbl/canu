6d63cfbc6f07296c17428e5e92391545716732e1
Eli Venter <invalid@invalid.org>
2005-07-14 20:08:46 +0000

    Add a couple more types, since x86_64 seems to be different.

--- src/AS_PER/testFrgStoreSizes.c
+++ src/AS_PER/testFrgStoreSizes.c
@@ -74,7 +74,9 @@ int main(int argc, char *argv[]){
   printf("IntMultiPos size is %ld\n",sizeof(IntMultiPos));
   printf("VarArrayType size is %ld\n",sizeof(VarArrayType));
   printf("FileVarArrayType size is %ld\n",sizeof(FileVarArrayType));
-  printf("size_t size is %ld\n",sizeof(size_t));
+  printf("CDS_IID_t size is %ld\n",sizeof(CDS_IID_t));
+  printf("CDS_UID_t size is %ld\n",sizeof(CDS_UID_t));
+  printf("time_t size is %ld\n",sizeof(time_t));
   return (0);
 }
 
