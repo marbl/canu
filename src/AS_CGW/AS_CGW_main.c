01c2676b717991392834cf342bc58f5a439db228
Eli Venter <invalid@invalid.org>
2005-07-25 15:55:53 +0000

    For microbial genomes the physical checkpoint can be smaller then the logical checkpoint.

--- src/AS_CGW/AS_CGW_main.c
+++ src/AS_CGW/AS_CGW_main.c
@@ -18,7 +18,7 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
-static const char CM_ID[] = "$Id: AS_CGW_main.c,v 1.7 2005-07-15 18:19:53 eliv Exp $";
+static const char CM_ID[] = "$Id: AS_CGW_main.c,v 1.8 2005-07-25 15:55:53 eliv Exp $";
 
 
 /*********************************************************************
@@ -496,7 +496,7 @@ int main(int argc, char *argv[]){
       {
 	fprintf(GlobalData->stderrc,"* argc = %d optind = %d setFragStore = %d setGatekeeperStore = %d outputPath = %s\n",
 		argc, optind, setFragStore,setGatekeeperStore, outputPath);
-	fprintf (GlobalData->stderrc, "USAGE:  cgw [-deEcvPuUtTjXY] [-j <uniqueCutoff>] [-D <debugLevel>] [-r <repeatRezLevel>] [-p <preMergeRezLevel>[L]] [-s <stoneLevel>[L]] [-w <walkLevel>] [-R <checkpoint>] [-N <checkpoint>] [-K <aligner>] -f <FragStoreName> -g <GatekeeperStoreName> -o <OutputPath> [<InputFileName>.<ext>]*\n"
+	fprintf (GlobalData->stderrc, "USAGE:  cgw [-deEcvPuUtTjXY] [-j <uniqueCutoff>] [-D <debugLevel>] [-r <repeatRezLevel>] [-p <preMergeRezLevel>[L]] [-s <stoneLevel>[L]] [-w <walkLevel>] [-R <checkpoint>] [-N <cgwStage>] [-K <aligner>] -f <FragStoreName> -g <GatekeeperStoreName> -o <OutputPath> [<InputFileName>.<ext>]*\n"
                  "Use 'L' after <repeatRezLevel> or <stoneLevel> to generate\n"
                  "  copious .log and .analysis files\n"
 		 "Opens ALL [<InputFileName>.<ext>]* to read input\n"
@@ -615,12 +615,6 @@ int main(int argc, char *argv[]){
 
   data->saveCheckPoints = checkPoint;
   data->outputCalculatedOffsets = (geneOutput == 0);
-
-  if(restartFromCheckpoint < restartFromLogicalCheckpoint && restartFromLogicalCheckpoint !=  CHECKPOINT_BEFORE_FINAL_CLEANUP){
-    fprintf(GlobalData->stderrc,"* Logical Checkpoint (%d)  MUST be no greater than restart checkpoint (%d)\n",
-	    restartFromLogicalCheckpoint, restartFromCheckpoint);
-    exit(1);
-  }
 
   if(restartFromLogicalCheckpoint == NULLINDEX){
     restartFromLogicalCheckpoint = restartFromCheckpoint;
