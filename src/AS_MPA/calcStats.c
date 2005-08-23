60b80d10fe8d41012138bf6006fec4fe4e3b97c3
Ian Dew <invalid@invalid.org>
2005-08-23 13:07:01 +0000

    Fixed compile bug in OSF1 & simultaneously fixed initialization of doubles from FLT_MIN & FLT_MAX to MINDOUBLE and MAXDOUBLE, respectively.

--- src/AS_MPA/calcStats.c
+++ src/AS_MPA/calcStats.c
@@ -1,3 +1,19 @@
+/**************************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received (LICENSE.txt) a copy of the GNU General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *************************************************************************/
+/* $Id: calcStats.c,v 1.2 2005-08-23 13:07:01 catmandew Exp $ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -40,8 +56,8 @@ int main(int argc, char ** argv)
   double val;
   double mean;
   double sigma;
-  double greaterThan = FLT_MIN; // MINFLOAT;
-  double lessThan = FLT_MAX; // MAXFLOAT;
+  double greaterThan = MINDOUBLE;
+  double lessThan = MAXDOUBLE;
   {
     int ch;
     while((ch = getopt(argc, argv, "chf:g:l:")) != EOF)
