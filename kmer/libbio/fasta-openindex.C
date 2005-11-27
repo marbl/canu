0336becc870fbeeee7b5b0094b32890b5dd84924
Brian P. Walenz <thebri@gmail.com>
2005-11-27 09:51:26 +0000

    Endianess and 64/32-bit safe now!

--- kmer/libbio/fasta-openindex.C
+++ kmer/libbio/fasta-openindex.C
@@ -83,6 +83,28 @@ FastAWrapper::isIndexOnDiskCompatible(u32bit indextype, const char *indexname, b
     //  can't close
   }
 
+  //  Fix endianess issues in the index.
+  //
+#if FASTA_VERSIONNUMBER > 4
+  if (theGlobalDesc._magic == u64bitSwap(FASTA_MAGICNUMBER)) {
+    //fprintf(stderr, "WARNING:  Fixing index for endianess swap!\n");
+
+    theGlobalDesc._magic                 = u64bitSwap(theGlobalDesc._magic);
+    theGlobalDesc._version               = u32bitSwap(theGlobalDesc._version);
+    theGlobalDesc._indexType             = u32bitSwap(theGlobalDesc._indexType);
+    theGlobalDesc._fastaType             = u32bitSwap(theGlobalDesc._fastaType);
+    theGlobalDesc._numberOfSequences     = u32bitSwap(theGlobalDesc._numberOfSequences);
+    theGlobalDesc._fastaFileSize         = idxfaPosSwap(theGlobalDesc._fastaFileSize);
+    theGlobalDesc._fastaModificationTime = idxfaTimeSwap(theGlobalDesc._fastaModificationTime);
+    theGlobalDesc._fastaCreationTime     = idxfaTimeSwap(theGlobalDesc._fastaCreationTime);
+    theGlobalDesc._seqlineLength         = idxfaLenSwap(theGlobalDesc._seqlineLength);
+    theGlobalDesc._seqendlLength         = idxfaLenSwap(theGlobalDesc._seqendlLength);
+    theGlobalDesc._fixedWidth            = u32bitSwap(theGlobalDesc._fixedWidth);
+    theGlobalDesc._squeezedSequences     = u32bitSwap(theGlobalDesc._squeezedSequences);
+  }
+#endif
+
+
   //  Check that the magic number and version are correct and that
   //  the modification time of the fasta file is the same as stored
   //  in the index.
@@ -106,7 +128,7 @@ FastAWrapper::isIndexOnDiskCompatible(u32bit indextype, const char *indexname, b
                 theGlobalDesc._version, (u32bit)FASTA_VERSIONNUMBER);
 
       if (theGlobalDesc._fastaModificationTime != st.st_mtime)
-        fprintf(stderr, "           File age not correct; got %ld, expected %ld.\n",
+        fprintf(stderr, "           File age not correct; got "s64bitFMT", expected %ld.\n",
                 theGlobalDesc._fastaModificationTime, st.st_mtime);
 
       if ((theGlobalDesc._indexType & FASTA_INDEX_MASK) < (indextype & FASTA_INDEX_MASK))
@@ -167,7 +189,6 @@ FastAWrapper::openIndex(u32bit indextype, const char *indexname) {
     exit(1);
   }
 
-
   //
   //  Read the index
   //
@@ -177,6 +198,33 @@ FastAWrapper::openIndex(u32bit indextype, const char *indexname) {
     exit(1);
   }
 
+
+
+
+  //  Fix endianess issues in the index.
+  //
+#if FASTA_VERSIONNUMBER > 4
+  bool swapIndex = _theGlobalDesc._magic == u64bitSwap(FASTA_MAGICNUMBER);
+
+  if (swapIndex) {
+    //fprintf(stderr, "WARNING:  Fixing index for endianess swap!\n");
+
+    _theGlobalDesc._magic                 = u64bitSwap(_theGlobalDesc._magic);
+    _theGlobalDesc._version               = u32bitSwap(_theGlobalDesc._version);
+    _theGlobalDesc._indexType             = u32bitSwap(_theGlobalDesc._indexType);
+    _theGlobalDesc._fastaType             = u32bitSwap(_theGlobalDesc._fastaType);
+    _theGlobalDesc._numberOfSequences     = u32bitSwap(_theGlobalDesc._numberOfSequences);
+    _theGlobalDesc._fastaFileSize         = idxfaPosSwap(_theGlobalDesc._fastaFileSize);
+    _theGlobalDesc._fastaModificationTime = idxfaTimeSwap(_theGlobalDesc._fastaModificationTime);
+    _theGlobalDesc._fastaCreationTime     = idxfaTimeSwap(_theGlobalDesc._fastaCreationTime);
+    _theGlobalDesc._seqlineLength         = idxfaLenSwap(_theGlobalDesc._seqlineLength);
+    _theGlobalDesc._seqendlLength         = idxfaLenSwap(_theGlobalDesc._seqendlLength);
+    _theGlobalDesc._fixedWidth            = u32bitSwap(_theGlobalDesc._fixedWidth);
+    _theGlobalDesc._squeezedSequences     = u32bitSwap(_theGlobalDesc._squeezedSequences);
+  }
+#endif
+
+
  _theSeqs = new _idxfa_desc [ _theGlobalDesc._numberOfSequences ];
 
   read(indexfile, _theSeqs, sizeof(_idxfa_desc) * _theGlobalDesc._numberOfSequences);
@@ -184,6 +232,18 @@ FastAWrapper::openIndex(u32bit indextype, const char *indexname) {
     fprintf(stderr, "FastAWrapper()-- couldn't read sequence descriptions from the index '%s'.\n%s\n", _indexname, strerror(errno));
     exit(1);
   }
+
+#if FASTA_VERSIONNUMBER > 4
+  if (swapIndex) {
+    for (u32bit i=0; i<_theGlobalDesc._numberOfSequences; i++) {
+      _theSeqs[i]._headerStart = idxfaPosSwap(_theSeqs[i]._headerStart);
+      _theSeqs[i]._seqStart    = idxfaPosSwap(_theSeqs[i]._seqStart);
+      _theSeqs[i]._headerLen   = idxfaLenSwap(_theSeqs[i]._headerLen);
+      _theSeqs[i]._seqLen      = idxfaLenSwap(_theSeqs[i]._seqLen);
+    }
+  }
+#endif
+
 
   //  If indextype is FASTA_INDEX_ANY, open the index as reported by
   //  the file.  Otherwise, open the index as specified.
@@ -201,6 +261,11 @@ FastAWrapper::openIndex(u32bit indextype, const char *indexname) {
       fprintf(stderr, "FastAWrapper()-- couldn't read lengths of names from the index '%s'.\n%s\n", _indexname, strerror(errno));
       exit(1);
     }
+
+#if FASTA_VERSIONNUMBER > 4
+  if (swapIndex)
+    _theNamesLen = u32bitSwap(_theNamesLen);
+#endif
 
     _theNames = new char [sizeof(char) * _theNamesLen];
 
@@ -221,6 +286,16 @@ FastAWrapper::openIndex(u32bit indextype, const char *indexname) {
       fprintf(stderr, "FastA::buildIndex() couldn't read checksums from the index '%s'.\n%s\n", _filename, strerror(errno));
       exit(1);
     }
+
+#if FASTA_VERSIONNUMBER > 4
+    if (swapIndex) {
+      for (u32bit i=0; i<_theGlobalDesc._numberOfSequences; i++) {
+        _theMD5s[i].a = u64bitSwap(_theMD5s[i].a);
+        _theMD5s[i].b = u64bitSwap(_theMD5s[i].b);
+        _theMD5s[i].i = u32bitSwap(_theMD5s[i].i);
+      }
+    }
+#endif
   }
 
 
@@ -394,8 +469,8 @@ FastAWrapper::printTextDescription(FILE *out) {
   fprintf(out, "/filename              = %s\n",          _filename);
   fprintf(out, "/numberOfSequences     = "u32bitFMT"\n", _theGlobalDesc._numberOfSequences);
   fprintf(out, "/fastaFileSize         = "u64bitFMT"\n", _theGlobalDesc._fastaFileSize);
-  fprintf(out, "/fastaModificationTime = "u32bitFMT"\n", _theGlobalDesc._fastaModificationTime);
-  fprintf(out, "/fastaCreationTime     = "u32bitFMT"\n", _theGlobalDesc._fastaCreationTime);
+  fprintf(out, "/fastaModificationTime = "s64bitFMT"\n", _theGlobalDesc._fastaModificationTime);
+  fprintf(out, "/fastaCreationTime     = "s64bitFMT"\n", _theGlobalDesc._fastaCreationTime);
   fprintf(out, "/seqlineLength         = "u32bitFMT"\n", _theGlobalDesc._seqlineLength);
   fprintf(out, "/seqendlLength         = "u32bitFMT"\n", _theGlobalDesc._seqendlLength);
   fprintf(out, "/fixedWidth            = %s\n",          _theGlobalDesc._fixedWidth ? "yes" : "no");
