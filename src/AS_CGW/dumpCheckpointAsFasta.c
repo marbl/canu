c48605de71a9b7fd4882f2464de45432f413d1f2
Brian P. Walenz <thebri@gmail.com>
2006-06-06 07:52:37 +0000

    dumpContigFromCkpt and scaffold2fasta are replaced by more flexible and simpler (YMMV) dumpCheckpointAsFasta.

--- src/AS_CGW/dumpCheckpointAsFasta.c
+++ src/AS_CGW/dumpCheckpointAsFasta.c
@@ -38,8 +38,10 @@
 
 void
 usage(char *name) {
-  fprintf(stderr, "usage: %s [-s begin-scaffold] [-e end-scaffold] -c ckpName -n ckpNum -f frgStore -g gkpStore\n",
+  fprintf(stderr, "usage: %s [-S | -C] [-b begin-iid] [-e end-iid] -c ckpName -n ckpNum -f frgStore -g gkpStore\n",
           name);
+  fprintf(stderr, "  -S       dump scaffolds (default)\n");
+  fprintf(stderr, "  -C       dump contigs\n");
   exit(1);
 }
 
@@ -47,6 +49,7 @@ int
 main(int argc, char **argv) {
   int   startScaff   = 0;
   int   endScaff     = CDS_INT32_MAX;
+  int   dumpScaffold = 1;
   int   ckptNum      = 0;
   char *ckptFileName = 0L;
   char *frgStoreName = 0L;
@@ -55,10 +58,14 @@ main(int argc, char **argv) {
   int arg=1;
   while (arg < argc) {
 
-    if        (strcmp(argv[arg], "-s") == 0) {
+    if        (strcmp(argv[arg], "-b") == 0) {
       startScaff = atoi(argv[++arg]);
     } else if (strcmp(argv[arg], "-e") == 0) {
       endScaff = atoi(argv[++arg]);
+    } else if (strcmp(argv[arg], "-S") == 0) {
+      dumpScaffold = 1;
+    } else if (strcmp(argv[arg], "-C") == 0) {
+      dumpScaffold = 0;
     } else if (strcmp(argv[arg], "-n") == 0) {
       ckptNum = atoi(argv[++arg]);
     } else if (strcmp(argv[arg], "-c") == 0) {
@@ -91,17 +98,67 @@ main(int argc, char **argv) {
   fprintf(stderr, "Loading scaffold graph\n");
   ScaffoldGraph = LoadScaffoldGraphFromCheckpoint(GlobalData->File_Name_Prefix, ckptNum, FALSE);
 
+  static VA_TYPE(char)  *contigConsensus = NULL;
+  static VA_TYPE(char)  *contigQuality   = NULL;
+  contigConsensus = CreateVA_char(1024);
+  contigQuality   = CreateVA_char(1024);
+
+  if (dumpScaffold == 0) {
+    char *sequence;
+
+    // GetNumGraphNodes(graph->ContigGraph);
+
+    NodeCGW_T  *contig = GetGraphNode(ScaffoldGraph->ContigGraph, startScaff);
+
+    while ((contig) && (startScaff < endScaff)) {
+      int skip = 0;
+
+      if (contig->flags.bits.isContig != TRUE) {
+        fprintf(stderr, "contig "F_CID" is not a contig.\n", startScaff);
+        skip = 1;
+      }
+      if (contig->flags.bits.isDead == TRUE) {
+        fprintf(stderr, "contig "F_CID" is DEAD!\n", startScaff);
+        skip = 1;
+      }
+
+      if (!skip) {
+        GetConsensus(ScaffoldGraph->ContigGraph, contig->id, contigConsensus, contigQuality);
+        sequence = Getchar(contigConsensus, 0);
+
+        fprintf(stdout, ">contig"F_CID" contig_length=%.f %s%s%s%s%s%s%s%s%s\n", 
+                contig->id,
+                contig->bpLength.mean,
+                (contig->flags.bits.isUnique         != TRUE) ? " notUnique" : "",
+                (contig->flags.bits.isSurrogate      == TRUE) ? " surrogate" : "",
+                (contig->flags.bits.isChaff          == TRUE) ? " chaff" : "",
+                (contig->flags.bits.isMisplaced      == TRUE) ? " misplaced" : "",
+                (contig->flags.bits.isStone          == TRUE) ? " stone" : "",
+                (contig->flags.bits.isWalk           == TRUE) ? " walk" : "",
+                (contig->flags.bits.isRock           == TRUE) ? " rock" : "",
+                (contig->flags.bits.isPotentialRock  == TRUE) ? " potentialrock" : "",
+                (contig->flags.bits.isPotentialStone == TRUE) ? " potentialstone" : "");
+        fwrite(sequence, sizeof(char), strlen(sequence), stdout);
+        fprintf(stdout, "\n");
+      }
+
+      startScaff++;
+      contig = GetGraphNode(ScaffoldGraph->ContigGraph, startScaff);
+    }
+
+    exit(0);
+  }
+
+
+
   //  Get the scaffold we care about
   //
-  NodeCGW_T  *scaff = GetGraphNode( ScaffoldGraph->ScaffoldGraph, startScaff);
+  NodeCGW_T  *scaff = GetGraphNode(ScaffoldGraph->ScaffoldGraph, startScaff);
 
   while ((scaff) && (startScaff < endScaff)) {
-
-    // not interested in dead, not real, or singleton scaffolds
-    //
     int  skip = 0;
 
-    if (isDeadCIScaffoldT(scaff)) {
+    if (scaff->flags.bits.isDead == TRUE) {
       fprintf(stderr, "scaffold " F_CID " is dead\n", scaff->id);
       skip = 1;
     }
@@ -113,20 +170,15 @@ main(int argc, char **argv) {
     if (!skip) {
       CIScaffoldTIterator    CIsTemp;
       ChunkInstanceT        *nextContig      = NULL;
-      static VA_TYPE(char)  *contigConsensus = NULL;
-      static VA_TYPE(char)  *contigQuality   = NULL;
       char                  *sequence;
-
-      contigConsensus = CreateVA_char(1024);
-      contigQuality   = CreateVA_char(1024);
     
       fprintf(stdout, ">scaffold"F_CID" scaffold_length=%.f\n", 
               scaff->id,
               scaff->bpLength.mean);
-    
+
       InitCIScaffoldTIterator(ScaffoldGraph, scaff, TRUE, FALSE, &CIsTemp);
       while (NextCIScaffoldTIterator(&CIsTemp)) {
-        ChunkInstanceT  *contig = GetGraphNode( ScaffoldGraph->ContigGraph, CIsTemp.curr);      
+        ChunkInstanceT  *contig = GetGraphNode(ScaffoldGraph->ContigGraph, CIsTemp.curr);      
 
         assert(contig != NULL);
 
@@ -157,7 +209,7 @@ main(int argc, char **argv) {
     }
 
     startScaff++;
-    scaff = GetGraphNode( ScaffoldGraph->ScaffoldGraph, startScaff);
+    scaff = GetGraphNode(ScaffoldGraph->ScaffoldGraph, startScaff);
   }
 
   return(0);
