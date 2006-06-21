b3629b58881a8df103acec3961c5c0c26c7cf478
Brian P. Walenz <thebri@gmail.com>
2006-06-21 04:36:16 +0000

    Ooof!  Complete rewrite!

--- kmer/leaff/fragmenter.C
+++ kmer/leaff/fragmenter.C
@@ -16,218 +16,160 @@
 //  the N's) will be discarded.
 
 
-//#define DEBUG
+void
+usage(char *name) {
+  fprintf(stderr, "usage: %s [-overlap len] -length len -input X.fasta -output Y.fasta -log T.log\n",
+          name);
+  exit(1);
+}
 
 
 int
 main(int argc, char **argv) {
+  u32bit                desiredLength = 0;
+  u32bit                overlapLength = 0;
+  bool                  beVerbose = false;
+  FastAWrapper         *F = 0L;
+  FastASequenceInCore  *B = 0L;
+  FILE                 *O = 0L;
+  FILE                 *L = 0L;
 
-  if (argc != 5) {
-    fprintf(stderr, "usage: %s <L> <input-file> <out-file> <log-file>\n", argv[0]);
-    exit(1);
+  u32bit                fragmentIndex = 0;
+
+  int arg=1;
+  while (arg < argc) {
+    if        (strcmp(argv[arg], "-length") == 0) {
+      desiredLength = strtou32bit(argv[++arg], 0L);
+    } else if (strcmp(argv[arg], "-overlap") == 0) {
+      overlapLength = strtou32bit(argv[++arg], 0L);
+    } else if (strcmp(argv[arg], "-input") == 0) {
+      F = new FastAWrapper(argv[++arg]);
+    } else if (strcmp(argv[arg], "-output") == 0) {
+      errno = 0;
+      O = fopen(argv[++arg], "w");
+      if (errno)
+        fprintf(stderr, "ERROR:  Can't open output file '%s': %s\n", argv[arg], strerror(errno)), exit(1);
+    } else if (strcmp(argv[arg], "-log") == 0) {
+      errno = 0;
+      L = fopen(argv[++arg], "w");
+      if (errno)
+        fprintf(stderr, "ERROR:  Can't open log file '%s': %s\n", argv[arg], strerror(errno)), exit(1);
+    } else if (strcmp(argv[arg], "-verbose") == 0) {
+      beVerbose = true;
+    } else {
+      usage(argv[arg]);
+    }
+
+    arg++;
   }
 
-  u32bit   desiredLength = strtou32bit(argv[1], 0L);
-  char    *seqFile       = argv[2];
+  if ((F == 0L) || (O == 0L) || (L == 0L))
+    usage(argv[0]);
 
-  FastAWrapper         *F = new FastAWrapper(seqFile);
-  FastASequenceInCore  *B;
-
-  u32bit       fragmentIndex = 0;
-
-  FILE        *O = fopen(argv[3], "w");
-  FILE        *L = fopen(argv[4], "w");
-
-  while (F->eof() == false) {
-    B = F->getSequence();
-
-#ifdef DEBUG
-    fprintf(stderr, "working on %s\n", B->header());
-#endif
+  B = F->getSequence();
+  while (B) {
+    if (beVerbose)
+      fprintf(stderr, "working on %s\n", B->header());
 
     char     *seq = (char *)B->sequence();
 
     u32bit    pos = 0;
-    u32bit    max = 4 * (B->sequenceLength() / desiredLength + 1);
+    u32bit    max = 16384;
     u32bit   *sta = new u32bit [max];
     u32bit   *end = new u32bit [max];
 
+    //  step 1: build a list of regions to output.  Scan the sequence,
+    //  making a new region if we see a significant chunk of N, or if we
+    //  hit the desiredLength.
+    //
+    u32bit s = 0;
+    u32bit e = 0;
+    while (s < B->sequenceLength()) {
 
-    //  step 1: Build a list of regions to output.  we now just make a
-    //  list of all length L segments, regardless of N content.  We'll
-    //  fix them later.
+      //  Skip any N at the start
+      while ((seq[s] == 'n') || (seq[s] == 'N') && (s < B->sequenceLength()))
+        s++;
 
-    for (u32bit s = 0; s < B->sequenceLength() ;) {
-      sta[pos]  = s;
-      s        += desiredLength;
+      //  Construct the preliminary block.
+      //
+      e = s + desiredLength;
+      if (e > B->sequenceLength())
+        e = B->sequenceLength();
 
-      if (s > B->sequenceLength())
-        s = B->sequenceLength();
+      fprintf(stderr, "got block1 "u32bitFMT" - "u32bitFMT"\n", s, e);
 
-      end[pos]  = s;
-      pos++;
+      //  Scan from s to e, looking for significant N.  If we find it,
+      //  reset e and stop.
+      //
+      u32bit  numN = 0;
+      for (u32bit i=s; i<e; i++) {
+        if ((seq[i] == 'n') || (seq[i] == 'N')) {
+          numN++;
+        } else {
+          numN = 0;
+        }
+        if (numN >= 50) {
+          e = i;
+          break;
+        }
+      }
 
-      if (pos >= max) {
-        fprintf(stderr, "pos >= max at line %d\n", __LINE__);
-        exit(1);
+      fprintf(stderr, "got block2 "u32bitFMT" - "u32bitFMT"\n", s, e);
+
+      //  Back up e until we hit the first non-N
+      if ((s < e) && ((seq[e] == 'n') || (seq[e] == 'N'))) {
+        while ((s <= e) && ((seq[e] == 'n') || (seq[e] == 'N')))
+          e--;
+        e++;
+      }
+
+      fprintf(stderr, "got block3 "u32bitFMT" - "u32bitFMT"\n", s, e);
+
+      //  Add this region
+      //
+      if (s > e) {
+        fprintf(stderr, "ERROR!  s>e!  "u32bitFMT" "u32bitFMT"\n", s, e);
+      }
+      if (s != e) {
+        fprintf(stderr, "ADD ["u32bitFMTW(3)"] "u32bitFMTW(9)" "u32bitFMTW(9)" length "u32bitFMTW(9)"\n", pos, s, e, e-s);
+        sta[pos] = s;
+        end[pos] = e;
+        pos++;
+        if (pos >= max) {
+          fprintf(stderr, "ERROR!  max exceeded!\n");
+        }
+      }
+
+      s = e;
+    }
+
+
+    //  If we're supposed to be overlapping, fiddle with the begin position to make it so.
+    //
+    if (overlapLength > 0) {
+      for (u32bit p=1; p<pos; p++) {
+        if (end[p-1] == sta[p]) {
+          sta[p] -= overlapLength;
+          fprintf(stderr, "ADJ ["u32bitFMTW(3)"] "u32bitFMTW(9)" "u32bitFMTW(9)" length "u32bitFMTW(9)"\n",
+                  p, sta[p], end[p], end[p] - sta[p]);
+        }
       }
     }
 
-#ifdef DEBUG
-    fprintf(stderr, "step1: created %d regions\n", pos);
-#endif
 
-    if (pos > 0) {
+    if (beVerbose)
+      fprintf(stderr, "created %d regions\n", pos+1);
 
-    //  step 2: examine each region, trimming N's on the ends
 
     for (u32bit p=0; p<pos; p++) {
-      while ((seq[sta[p]] == 'N') && (sta[p] < end[p]))
-        sta[p]++;
 
-      while ((seq[end[p]] == 'N') && (sta[p] < end[p]))
-        end[p]--;
-    }
-
-    //  step 3: remove empty things
-
-    u32bit p=0;
-    u32bit q=0;
-
-    for (p=0, q=0; p < pos; p++) {
-      if (sta[p] != end[p]) {
-        if (p != q) {
-          sta[q] = sta[p];
-          end[q] = end[p];
-        }
-        q++;
-      }
-    }
-    pos = q;
-
-#ifdef DEBUG
-    fprintf(stderr, "step3: removed empty regions, left with %d regions\n", pos);
+#if 1
+      fprintf(O, "%s begin "u32bitFMT" end "u32bitFMT" length "u32bitFMT"\n",
+              B->header(), sta[p], end[p], end[p] - sta[p]);
+      fwrite(seq+sta[p], sizeof(char), end[p] - sta[p], O);
+      fprintf(O, "\n");
 #endif
 
-    if (pos > 0) {
-
-    //  step 4: split things with "significant" amount N these should
-    //  be things with contained blocks, or low-quality things
-
-    u32bit    pos2 = 0;
-    u32bit   *sta2 = new u32bit [max];
-    u32bit   *end2 = new u32bit [max];
-
-    for (p=0; p<pos; p++) {
-
-      u32bit  numN = 0;
-      for (u32bit s=sta[p]; s<end[p]; s++) {
-        if (seq[s] == 'N')
-          numN++;
-      }
-
-      //  If there are more N than allowed, see if we can split it.
-      //
-      //  (u32bit)(0.1 * (end[p] - sta[p]))
-      //
-      if (numN > 50) {
-        u32bit s = sta[p];
-        u32bit e = end[p] - 1;
-
-        while (seq[s] != 'N')
-          s++;
-
-        while (seq[--e] != 'N')
-          ;
-        e++;
-
-        //  s is on the first N after the start
-        //  e is on the first non-N after the last N
-
-
-        //  We want to save (0...sta[p]) and (e...end[p]) Probably
-        //  don't want to keep the stuff in the middle, but that's not
-        //  clear.
-
-        sta2[pos2] = sta[p];
-        end2[pos2] = s;
-        pos2++;
-
-        sta2[pos2] = e;
-        end2[pos2] = end[p];
-        pos2++;
-
-#if 0
-        fprintf(L, "discard %9d to %9d -- %7d N's (%8.5f%%)\n",
-                s, e, numN, 100.0 * (double)numN / (double)(e-s));
-        fflush(L);
-#endif
-
-      } else {
-        sta2[pos2] = sta[p];
-        end2[pos2] = end[p];
-        pos2++;
-      }
-
-      if (pos2 >= max) {
-        fprintf(stderr, "pos >= max at line %d\n", __LINE__);
-        exit(1);
-      }
-
-    }
-
-#ifdef DEBUG
-    fprintf(stderr, "step4: split/removed significant N regions, left with %d regions\n", pos2);
-#endif
-
-    //  step 5: Merge any adjacent regions that are less than 2*L
-
-    u32bit merged = 0;
-
-    for (p=0, q=1; q<pos2;) {
-      if ((end2[p] == sta2[q]) &&
-          (end2[q] - sta2[p] < 2*desiredLength)) {
-        // merge q into p
-        //
-        merged++;
-        end2[p] = end2[q];
-        q++;
-      } else {
-        // save q on the next position
-        //
-        p++;
-        sta2[p] = sta2[q];
-        end2[p] = end2[q];
-        q++;
-      }
-
-#if 0
-      fprintf(stdout, "p=%5d [%6d-%6d]  q=%5d [%6d-%6d]\n",
-              p, sta2[p], end2[p], q, sta2[q], end2[q]);
-#endif
-    }
-
-    delete [] sta;
-    delete [] end;
-
-    sta = sta2;
-    end = end2;
-    pos = p + 1;
-
-
-#ifdef DEBUG
-    fprintf(stderr, "step5: merged %d pairs of regions, left with %d regions\n", merged, pos);
-#endif
-
-    //  step 6: write regions
-
-    char *buf = new char [desiredLength + desiredLength + 1];
-
-    for (p=0; p<pos; p++) {
-      memcpy(buf, seq + sta[p], sizeof(char) * (end[p] - sta[p]));
-      buf[ end[p] - sta[p] ] = 0;
-      fprintf(O, "%s sta="u32bitFMT" end="u32bitFMT"\n%s\n",
-              B->header(), sta[p], end[p], buf);
       fprintf(L, u32bitFMT" : "u32bitFMT"["u32bitFMT"-"u32bitFMT"]\n",
               fragmentIndex++,
               B->getIID(),
@@ -235,15 +177,11 @@ main(int argc, char **argv) {
               end[p]);
     }
 
-    delete [] buf;
-
-    }
-    }
-
     delete [] sta;
     delete [] end;
 
     delete B;
+    B = F->getSequence();
   }
 
   fclose(L);
