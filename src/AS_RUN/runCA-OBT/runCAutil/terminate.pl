f37abd0d9c5b73ddc42abc4bef5546a1112ca5f6
Brian P. Walenz <thebri@gmail.com>
2006-09-25 22:26:46 +0000

    Include unitig consensus stats in the qc report.

--- src/AS_RUN/runCA-OBT/runCAutil/terminate.pl
+++ src/AS_RUN/runCA-OBT/runCAutil/terminate.pl
@@ -1,5 +1,57 @@
 use strict;
 
+sub summarizeConsensusStatistics ($) {
+    my $dir = shift @_;
+
+    if (! -e "$dir/consensus.stats.summary") {
+        my $NumColumnsInUnitigs           = 0;
+        my $NumGapsInUnitigs              = 0;
+        my $NumRunsOfGapsInUnitigReads    = 0;
+        my $NumColumnsInContigs           = 0;
+        my $NumGapsInContigs              = 0;
+        my $NumRunsOfGapsInContigReads    = 0;
+        my $NumAAMismatches               = 0;
+        my $NumFAMismatches               = 0;
+        my $NumVARRecords                 = 0;
+        my $NumVARStringsWithFlankingGaps = 0;
+
+        open(F, "ls $dir/$asm.cns_contigs.*.err |");
+        my @files = <F>;
+        chomp @files;
+        close(F);
+
+        foreach my $f (@files) {
+            open(F, "< $f");
+            while (<F>) {
+                $NumColumnsInUnitigs += $1           if (m/NumColumnsInUnitigs\s+=\s+(\d+)/);
+                $NumGapsInUnitigs += $1              if (m/NumGapsInUnitigs\s+=\s+(\d+)/);
+                $NumRunsOfGapsInUnitigReads += $1    if (m/NumRunsOfGapsInUnitigReads\s+=\s+(\d+)/);
+                $NumColumnsInContigs += $1           if (m/NumColumnsInContigs\s+=\s+(\d+)/);
+                $NumGapsInContigs += $1              if (m/NumGapsInContigs\s+=\s+(\d+)/);
+                $NumRunsOfGapsInContigReads += $1    if (m/NumRunsOfGapsInContigReads\s+=\s+(\d+)/);
+                $NumAAMismatches += $1               if (m/NumAAMismatches\s+=\s+(\d+)/);
+                $NumFAMismatches += $1               if (m/NumFAMismatches\s+=\s+(\d+)/);
+                $NumVARRecords += $1                 if (m/NumVARRecords\s+=\s+(\d+)/);
+                $NumVARStringsWithFlankingGaps += $1 if (m/NumVARStringsWithFlankingGaps\s+=\s+(\d+)/);
+            }
+            close(F);
+        }
+
+        open(F, "> $dir/consensus.stats.summary");
+        print F "NumColumnsInUnitigs=$NumColumnsInUnitigs\n";
+        print F "NumGapsInUnitigs=$NumGapsInUnitigs\n";
+        print F "NumRunsOfGapsInUnitigReads=$NumRunsOfGapsInUnitigReads\n";
+        print F "NumColumnsInContigs=$NumColumnsInContigs\n";
+        print F "NumGapsInContigs=$NumGapsInContigs\n";
+        print F "NumRunsOfGapsInContigReads=$NumRunsOfGapsInContigReads\n";
+        print F "NumAAMismatches=$NumAAMismatches\n";
+        print F "NumFAMismatches=$NumFAMismatches\n";
+        print F "NumVARRecords=$NumVARRecords\n";
+        print F "NumVARStringsWithFlankingGaps=$NumVARStringsWithFlankingGaps\n";
+        close(F);
+    }
+}
+
 sub terminate ($) {
     my $cgwDir = shift @_;
     $cgwDir = "$wrk/7-CGW" if (!defined($cgwDir));
@@ -88,54 +140,8 @@ sub terminate ($) {
     #  Sum up the consensus stats, so we can tack them onto the end of
     #  the qc report.
     #
-    if (! -e "$wrk/8-consensus/consensus.stats.summary") {
-        my $NumColumnsInUnitigs           = 0;
-        my $NumGapsInUnitigs              = 0;
-        my $NumRunsOfGapsInUnitigReads    = 0;
-        my $NumColumnsInContigs           = 0;
-        my $NumGapsInContigs              = 0;
-        my $NumRunsOfGapsInContigReads    = 0;
-        my $NumAAMismatches               = 0;
-        my $NumFAMismatches               = 0;
-        my $NumVARRecords                 = 0;
-        my $NumVARStringsWithFlankingGaps = 0;
-
-        open(F, "ls $wrk/8-consensus/$asm.cns_contigs.*.err |");
-        my @files = <F>;
-        chomp @files;
-        close(F);
-
-        foreach my $f (@files) {
-            open(F, "< $f");
-            while (<F>) {
-                $NumColumnsInUnitigs += $1           if (m/NumColumnsInUnitigs\s+=\s+(\d+)/);
-                $NumGapsInUnitigs += $1              if (m/NumGapsInUnitigs\s+=\s+(\d+)/);
-                $NumRunsOfGapsInUnitigReads += $1    if (m/NumRunsOfGapsInUnitigReads\s+=\s+(\d+)/);
-                $NumColumnsInContigs += $1           if (m/NumColumnsInContigs\s+=\s+(\d+)/);
-                $NumGapsInContigs += $1              if (m/NumGapsInContigs\s+=\s+(\d+)/);
-                $NumRunsOfGapsInContigReads += $1    if (m/NumRunsOfGapsInContigReads\s+=\s+(\d+)/);
-                $NumAAMismatches += $1               if (m/NumAAMismatches\s+=\s+(\d+)/);
-                $NumFAMismatches += $1               if (m/NumFAMismatches\s+=\s+(\d+)/);
-                $NumVARRecords += $1                 if (m/NumVARRecords\s+=\s+(\d+)/);
-                $NumVARStringsWithFlankingGaps += $1 if (m/NumVARStringsWithFlankingGaps\s+=\s+(\d+)/);
-            }
-            close(F);
-        }
-
-        open(F, "> $wrk/8-consensus/consensus.stats.summary");
-        print F "[Consensus]\n";
-        print F "NumColumnsInUnitigs=$NumColumnsInUnitigs\n";
-        print F "NumGapsInUnitigs=$NumGapsInUnitigs\n";
-        print F "NumRunsOfGapsInUnitigReads=$NumRunsOfGapsInUnitigReads\n";
-        print F "NumColumnsInContigs=$NumColumnsInContigs\n";
-        print F "NumGapsInContigs=$NumGapsInContigs\n";
-        print F "NumRunsOfGapsInContigReads=$NumRunsOfGapsInContigReads\n";
-        print F "NumAAMismatches=$NumAAMismatches\n";
-        print F "NumFAMismatches=$NumFAMismatches\n";
-        print F "NumVARRecords=$NumVARRecords\n";
-        print F "NumVARStringsWithFlankingGaps=$NumVARStringsWithFlankingGaps\n";
-        close(F);
-    }
+    summarizeConsensusStatistics("$wrk/5-consensus");
+    summarizeConsensusStatistics("$wrk/8-consensus");
 
 
     ########################################
@@ -161,8 +167,14 @@ sub terminate ($) {
         }
 
         open(F, ">> $wrk/$asm.qc") or die;
-        print F "\n";
+        print F "\n[Unitig Consensus]\n";
+        open(G, "<  $wrk/5-consensus/consensus.stats.summary") or die;
+        while (<G>) {
+            print F $_;
+        }
+        close(G);
 
+        print F "\n[Contig Consensus]\n";
         open(G, "<  $wrk/8-consensus/consensus.stats.summary") or die;
         while (<G>) {
             print F $_;
