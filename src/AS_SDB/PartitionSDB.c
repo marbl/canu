924cb2aa3ded8260092938363f4c7c28fdfd7f1b
Brian P. Walenz <thebri@gmail.com>
2006-09-27 06:49:59 +0000

    Refactor partitioning of SeqDB/SeqStore.  Add -all flag to send all, even unitigs in single-unitig-contigs, to consensus.

---
+++ src/AS_SDB/PartitionSDB.c
@@ -0,0 +1,338 @@
+
+/**************************************************************************
+ * This file is part of Celera Assembler, a software program that 
+ * assembles whole-genome shotgun reads into contigs and scaffolds.
+ * Copyright (C) 1999-2004, Applera Corporation. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received (LICENSE.txt) a copy of the GNU General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *************************************************************************/
+static char CM_ID[] = "$Id: PartitionSDB.c,v 1.1 2006-09-27 06:49:59 brianwalenz Exp $";
+
+//  PartitionSequenceDB:
+//  
+//  Given a SequenceDB and a partition size, measured in number of
+//  fragments, produce the following:
+//
+//  1) A set of SequenceDBs called partition0-partitionN where:
+//    - partition0 contains all live contigs containing a single
+//      unitig (can grow to arbitrary size) Paritition 0 is really
+//      not very interesting and should be omitted later
+//    - partition1-partitionN contain contigs with >1 unitig, together
+//      will all unitigs and unitig surrogates referenced by the
+//      contig. This may cause a unitig to appear in more than one
+//      partition
+//    - The partition file, UnitigPartition.txt, is the input to
+//      PartitionSequenceDB2.c
+//
+//  2) A fragment partition file specifying for each fragment which
+//  partition (possibly partition 0) its contig belongs.  This file,
+//  FragPartition.txt, is of the form <partitionID> <fragID>
+//
+//  This tool will operate in two phases, first partitioning the
+//  Contigs and generating a fragment partition file, and an in-memory
+//  unitig assignment list.  The second phase will read the unitigs
+//  and partition them according the the assignment list.  Partition 0
+//  contigs/unitigs can be dealt with on the first pass.
+
+//  input is cgw_contigs
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "AS_global.h"
+#include "AS_UTL_Var.h"
+#include "AS_SDB_SequenceDBPartition.h"
+#include "AS_MSG_pmesg.h"
+
+
+FILE *
+openInputFile(char *inputFileName) {
+  FILE *I;
+
+  errno = 0;
+  I = fopen(inputFileName, "r");
+  if (errno)
+    fprintf(stderr, "Can't open inputFileName '%s': %s\n", inputFileName, strerror(errno)), exit(1);
+  return(I);
+}
+
+FILE *
+openOutputPartition(char *inputFileName, int currentPartition) {
+  FILE *O;
+  char  N[1024];
+
+  sprintf(N, "%s.%d", inputFileName, currentPartition);
+  errno = 0;
+  O = fopen(N, "w");
+  if (errno)
+    fprintf(stderr, "Can't open output partition '%s': %s\n", N, strerror(errno)), exit(1);
+  return(O);
+}
+
+
+int
+ComparePartitionElements(const void *p1, const void *p2) {
+  const tPartitionElement *pe1 = (const tPartitionElement *)p1;
+  const tPartitionElement *pe2 = (const tPartitionElement *)p2;
+
+  int diff = pe1->elemID - pe2->elemID;
+  if (diff)
+    return(diff);
+  return(pe1->partitionID - pe2->partitionID);
+}
+
+
+
+int
+main(int argc, char **argv) {
+  char  *storeName         = NULL;
+  int    storeVersion      = 0;
+  int    fragsPerPartition = 0;
+  char  *inputFileName     = NULL;
+  int    includeAllUnitigs = 0;
+
+  int     skippedFragments = 0;
+  int     skippedUnitigs = 0;
+  int     skippedContigs = 0;
+
+  int     totalFragments  = 0;
+  int     totalUnitigs    = 0;
+  int     totalContigs    = 0;
+
+  int     partitionFragments = 0;
+  int     currentPartition = 1;
+
+  VA_TYPE(tPartitionElement) *utgPartitionElems;
+  VA_TYPE(tPartitionElement) *frgPartitionElems;
+
+  int arg=1;
+  while (arg < argc) {
+    if        (strcmp(argv[arg], "-seqstore") == 0) {
+      storeName = argv[++arg];
+    } else if (strcmp(argv[arg], "-version") == 0) {
+      storeVersion = atoi(argv[++arg]);
+    } else if (strcmp(argv[arg], "-fragsper") == 0) {
+      fragsPerPartition = atoi(argv[++arg]);
+    } else if (strcmp(argv[arg], "-input") == 0) {
+      inputFileName     = argv[++arg];
+    } else if (strcmp(argv[arg], "-all") == 0) {
+      includeAllUnitigs = 1;
+    }
+    arg++;
+  }
+
+  if ((storeName == NULL) ||
+      (storeVersion == 0) ||
+      (fragsPerPartition == 0) ||
+      (inputFileName == NULL)) {
+    fprintf(stderr, "usage: %s [opts] -seqstore s.SeqStore -version N -fragsper M -i asm.cgw_contigs\n", argv[0]);
+    fprintf(stderr, "  -all      Include all unitigs, including unitigs in single-unitig-contigs.\n");
+    exit(1);
+  }
+
+  FILE         *inputFile         = openInputFile(inputFileName);
+  MesgReader    reader            = (MesgReader)InputFileType_AS(inputFile);
+  FILE         *outputPartition   = openOutputPartition(inputFileName, currentPartition);
+  tSequenceDB  *sequenceDB        = OpenSequenceDB(storeName, FALSE, storeVersion);
+
+  if (((NumContigsInSequenceDB(sequenceDB) == 0) && (includeAllUnitigs == 0)) ||
+      ((NumUnitigsInSequenceDB(sequenceDB) == 0))) {
+    fprintf(stderr, "Nothing to do: %d unitigs, %d contigs.\n",
+            NumUnitigsInSequenceDB(sequenceDB),
+            NumContigsInSequenceDB(sequenceDB));
+    exit(0);
+  }
+
+  utgPartitionElems = CreateVA_tPartitionElement(NumUnitigsInSequenceDB(sequenceDB) + 100);
+  frgPartitionElems = CreateVA_tPartitionElement(NumUnitigsInSequenceDB(sequenceDB) * 5);
+
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  //  Step 1
+  //
+  GenericMesg  *pmesg;
+
+  while (reader(inputFile, &pmesg) != EOF) {
+    IntConConMesg *ma;
+
+    if (pmesg->t == MESG_ICM) {
+      int nunitigs = ma->num_unitigs;
+      int nfrags   = ma->num_pieces;
+
+      if ((includeAllUnitigs) || (nunitigs > 1)) {
+        int u, f;
+
+        for (u = 0; u < nunitigs; u++) {
+          tPartitionElement partElem;
+          partElem.elemID      = ma->unitigs[u].ident;
+          partElem.partitionID = currentPartition;
+          AppendtPartitionElement(utgPartitionElems, &partElem);
+        }	    
+
+        for (f = 0; f < nfrags; f++) {
+          tPartitionElement partElem;
+          partElem.elemID      = ma->pieces[f].ident;
+          partElem.partitionID = currentPartition;
+          AppendtPartitionElement(frgPartitionElems, &partElem);
+        }
+
+        WriteProtoMesg_AS(outputPartition, pmesg);
+
+        partitionFragments += nfrags;
+
+        totalFragments     += nfrags;
+        totalUnitigs       += nunitigs;
+        totalContigs++;
+
+        if (partitionFragments > fragsPerPartition) {
+          currentPartition++;
+
+          fclose(outputPartition);
+          outputPartition = openOutputPartition(inputFileName, currentPartition);
+
+          partitionFragments = 0;
+        }
+      } else {
+        skippedFragments += nfrags;
+        skippedUnitigs   += nunitigs;
+        skippedContigs++;
+      }
+    }
+  }
+
+  fclose(outputPartition);
+
+  qsort(GettPartitionElement(utgPartitionElems, 0),
+        GetNumtPartitionElements(utgPartitionElems),
+        sizeof(tPartitionElement),
+        ComparePartitionElements);
+  qsort(GettPartitionElement(frgPartitionElems, 0),
+        GetNumtPartitionElements(frgPartitionElems),
+        sizeof(tPartitionElement),
+        ComparePartitionElements);
+
+  {
+    FILE *output = NULL;
+    int i;
+
+    errno = 0;
+    output = fopen("UnitigPartition.txt", "w");
+    if (errno)
+      fprintf(stderr, "Can't open output 'UnitigPartition.txt': %s\n", strerror(errno)), exit(1);
+
+    for (i=0; i<GetNumtPartitionElements(utgPartitionElems); i++) {
+      tPartitionElement *pElem = GettPartitionElement(utgPartitionElems,i);
+      fprintf(output, "%d %d\n", pElem->elemID, pElem->partitionID);
+    }
+
+    errno = 0;
+    output = fopen("FragPartition.txt", "w");
+    if (errno)
+      fprintf(stderr, "Can't open output 'FragPartition.txt': %s\n", strerror(errno)), exit(1);
+
+    for (i=0; i<GetNumtPartitionElements(frgPartitionElems); i++) {
+      tPartitionElement *pElem = GettPartitionElement(frgPartitionElems,i);
+      fprintf(output, "%d %d\n", pElem->partitionID, pElem->elemID);
+    }
+  }
+
+  fprintf(stderr, "Fragments:  %8d skipped   %8d partitioned\n", skippedFragments, totalFragments);
+  fprintf(stderr, "Unitigs:    %8d skipped   %8d partitioned   %8d input\n", skippedUnitigs, totalUnitigs, NumUnitigsInSequenceDB(sequenceDB));
+  fprintf(stderr, "Contigs:    %8d skipped   %8d partitioned   %8d input\n", skippedContigs, totalContigs, NumContigsInSequenceDB(sequenceDB));
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  //  Step 2
+  //
+  int i;
+
+  //MultiAlignT *ma = CreateEmptyMultiAlignT();
+  MultiAlignT *ma = NULL;
+
+  //  We know there are 'currentPartition + 1' partitions, so we can
+  //  allocate all our output files ahead of time.
+  //
+  currentPartition++;
+  FILE               **pOutput = (FILE **)              safe_calloc(sizeof(FILE *),               currentPartition);
+  VA_TYPE(tMARecord) **pIndex  = (VA_TYPE(tMARecord) **)safe_calloc(sizeof(VA_TYPE(tMARecord) *), currentPartition);
+
+  for (i=1; i<currentPartition; i++) {
+    char  N[1024];
+
+    sprintf(N, "%s/seqDB.data.%d.%d", storeName, storeVersion, i);
+
+    errno = 0;
+    pOutput[i] = fopen(N, "w");
+    if (errno)
+      fprintf(stderr, "Can't open SDB partition '%s': %s\n", N, strerror(errno)), exit(1);
+
+    pIndex[i] = CreateVA_tMARecord(100);
+  }
+
+
+  int lastBin    = -1;
+  int lastUnitig = -1;
+
+  for (i=0; i<GetNumtPartitionElements(utgPartitionElems); i++) {
+    tPartitionElement *pElem = GettPartitionElement(utgPartitionElems, i);
+
+    int unitigID = pElem->partitionID;
+    int binID    = pElem->elemID;
+
+    if ((lastBin    == binID) && 
+        (lastUnitig == unitigID)) {
+      fprintf(stderr, "* Attempt to Duplicate unitig "F_CID" in bin "F_CID"...skipping\n", unitigID, binID);
+      continue;
+    }
+
+    assert(binID > 0);
+    assert(binID < currentPartition);
+
+    tMARecord mar;
+
+    mar.flags.all = 0;
+    mar.storeID   = unitigID;
+    mar.offset    = CDS_FTELL(pOutput[binID]);
+
+    AppendtMARecord(pIndex[binID], &mar);
+
+    ReLoadMultiAlignTFromSequenceDB(sequenceDB, ma, unitigID, TRUE);    // This will load the ma from the disk file
+    SaveMultiAlignTToStream(ma, pOutput[binID]);
+
+    lastUnitig = unitigID;
+    lastBin    = binID;
+  }
+
+
+  for (i=1; i<currentPartition; i++) {
+    char N[1024];
+
+    sprintf(N, "%s/seqDB.%d.%d", storeName, storeVersion, i);
+    errno = 0;
+    FILE *F = fopen(N, "w");
+    if (errno)
+      fprintf(stderr, "Can't open SDB partition '%s': %s\n", N, strerror(errno)), exit(1);
+    CopyToFileVA_tMARecord(pIndex[i], F);
+    fclose(F);
+
+    fclose(pOutput[i]);
+  }
+
+
+  return(0);
+}
