116e880027c48e53974e577784a7f48e08d92c85
Brian P. Walenz <thebri@gmail.com>
2006-10-15 08:27:40 +0000

    Remove private copy of AS_UTL_ID_store functions.

--- src/AS_MSG/remove_fragment.c
+++ src/AS_MSG/remove_fragment.c
@@ -18,121 +18,15 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
-/* $Id: remove_fragment.c,v 1.5 2005-09-15 15:20:16 eliv Exp $ */
+/* $Id: remove_fragment.c,v 1.6 2006-10-15 08:27:40 brianwalenz Exp $ */
 
-/*
-  This program, bcp2frg, converts a set of BAC-end batch copy files
-  into one DST, and several FRG and LKG messages written to stdout.
-
-  NOTE: this is likely to be of very temporary use. In the (near?) future
-        BAC end data should be queryable in a database...
-*/
-
-// standard headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-// project headers
 #include "AS_global.h"
-
-typedef struct
-{
-  CDS_UID_t * ids;
-  cds_uint32   num_ids;
-  cds_uint32   size;
-  CDS_UID_t min_id;
-  CDS_UID_t max_id;
-} ID_Array;
-typedef ID_Array * ID_Arrayp;
-
-
-// qsort sorter, with some checking - there should be no dupes
-static int uid_compare( const CDS_UID_t * a, const CDS_UID_t * b )
-{
-  if( *a > *b ) return 1;
-  if( *a == *b )
-  {
-    fprintf( stderr, "Duplicate IDs!: " F_UID "\n", *a );
-    return 0;
-  }
-  return -1;
-}
-
-
-void FreeID_Array( ID_Arrayp array )
-{
-  if( array != NULL )
-  {
-    if( array->ids != NULL )
-      free( array->ids );
-    free( array );
-  }
-}
-
-
-ID_Arrayp AllocateID_Array( cds_uint32 num_ids )
-{
-  ID_Arrayp ret_array;
-  ret_array = (ID_Arrayp) calloc( 1, sizeof( ID_Array ) );
-  if( ret_array == NULL )
-    return NULL;
-  if( num_ids != 0 )
-  {
-    ret_array->ids = (CDS_UID_t *) calloc( num_ids, sizeof( CDS_UID_t ) );
-    if( ret_array->ids == NULL )
-    {
-      FreeID_Array( ret_array );
-      return NULL;
-    }
-    ret_array->size = num_ids;
-  }
-  return ret_array; 
-}
-
-int AppendToID_Array( ID_Arrayp array, CDS_UID_t id, int sort )
-{
-  if( array->num_ids == array->size )
-    return 1;
-  
-  array->ids[array->num_ids] = id;
-  array->num_ids++;
-  array->min_id = min( id, array->min_id );
-  array->max_id = max( id, array->max_id );
-  if( array->num_ids > 1 && sort != 0 )
-    qsort( array->ids, array->num_ids, sizeof( CDS_UID_t ),
-           (int (*)(const void *, const void *)) uid_compare );
-
-  return 0;
-}
-
-
-cds_int64 FindID_ArrayID( ID_Array * ids, CDS_UID_t id )
-{
-  double delta = ids->max_id - id;
-  CDS_UID_t i = (CDS_UID_t) ( (ids->num_ids - 1) *
-    (1.0f - delta / (ids->max_id - ids->min_id)) );
-
-  if( id > ids->ids[ids->num_ids - 1] || id < ids->ids[0] )
-    return -1;
-  
-  i = min( ids->num_ids - 1, i );
-  
-  if( ids->ids[i] > id )
-  {
-    for( ; i > 0 && ids->ids[i] > id; i-- );
-  }
-  else
-  {
-    for( ; i < ids->num_ids && ids->ids[i] < id; i++ );
-  }
-  
-  if( i < ids->num_ids && ids->ids[i] == id )
-    return i;
-
-  return -1;
-}
+#include "AS_UTL_ID_store.h"
 
 CDS_UID_t a2UID( char * string )
 {
@@ -144,7 +38,6 @@ CDS_UID_t a2UID( char * string )
 // main function
 int main( int argc, char ** argv )
 {
-  FILE       * fp_in;
   FILE       * fp_out;
   MesgReader   reader;
   MesgWriter   writer;
@@ -161,14 +54,7 @@ int main( int argc, char ** argv )
     return 1;
   }
 
-  // open files
-  fp_in = fopen( argv[1], "r" );
-  if( fp_in == NULL )
-  {
-    fprintf( stderr, "Failed to open file %s for reading.\n", argv[1] );
-    return 1;
-  }
-  reader = (MesgReader)InputFileType_AS( fp_in );
+  reader = (MesgReader)InputFileType_AS(stdin);
 
   // set up frag_uids array
   if( argv[3][0] != '-' )
@@ -230,7 +116,7 @@ int main( int argc, char ** argv )
   writer =
     (reader == ReadBinaryMesg_AS) ? WriteBinaryMesg_AS : WriteProtoMesg_AS;
   
-  while( reader( fp_in, &gen ) != EOF )
+  while( reader(stdin, &gen ) != EOF )
   {
     switch( gen->t )
     {
@@ -318,7 +204,6 @@ int main( int argc, char ** argv )
     }
   }
 
-  fclose( fp_in );
   fclose( fp_out );
   return 0;
 }
