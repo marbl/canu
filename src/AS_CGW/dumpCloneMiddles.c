16dd87ed3aee570fe9c74a26e388e3579cae967d
Brian P. Walenz <thebri@gmail.com>
2006-12-12 06:28:48 +0000

    Refactor.  If a list of integers is given last on the command line, clone middles for those scaffolds will be dumped.

--- src/AS_CGW/dumpCloneMiddles.c
+++ src/AS_CGW/dumpCloneMiddles.c
@@ -21,76 +21,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <sys/types.h>
 #include <string.h>
-#include <dirent.h>
+#include <fcntl.h>
 #include <sys/stat.h>
-#include <unistd.h>
-
+#include <sys/types.h>
 
 #include "AS_global.h"
-#include "AS_UTL_Var.h"
-#include "AS_UTL_timer.h"
-#include "AS_CGW_dataTypes.h"
-#include "ScaffoldGraph_CGW.h"
-#include "ScaffoldGraphIterator_CGW.h"
-#include "Globals_CGW.h"
-#include "DiagnosticsCGW.h"
-#include "ScaffoldGraph_CGW.h"
-#include "Output_CGW.h"
-#include "GreedyOverlapREZ.h"
-#include "CommonREZ.h"
-#include "RepeatRez.h"
-#include "FbacREZ.h"
-#include "PublicAPI_CNS.h"
-#include "AS_ALN_aligners.h"
-#include "AS_ALN_forcns.h"
-#include "AS_UTL_Var.h"
-#include "AS_UTL_Hash.h"
-#include "OlapStoreOVL.h"
 #include "Instrument_CGW.h"
+
 #define CMDIR "CloneMiddles"
 
 static ScaffoldInstrumenter *si;
 
 extern int do_draw_frags_in_CelamyScaffold;
 extern int do_compute_missing_overlaps;
-extern do_surrogate_tracking;
-extern printMateUIDs;
+extern int do_surrogate_tracking;
+extern int printMateUIDs;
 
-void dumpCloneMiddle(int sid){
-  char camname[1000];
-  FILE *camfile=NULL;
+void
+dumpCloneMiddle(int sid) {
+  char          camname[1000];
+  FILE         *camfile = NULL;
+  struct stat   sb;
+
   CIScaffoldT *scaffold = GetGraphNode(ScaffoldGraph->ScaffoldGraph,sid);
+
   if ((isDeadCIScaffoldT(scaffold)) ||
       (scaffold->type != REAL_SCAFFOLD))
     return;
 
+  sprintf(camname, "%s/scf%d_cm.cam", CMDIR, scaffold->id);
 
-  sprintf(camname,CMDIR "/scf%d_cm.cam",scaffold->id);
+  if (stat(camname, &sb) == 0) {
+    fprintf(stderr, "Dumping clone middle for scaffold %d -- file exists, skipped!\n", sid);
+    return;
+  }
+
+  errno = 0;
   camfile = fopen(camname,"w");
-  assert(camfile!=NULL);
+  if (errno)
+    fprintf(stderr, "Failed to open '%s': %s\n", camname, strerror(errno)), exit(1);
+
+  fprintf(stderr, "Dumping clone middle for scaffold %d\n", sid);
+
   DumpCelamyColors(camfile);
   DumpCelamyMateColors(camfile);
+
   if(do_draw_frags_in_CelamyScaffold)
     DumpCelamyFragColors(camfile);
 
-  //  fprintf(stderr,"pre CelamyScaffold");
-
   CelamyScaffold(camfile,scaffold,0,scaffold->bpLength.mean);
-
-  //  fprintf(stderr,"past CelamyScaffold");
 
   InstrumentScaffold(ScaffoldGraph,
 		     scaffold,
 		     si,
 		     InstrumenterVerbose2,
 		     GlobalData->stderrc);
-
-  //  fprintf(stderr,"past InstrumentScaffold");
 
   PrintScaffoldInstrumenterMateDetails(si,camfile,PRINTCELAMY);
   PrintExternalMateDetailsAndDists(ScaffoldGraph,si->bookkeeping.wExtMates,"\t",camfile,PRINTCELAMY);
@@ -99,20 +85,9 @@ void dumpCloneMiddle(int sid){
   fclose(camfile);
 }
 
-void setup_ovlStore(void){
 
-  ScaffoldGraph->frgOvlStore = New_OVL_Store ();
-  Open_OVL_Store (ScaffoldGraph->frgOvlStore, GlobalData->OVL_Store_Name);
-
-}
-
-void finished_with_ovlStore(void){
-
-  Free_OVL_Store (ScaffoldGraph->frgOvlStore);
-
-}
-
-void usage(char *pgm){
+void
+usage(char *pgm) {
   fprintf(stderr, "usage: %s -f <frgStore> -g <gkpStore> -o <ovlStore> -c <ckpName> -n <ckpNum> [other options]\n", pgm);
   fprintf(stderr, "  META OPTION\n");
   fprintf(stderr, "    -p <prefix>          -- attempt to guess all the required options, if your assembly\n");
@@ -127,130 +102,117 @@ void usage(char *pgm){
   fprintf(stderr, "    -s <single scfIID>   -- generate a single scaffold\n");
   fprintf(stderr, "    -l <min length>      -- generate only scaffolds larger than min length\n");
   fprintf(stderr, "    -U                   -- name clones with the UID of their read\n");
-
 }
 
-int main (int argc , char * argv[] ) {
 
-  Global_CGW *data;
-  char *prefix;
-  int setFragStore = FALSE;
-  int setGatekeeperStore = FALSE;
-  int setOvlStore = FALSE;
-  int setPrefixName = FALSE;
-  int ckptNum = NULLINDEX;
-  int i, index;
-  char subset_map[1000];
-  char full_map[1000];
-  char setSubsetMap=0;
-  char setFullMap=0;
-  char ovlPath[1000];
-  int setFullOvl=0;
+int
+main(int argc, char **argv) {
+  int ckptNum     = NULLINDEX;
+  int sid         = NULLINDEX;
   int specificScf = NULLINDEX;
-  int minLen=0;
+  int minLen      = 0;
+  int arg         = 1;
+  int err         = 0;
+  int firstScfArg = 0;
 
-  GlobalData  = data = CreateGlobal_CGW();
-  data->stderrc = stderr;
-  data->timefp = stderr;
+  GlobalData      = CreateGlobal_CGW();
 
-  setbuf(stdout,NULL);
-
-  { /* Parse the argument list using "man 3 getopt". */ 
-    int ch,errflg=0;
-    optarg = NULL;
-    while (!errflg && ((ch = getopt(argc, argv,"c:f:g:n:s:o:p:l:SU")) != EOF)){
-      switch(ch) {
-        case 'c':
-          strcpy( data->File_Name_Prefix, argv[optind - 1]);
-          setPrefixName = TRUE;
-          break;
-        case 'f':
-          strcpy( data->Frag_Store_Name, argv[optind - 1]);
-          setFragStore = TRUE;
-          break;
-        case 'g':
-          strcpy( data->Gatekeeper_Store_Name, argv[optind - 1]);
-          setGatekeeperStore = TRUE;
-          break;	  
-        case 'l':
-          minLen=atoi(optarg);
-          assert(minLen>0);
-          break;
-        case 'n':
-          ckptNum = atoi(argv[optind - 1]);
-          break;
-        case 'o':
-          strcpy( data->OVL_Store_Name, argv[optind - 1]);
-          setOvlStore = TRUE;
-          break;	  
-        case 'p':
-          ckptNum = SetFileNamePrefix_CGW(data, argv[optind - 1]);
-          setFragStore       = TRUE;
-          setGatekeeperStore = TRUE;
-          setOvlStore        = TRUE;
-          setPrefixName      = TRUE;
-          break;
-        case 's':
-          specificScf = atoi(argv[optind - 1]);
-          break;
-        case 'S':
-          do_surrogate_tracking=0;
-          break;
-        case 'U':
-          printMateUIDs=1;
-          break;
-        case '?':
-          fprintf(stderr,"Unrecognized option -%c",optopt);
-        default :
-          errflg++;
+  while (arg < argc) {
+    if        (strcmp(argv[arg], "-c") == 0) {
+      strcpy(GlobalData->File_Name_Prefix, argv[++arg]);
+    } else if (strcmp(argv[arg], "-f") == 0) {
+      strcpy(GlobalData->Frag_Store_Name, argv[++arg]);
+    } else if (strcmp(argv[arg], "-g") == 0) {
+      strcpy(GlobalData->Gatekeeper_Store_Name, argv[++arg]);
+    } else if (strcmp(argv[arg], "-o") == 0) {
+      strcpy(GlobalData->OVL_Store_Name, argv[++arg]);
+    } else if (strcmp(argv[arg], "-l") == 0) {
+      minLen = atoi(argv[++arg]);
+      if (minLen <= 0) {
+        fprintf(stderr, "error: min length -l must be greater than zero.\n");
+        err = 1;
       }
-    }
-
-    if((setPrefixName == FALSE) || (setFragStore == 0) || (setGatekeeperStore == 0) || ( setOvlStore == 0)){
-      fprintf(stderr,"* argc = %d optind = %d setFragStore = %d setGatekeeperStore = %d\n",
-              argc, optind, setFragStore,setGatekeeperStore);
-
-      usage(argv[0]);
-      exit (-1);
-    }
-  }
-
-  ScaffoldGraph = 
-    LoadScaffoldGraphFromCheckpoint( data->File_Name_Prefix, ckptNum, FALSE);
-  setup_ovlStore();
-
-  {
-    static DIR *camdir=NULL;
-    /* global instrumenter */
-    si = CreateScaffoldInstrumenter(ScaffoldGraph, INST_OPT_ALL);
-    assert(si != NULL);
-  
-    camdir=opendir(CMDIR);
-    if(camdir==NULL){
-      system("mkdir " CMDIR);
-      camdir=opendir(CMDIR);
-      assert(camdir!=NULL);
-    }
-    closedir(camdir);
-
-    do_draw_frags_in_CelamyScaffold=1;
-    do_compute_missing_overlaps=1;
-
-    // over all scfs in graph
-    if(specificScf!=NULLINDEX){
-      dumpCloneMiddle(specificScf);
+    } else if (strcmp(argv[arg], "-n") == 0) {
+      ckptNum = atoi(argv[++arg]);
+      if (ckptNum <= 0) {
+        fprintf(stderr, "error: checkpoint number -n must be greater than zero.\n");
+        err = 1;
+      }
+    } else if (strcmp(argv[arg], "-p") == 0) {
+      ckptNum = SetFileNamePrefix_CGW(GlobalData, argv[++arg]);
+    } else if (strcmp(argv[arg], "-s") == 0) {
+      specificScf = atoi(argv[++arg]);
+    } else if (strcmp(argv[arg], "-S") == 0) {
+      do_surrogate_tracking = 0;
+    } else if (strcmp(argv[arg], "-U") == 0) {
+      printMateUIDs = 1;
     } else {
-      int sid;
-      for (sid = 0; sid < GetNumGraphNodes(ScaffoldGraph->ScaffoldGraph); sid++){
-	if(GetGraphNode(ScaffoldGraph->ScaffoldGraph,sid)->bpLength.mean>=minLen){
-	  dumpCloneMiddle(sid);
-	}
+      if (atoi(argv[arg]) > 0) {
+        firstScfArg = arg;
+        break;
+      } else {
+        err = 1;
       }
     }
-    DestroyScaffoldInstrumenter(si);
+
+    arg++;
   }
 
-  finished_with_ovlStore ();
+  if ((GlobalData->File_Name_Prefix[0]      == 0) ||
+      (GlobalData->Frag_Store_Name[0]       == 0) ||
+      (GlobalData->Gatekeeper_Store_Name[0] == 0) ||
+      (GlobalData->OVL_Store_Name[0]        == 0)) {
+    fprintf(stderr, "At least one of -c, -f, -g, -o not supplied.\n");
+    fprintf(stderr, "'%s'\n", GlobalData->File_Name_Prefix);
+    fprintf(stderr, "'%s'\n", GlobalData->Frag_Store_Name);
+    fprintf(stderr, "'%s'\n", GlobalData->Gatekeeper_Store_Name);
+    fprintf(stderr, "'%s'\n", GlobalData->OVL_Store_Name);
+    err = 1;
+  }
+
+  if (err) {
+    usage(argv[0]);
+    exit(1);
+  }
+
+  errno=0;
+  mkdir(CMDIR, S_IRWXU | S_IRWXG | S_IRWXO);
+
+  ScaffoldGraph = LoadScaffoldGraphFromCheckpoint(GlobalData->File_Name_Prefix, ckptNum, FALSE);
+  ScaffoldGraph->frgOvlStore = New_OVL_Store();
+  Open_OVL_Store(ScaffoldGraph->frgOvlStore, GlobalData->OVL_Store_Name);
+
+  si = CreateScaffoldInstrumenter(ScaffoldGraph, INST_OPT_ALL);
+  if (si == NULL) {
+    fprintf(stderr, "Failed to CreateScaffoldInstrumenter().\n");
+    exit(1);
+  }
+
+  do_draw_frags_in_CelamyScaffold = 1;
+  do_compute_missing_overlaps     = 1;
+
+  // over all scfs in graph
+
+  if        (specificScf != NULLINDEX){
+    dumpCloneMiddle(specificScf);
+  } else if (firstScfArg > 0) {
+    while (firstScfArg < argc) {
+      sid = atoi(argv[firstScfArg]);
+      if (sid > 0)
+        dumpCloneMiddle(atoi(argv[firstScfArg]));
+      else
+        fprintf(stderr, "WARNING: scaffold arg %d '%s' isn't numeric!\n", firstScfArg, argv[firstScfArg]);
+      firstScfArg++;
+    }
+  } else {
+    for (sid = 0; sid < GetNumGraphNodes(ScaffoldGraph->ScaffoldGraph); sid++) {
+      if (GetGraphNode(ScaffoldGraph->ScaffoldGraph,sid)->bpLength.mean>=minLen)
+        dumpCloneMiddle(sid);
+    }
+  }
+
+  DestroyScaffoldInstrumenter(si);
+  Free_OVL_Store (ScaffoldGraph->frgOvlStore);
 
 }
 
