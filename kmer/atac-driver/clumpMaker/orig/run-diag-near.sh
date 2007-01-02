f86d263dc15dc28d6495dc19d19fedcdc37348ed
Brian P. Walenz <thebri@gmail.com>
2007-01-02 12:25:00 +0000

    Original clumpMaker no longer relevant.

--- kmer/atac-driver/clumpMaker/orig/run-diag-near.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#  1) run clumpMaker
-#  2) I expect the prefix of the *.clumps file
-
-awk -f ../clump_diags.awk $1.clumps > $1.diags
-awk -v fudge=200000 -f ../near_clumps.awk < $1.clumps > $1.near
-awk '$14!=-1{inclump+=$7}$14==-1{if($15~/near/){if($12==-1){revnear+=$7}else{fwdnear+=$7}}else{far+=$7}}END{print inclump+revnear+fwdnear+far,inclump,fwdnear,revnear,far}' $1.near
