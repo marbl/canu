3cb00cee7e4bd35dccb8b220c90d10b8f56770ae
Brian P. Walenz <thebri@gmail.com>
2007-03-15 19:29:29 +0000

    Convert either the whole extent of the mapped sequence, or individual exons.

--- kmer/sim4dbutils/convertToExtent.C
+++ kmer/sim4dbutils/convertToExtent.C
@@ -7,25 +7,74 @@
 
 //  Writes polishes from stdin as a one-line-per-match format, space-based!
 
+
+void
+output(sim4polish *p,
+       char       *Ep,
+       char       *Gp,
+       u32bit      a,
+       u32bit      b,
+       bool        isExon) {
+  u32bit  beg = p->exons[a].estFrom - 1;
+  u32bit  end = p->exons[b].estTo;
+
+  if (p->matchOrientation == SIM4_MATCH_COMPLEMENT) {
+    beg = p->estLen - beg;
+    end = p->estLen - end;
+  }
+
+  double  ident = p->exons[a].percentIdentity;
+  double  cover = 0.0;
+
+  //  If we're not a single exon, compute the real identity of the whole thing.
+  //
+  if (isExon == false) {
+    if (p->exons[a].estAlignment) {
+      s4p_percentIdentityExact(p);
+      s4p_percentCoverageExact(p);
+    } else {
+      ident = p->percentIdentity;
+      cover = p->querySeqIdentity;
+    }
+  }
+
+  fprintf(stdout, "%s\t"u32bitFMT"\t"u32bitFMT"\t"u32bitFMT"\t"u32bitFMT"\t%s\t"u32bitFMT"\t"u32bitFMT"\t%6.3f\t%6.3f\n",
+          Ep, p->estLen, a, beg, end,
+          Gp, p->genLo + p->exons[a].genFrom - 1, p->genLo + p->exons[b].genTo,
+          ident, cover);
+}
+
+
 int
 main(int argc, char **argv) {
   bool beVerbose     = false;
   bool wholeEDefLine = false;
   bool wholeGDefLine = false;
+  bool doExons       = false;
 
   int arg = 1;
+  int err = 0;
   while (arg < argc) {
     if        (strncmp(argv[arg], "-v", 2) == 0) {
       beVerbose = true;
-    } else if (strncmp(argv[arg], "-dq", 2) == 0) {
+    } else if (strncmp(argv[arg], "-q", 2) == 0) {
       wholeEDefLine = true;
-    } else if (strncmp(argv[arg], "-dg", 2) == 0) {
+    } else if (strncmp(argv[arg], "-g", 2) == 0) {
       wholeGDefLine = true;
+    } else if (strncmp(argv[arg], "-exons", 2) == 0) {
+      doExons = true;
     } else {
       fprintf(stderr, "Unknown arg '%s'\n", argv[arg]);
+      err++;
     }
     arg++;
   }
+  if (isatty(fileno(stdin)) || (err)) {
+    fprintf(stderr, "usage: %s [-v] [-q] [-g] [-exons]\n", argv[0]);
+    exit(1);
+  }
+
+  fprintf(stdout, "cDNAid\tcDNAlen\texonNum\tbegin\tend\tgenomicid\tbegin\tend\tidentity\tcoverage\n");
 
   char          E[1024], *Ep;
   char          G[1024], *Gp;
@@ -52,26 +101,12 @@ main(int argc, char **argv) {
         Gp = G;
       }
 
-      u32bit  beg = p->exons[0].estFrom - 1;
-      u32bit  end = p->exons[p->numExons-1].estTo;
-      if (p->matchOrientation == SIM4_MATCH_COMPLEMENT) {
-        beg = p->estLen - beg;
-        end = p->estLen - end;
+      if (doExons == false) {
+        output(p, Ep, Gp, 0, p->numExons-1, false);
+      } else {
+        for (u32bit i=0; i<p->numExons; i++)
+          output(p, Ep, Gp, i, i, true);
       }
-
-      if (p->exons[0].estAlignment)
-        fprintf(stdout, "%s\t"u32bitFMT"\t"u32bitFMT"\t"u32bitFMT"\t%s\t"u32bitFMT"\t"u32bitFMT"\t%6.3f\t%6.3f\n",
-                Ep, p->estLen, beg, end,
-                Gp, p->genLo + p->exons[0].genFrom - 1, p->genLo + p->exons[p->numExons-1].genTo,
-                s4p_percentIdentityExact(p),
-                s4p_percentCoverageExact(p));
-      else
-        fprintf(stdout, "%s\t"u32bitFMT"\t"u32bitFMT"\t"u32bitFMT"\t%s\t"u32bitFMT"\t"u32bitFMT"\t"u32bitFMT"\t"u32bitFMT"\n",
-                Ep, p->estLen, beg, end,
-                Gp, p->genLo + p->exons[0].genFrom - 1, p->genLo + p->exons[p->numExons-1].genTo,
-                p->percentIdentity,
-                p->querySeqIdentity);
-
 
       s4p_destroyPolish(p);
     }
