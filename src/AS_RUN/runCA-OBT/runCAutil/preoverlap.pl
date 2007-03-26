c984fce55d852691cd96a26872981541407ab68b
Brian P. Walenz <thebri@gmail.com>
2007-03-26 10:13:31 +0000

    Remove a goto, only write .err file.

--- src/AS_RUN/runCA-OBT/runCAutil/preoverlap.pl
+++ src/AS_RUN/runCA-OBT/runCAutil/preoverlap.pl
@@ -9,46 +9,45 @@ sub preoverlap {
     #  Return if there are fragments in the store, and die if there
     #  are no fragments and no source files.
     #
-    goto alldone if ($numFrags > 0);
+    if ($numFrags <= 0) {
 
-    die "ERROR: No fragment files specified, and stores not already created.\n" if (scalar(@fragFiles) == 0);
+        die "ERROR: No fragment files specified, and stores not already created.\n" if (scalar(@fragFiles) == 0);
 
-    system("mkdir $wrk/0-preoverlap") if (! -d "$wrk/0-preoverlap");
+        system("mkdir $wrk/0-preoverlap") if (! -d "$wrk/0-preoverlap");
 
-    if ((! -d "$wrk/$asm.gkpStore") || (! -e "$wrk/$asm.gkpStore/frg")) {
+        if ((! -d "$wrk/$asm.gkpStore") || (! -e "$wrk/$asm.gkpStore/frg")) {
 
-        #  Make sure all the inputs are here
-        #
-        my $failedFiles = 0;
-        my $gkpInput = "";
-        foreach my $frg (@fragFiles) {
-            if (! -e $frg) {
-                print STDERR "MISSING: $frg\n";
-                $failedFiles++;
+            #  Make sure all the inputs are here
+            #
+            my $failedFiles = 0;
+            my $gkpInput = "";
+            foreach my $frg (@fragFiles) {
+                if (! -e $frg) {
+                    print STDERR "MISSING: $frg\n";
+                    $failedFiles++;
+                }
+                $gkpInput .= " $frg";
             }
-            $gkpInput .= " $frg";
-        }
-        die if ($failedFiles);
+            die if ($failedFiles);
 
-        my $cmd;
-        $cmd  = "$bin/gatekeeper -e 10000000 ";
-        $cmd .= "-Q -T " if (getGlobal("doOverlapTrimming"));
-        $cmd .= "-o $wrk/$asm.gkpStore ";
-        $cmd .= "$gkpInput ";
-        $cmd .= "> $wrk/0-preoverlap/gatekeeper.out ";
-        $cmd .= "2> $wrk/0-preoverlap/gatekeeper.err";
+            my $cmd;
+            $cmd  = "$bin/gatekeeper -e 10000000 ";
+            $cmd .= "-Q -T " if (getGlobal("doOverlapTrimming"));
+            $cmd .= "-o $wrk/$asm.gkpStore ";
+            $cmd .= "$gkpInput ";
+            $cmd .= "> $wrk/0-preoverlap/gatekeeper.err 2>&1";
 
-        if (runCommand("$wrk/0-preoverlap", $cmd)) {
-            print STDERR "Failed.\n";
-            rename "$wrk/0-preoverlap/$asm.inp", "$wrk/0-preoverlap/$asm.inp.FAILED";
-            rename "$wrk/$asm.gkpStore", "$wrk/$asm.gkpStore.FAILED";
-            exit(1);
+            if (runCommand("$wrk/0-preoverlap", $cmd)) {
+                print STDERR "Failed.\n";
+                rename "$wrk/0-preoverlap/$asm.inp", "$wrk/0-preoverlap/$asm.inp.FAILED";
+                rename "$wrk/$asm.gkpStore", "$wrk/$asm.gkpStore.FAILED";
+                exit(1);
+            }
         }
+
+        $numFrags = getNumberOfFragsInStore($bin, $wrk, $asm);
     }
 
-    $numFrags = getNumberOfFragsInStore($bin, $wrk, $asm);
-
-alldone:
     stopAfter("initialStoreBuilding");
 }
 
