28a6d51f7b1201b4f3dc249591786657820cd66a
Brian P. Walenz <thebri@gmail.com>
2007-08-02 18:07:35 +0000

    Alert Reader Eli noticed that overlapStore usage was broken w.r.t. -d.

--- src/AS_OVS/overlapStore.c
+++ src/AS_OVS/overlapStore.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
 
-static char CM_ID[] = "$Id: overlapStore.c,v 1.10 2007-07-23 08:40:09 brianwalenz Exp $";
+static char CM_ID[] = "$Id: overlapStore.c,v 1.11 2007-08-02 18:07:35 brianwalenz Exp $";
 
 #include "overlapStore.h"
 
@@ -158,7 +158,7 @@ main(int argc, char **argv) {
     fprintf(stderr, "\n");
     exit(1);
   }
-  if (fileListLen == 0) {
+  if ((fileListLen == 0) && (operation == OP_BUILD)) {
     fprintf(stderr, "No input files?\n");
     exit(1);
   }
