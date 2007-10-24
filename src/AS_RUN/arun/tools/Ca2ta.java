7259ba43466d4d333bd067e090bd6eb165ed7759
M Oweis <invalid@invalid.org>
2007-10-24 15:12:02 +0000

    Reads in most of the data (asm,frg)

--- src/AS_RUN/arun/tools/Ca2ta.java
+++ src/AS_RUN/arun/tools/Ca2ta.java
@@ -1,19 +1,65 @@
 import java.io.*;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.*;
+import java.lang.reflect.Field;
 
-class Contig {
+class ToString {
+	/**
+	* Intended only for debugging.
+	*
+	* <P>Here, a generic implementation uses reflection to print
+	* names and values of all fields <em>declared in this class</em>. Note that
+	* superclass fields are left out of this implementation.
+	*
+	* <p>The format of the presentation could be standardized by using
+	* a MessageFormat object with a standard pattern.
+	*/
+	public String toString() {
+	  StringBuilder result = new StringBuilder();
+	  String newLine = System.getProperty("line.separator");
+
+	  result.append( this.getClass().getName() );
+	  result.append( "$ Object {" );
+	  result.append(newLine);
+
+	  //determine fields declared in this class only (no fields of superclass)
+	  Field[] fields = this.getClass().getDeclaredFields();
+
+	  //print field names paired with their values
+	  for ( Field field : fields  ) {
+	    result.append("  ");
+	    try {
+              result.append( field.getName() );
+              result.append(": ");
+              //requires access to private field:
+              result.append( field.get(this) );
+	    }
+	    catch ( IllegalAccessException ex ) {
+              System.out.println(ex);
+	    }
+	    result.append(newLine);
+	  }
+	  result.append("}");
+
+	  return result.toString();
+	}
+
+
+}
+
+class FRG extends ToString {
+	String mid;
+	String nm;
+	String seq;
+}
+
+class Contig extends ToString {
 	int len;
 	int npc;
 	String cns;
 }
 
-class MPS {
+class MPS extends ToString {
 	String typ;
 	String mid;
 	int lpos;
@@ -29,6 +75,7 @@ public class Ca2ta {
 	
 	static Map<String,String> clrHash;
 	static Map<String,Contig> ccoHash;
+	static Map<String,FRG> frgHash;
 	static Map<String,ArrayList<MPS>> ccoMPSHash;
 	static String prefix;
 
@@ -38,8 +85,13 @@ public class Ca2ta {
 		
 		//Convert input .asm into .clv and .cco files		
 		//Step 1: Read prefix.clv file
-		if (false) 
-			readClr();
+		System.out.println("readCLR\n");
+		readClr();
+			
+		//Step 2: Read the FRG file
+		System.out.println("readFRG\n");
+		readFrg();
+		
 		
 		//Step 2: Read the noAFG file
 		System.out.println("readCCO\n");
@@ -49,6 +101,49 @@ public class Ca2ta {
 		System.out.println("printContig\n");
 		printContig();
 
+   }
+   
+   static void readFrg() {
+	try {
+		FileReader input = new FileReader(prefix.concat(".frg"));
+
+		BufferedReader bufRead = new BufferedReader(input);
+   		String line = bufRead.readLine();      
+
+        	int count = 0;
+
+		frgHash = new HashMap<String,FRG>();
+		while ( line != null ) {
+			Matcher m = bracket.matcher(line);
+			String recName = "";
+			if ( m.lookingAt() ) {
+				recName = m.group(1);
+			}
+			int brackets = 0;
+			if (recName.equals("FRG") ) { 
+				FRG frg = processFRG(bufRead);
+				if ( frg != null )  {
+					frgHash.put(frg.mid,frg);
+					count++;
+			   	        if ( count % 100000 == 0 ) System.out.println("FRG: " + count);
+				}
+			} else {
+				skipMsg(bufRead);
+			}
+			line = bufRead.readLine();
+		}
+
+		System.out.println("FRG Final Read: " + count);
+		System.out.println("FRG Hash size: " + frgHash.size());
+		bufRead.close();              	
+	}catch (ArrayIndexOutOfBoundsException e){
+		System.out.println("Usage: java ReadFile filename\n");
+		e.printStackTrace();
+	}catch (IOException e){
+		e.printStackTrace();
+	}catch (Exception e){
+		e.printStackTrace();
+	}
    }
    
    static void readClr() {
@@ -69,10 +164,11 @@ public class Ca2ta {
 		  clrHash.put(values[0],values[1].concat("\t").concat(values[2]));
 		  line = bufRead.readLine();
 		  count++;
+		  if ( count % 100000 == 0 ) System.out.println("CLR: " + count);
 
 		}
-		System.out.println("Final Read: " + count);
-		System.out.println("Hash size: " + clrHash.size());
+		System.out.println("CLR Final Read: " + count);
+		System.out.println("CLR Hash size: " + clrHash.size());
 		bufRead.close();              
 	}catch (ArrayIndexOutOfBoundsException e){
 		System.out.println("Usage: java ReadFile filename\n");
@@ -90,7 +186,7 @@ public class Ca2ta {
 			ccoMPSHash = new HashMap<String,ArrayList<MPS>>();
 			getCARecord(bufRead);	
 			bufRead.close();              
-			System.out.println("Final hash size: " + ccoHash.size());
+			System.out.println("Final CCO hash size: " + ccoHash.size());
 		}catch (ArrayIndexOutOfBoundsException e){
 			System.out.println("Usage: java ReadFile filename\n");
 			e.printStackTrace();
@@ -116,11 +212,9 @@ public class Ca2ta {
 			e.printStackTrace();
 		}
    }   
-
+   
    static void getCARecord ( BufferedReader bufRead ) throws Exception {
    	String line = bufRead.readLine();
-
-        int i = 0;
 
 	while ( line != null ) {
 		Matcher m = bracket.matcher(line);
@@ -135,10 +229,33 @@ public class Ca2ta {
 		} else {
 			skipMsg(bufRead);
 		}
-		i++;		
-
 		line = bufRead.readLine();
 	}        
+   }
+   
+   static FRG processFRG ( BufferedReader bufRead ) throws Exception {
+   
+   	String line = bufRead.readLine();
+	FRG frg = new FRG();
+
+	while ( line != null && !line.equals("}")) {
+		Matcher fieldMatch = field.matcher(line);
+		String fieldName = "";
+		String fieldValue;
+		if ( fieldMatch.lookingAt() )  {
+			fieldName = fieldMatch.group(1);
+			fieldValue = fieldMatch.group(2);			
+			if ( fieldName.equals("acc") ) {
+				frg.mid = fieldValue;
+			} else if ( fieldName.equals("src") ) {
+				frg.nm = bufRead.readLine();
+			} else if ( fieldName.equals("seq") ) {			
+				frg.seq = readSequence( bufRead );
+			}
+		} 
+		line = bufRead.readLine();
+	}
+	return frg;   
    }
    
    static void processContig( BufferedReader bufRead ) throws Exception {
@@ -161,7 +278,10 @@ public class Ca2ta {
 				skipMsg(bufRead);
 			}
 			else if ( recName.equals("MPS") ) {
-				mpsList.add(readMPS(bufRead));
+				MPS returnedMPS = readMPS(bufRead);
+				if ( returnedMPS != null ) {
+					mpsList.add(returnedMPS);
+				}
 			}
 		else if ( fieldMatch.lookingAt() )  {
 			fieldName = fieldMatch.group(1);
@@ -181,9 +301,49 @@ public class Ca2ta {
    }
    
    static MPS readMPS ( BufferedReader bufRead) throws Exception {
+   	String line = bufRead.readLine();
+	
    	MPS mps = new MPS();
-	
-	
+	while ( line != null && !line.equals("}")) {
+		Matcher fieldMatch = field.matcher(line);
+		String fieldName = "";
+		String fieldValue;
+		if ( fieldMatch.lookingAt() )  {
+			fieldName = fieldMatch.group(1);
+			fieldValue = fieldMatch.group(2);			
+			if ( fieldName.equals("typ") ) {
+				if ( !fieldValue.equals("R") ) {
+					return null;
+				}
+				mps.typ = fieldValue;
+			} else if ( fieldName.equals("mid") ) {
+				mps.mid = fieldValue;
+			} else if ( fieldName.equals("pos") ) {			
+				String [] posArray = fieldValue.split(",");
+				mps.lpos = Integer.parseInt(posArray[0]);
+				mps.rpos = Integer.parseInt(posArray[1]);
+			} else if ( fieldName.equals("dln") ) {
+				mps.dln = Integer.parseInt(fieldValue);
+			} else if ( fieldName.equals("del") ) {
+				line = bufRead.readLine();
+				if ( line.equals("}") )  {
+					return null;
+				}
+				String delStr = new String();
+				while ( line != null && !line.equals("}") ) {
+					delStr = delStr.concat(line).concat(" ");
+					line = bufRead.readLine();
+				}
+				String [] delStrArray = delStr.split(" ");
+				mps.del = new int[delStrArray.length];
+				for( int i= 0; i < delStrArray.length ; i++ )
+					mps.del[i] = Integer.parseInt(delStrArray[i]);
+				if ( line.equals("}") )
+					break;
+			}
+		} 
+		line = bufRead.readLine();
+	}
 	return mps;
    }
    
@@ -223,6 +383,8 @@ public class Ca2ta {
 		Contig contig = ccoHash.get(contigId);
 		bufWrite.write("##" + contigId + " " + contig.npc + " " + contig.cns.length() + " bases, 00000000 checksum.\n");
 		printSeq(bufWrite,contig.cns);
+		for ( MPS mps : ccoMPSHash.get(contigId) )
+			bufWrite.write(mps.toString());
 	}
    }
    
