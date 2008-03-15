dcbf80edf9f4d49845e15525a63ec9fc1b6f2fd3
Brian P. Walenz <thebri@gmail.com>
2008-03-15 05:30:35 +0000

    If pathMap isn't given, make or find a default.

--- src/AS_RUN/runCA-OBT/runCAutil/site-bri.pl
+++ src/AS_RUN/runCA-OBT/runCAutil/site-bri.pl
@@ -9,10 +9,12 @@ sub localDefaults() {
     #  occurs, and we don't know where the assembly directory is, so
     #  we can't make the pathMap there.
 
-    my $siteName = "/home/work/w/wgs/siteName";
+    if (!defined(getGlobal("pathMap")) &&
+        (-d "/home/work/w/wgs/") &&
+        (! -e "/home/work/w/wgs/pathMap")) {
+        setGlobal("pathMap", "/home/work/w/wgs/pathMap");
 
-    if (! -e $siteName) {
-        open(F, "> $siteName") or die;
+        open(F, "> /home/work/w/wgs/pathMap") or die;
         print F "twobyfour.home  /home/work/w/wgs/FreeBSD-6.2-RELEASE-amd64/bin\n";
         print F "node5.home      /home/work/w/wgs/FreeBSD-7.0-RELEASE-amd64/bin\n";
         print F "node6.home      /home/work/w/wgs/FreeBSD-7.0-RELEASE-amd64/bin\n";
@@ -20,8 +22,6 @@ sub localDefaults() {
         print F "node8.home      /home/work/w/wgs/FreeBSD-7.0-RELEASE-amd64/bin\n";
         close(F);
     }
-
-    setGlobal("pathMap", $siteName);
 }
 
 sub localOption ($@) {
