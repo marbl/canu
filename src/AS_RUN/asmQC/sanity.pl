5460d729d243d0349cc1a644fad39cf8a3ea1e8a
Brian P. Walenz <thebri@gmail.com>
2008-04-09 13:00:09 +0000

    Significant rewrite.  Checks out, generates change log, builds.  Doesn't assemble anything.

--- src/AS_RUN/asmQC/sanity.pl
+++ src/AS_RUN/asmQC/sanity.pl
@@ -3,132 +3,258 @@
 use strict;
 use Config;  #  for @signame
 
-my %logs;
-my $file;
-my $revs;
-my $date;
-my $auth;
-my $inlog = 0;
-my $log;
-my %logdate;
-
-my $daten   = `date -u`;
-my $datep   = `tail -1 update-history`;
-
-chomp $daten;
-chomp $datep;
-
-my $message = "log-$daten";
-
-my $srcdir  = "src";
-
-
-#  Check out the source if we don't have it.
+#  The only two globals:
 #
-if (! -d $srcdir) {
-    #  Anonymous cvs doesn't work for this -- there is apparently a lag behing reality.
-    #runCommand(".", "cvs -z3 -d:pserver:anonymous\@wgs-assembler.cvs.sourceforge.net:/cvsroot/wgs-assembler co -P src");
-    #runCommand(".", "cvs co -P test");
+my $wrkdir = "/home/work/nightly";
+my $cvsdir = "/home/work/nightly/wgs-assembler-cvs";
+
+
+#  Command line options are
+#
+#    'oper'   -- thing to do.
+#
+#    'ddir' -- properly formatted directory name to do it in.  op ==
+#    checkout creates this directory.  If still supplied, it will
+#    force a checkout from that date.
+
+{
+    my $oper   = shift @ARGV;
+    my $ddir   = shift @ARGV;
+
+    my ($thisdate, $lastdate) = parseDate($ddir);
+
+    if ($oper eq "rsync") {
+        print STDERR "NOT RSYNCing UNTIL DEVELOPMENT FINISHED.\n";
+        #runCommand($cvsdir, "rsync -av rsync://wgs-assembler.cvs.sourceforge.net/cvsroot/wgs-assembler/\* .");
+    }
+
+    if ($oper eq "checkout") {
+        checkoutAndLog($thisdate, $lastdate);
+    }
+
+    if ($oper eq "build") {
+        build($thisdate);
+    }
+
+    if ($oper eq "daily") {
+        daily($thisdate);
+    }
+
+    if ($oper eq "weekly") {
+        weekly($thisdate);
+    }
+
+    if ($oper eq "monthly") {
+        monthly($thisdate);
+    }
+
+    if (!defined($oper)) {
+    }
+
+    exit;
 }
 
 
-#  Build logs
-#
-runCommand($srcdir, "cvs -z3 log -N -S -d '$datep<now' > \"../$message\"");
+sub parseDate ($) {
+    my $pathdate = shift @_;
+    my $thisdate;
+    my $lastdate;
 
-open(F, "< $message");
-while (<F>) {
-    if (m/^==========.*========$/) {
-        $inlog = 0;
-        if (!defined($logs{$log})) {
-            $logs{$log}  = "$date\t$revs\t$auth\t$file\n";
+    #  Until our Makefile is fixed, the ":" cannot be used, and so we need
+    #  to adopt a slightly goofy timestamp name.
+
+    if (defined($pathdate)) {
+        if ($pathdate =~ m/^\d\d\d\d-\d\d-\d\d-\d\d\d\d$/) {
+            $thisdate = $pathdate;
         } else {
-            $logs{$log} .= "$date\t$revs\t$auth\t$file\n";
+            die "Malformed ddir '$pathdate'\n";
         }
-        $logdate{"$date\0$log"}++;
-        undef $log;
+    } else {
+        my @v = localtime;
+        $v[5] += 1900;
+        $v[4]++;
+
+        $v[5] = substr("0000$v[5]", -4);
+        $v[4] = substr("0000$v[4]", -2);
+        $v[3] = substr("0000$v[3]", -2);
+        $v[2] = substr("0000$v[2]", -2);
+        $v[1] = substr("0000$v[1]", -2);
+
+        $thisdate = "$v[5]-$v[4]-$v[3]-$v[2]$v[1]";
     }
-    if (m/^----------------------------$/) {
-        $inlog = 0;
-        if (!defined($logs{$log})) {
-            $logs{$log}  = "$date\t$revs\t$auth\t$file\n";
+
+    open(F, "ls $wrkdir |");
+    while (<F>) {
+        chomp;
+        if (m/^\d\d\d\d-\d\d-\d\d-\d\d\d\d$/) {
+            $lastdate = $_;
+        }
+    }
+    close(F);
+
+    print STDERR "Working on '$thisdate' -- last found is '$lastdate'.\n";
+
+    return($thisdate, $lastdate);
+}
+
+
+sub checkoutAndLog ($$) {
+    my $thisdate = shift @_;
+    my $lastdate = shift @_;
+
+    print STDERR "Checking out $wrkdir/$thisdate\n";
+
+    if (-d "$wrkdir/$thisdate") {
+        print STDERR "$wrkdir/$thisdate already exists.  Please remove to rerun.\n";
+        return;
+    }
+
+    runCommand($wrkdir, "mkdir $wrkdir/$thisdate");
+    runCommand($wrkdir, "mkdir $wrkdir/$thisdate/wgs");
+
+
+    #  Convert time-names to dates that cvs can use.
+
+    my $thisdatecvs;
+    my $lastdatecvs;
+
+    my $tz = "EDT";
+
+    if ($thisdate =~ m/(\d\d\d\d)-(\d\d)-(\d\d)-(\d\d)(\d\d)/) {
+        $thisdatecvs = "$1-$2-$3 $4$5 $tz";
+    } else {
+    }
+
+    if (defined($lastdate)) {
+        if ($lastdate =~ m/(\d\d\d\d)-(\d\d)-(\d\d)-(\d\d)(\d\d)/) {
+            $lastdatecvs = "$1-$2-$3 $4$5 $tz";
         } else {
-            $logs{$log} .= "$date\t$revs\t$auth\t$file\n";
         }
-        $logdate{"$date\0$log"}++;
-        undef $log;
     }
-    if ($inlog) {
-        $log .= $_;
+
+    runCommand("$wrkdir/$thisdate/wgs", "cvs -r -R -d $cvsdir -z3 co  -N    -D '$thisdatecvs' src > checkout.err 2>&1");
+    runCommand("$wrkdir/$thisdate/wgs", "cvs -r -R -d $cvsdir -z3 log -N -S -d '$lastdatecvs<$thisdatecvs' src > updates-raw");
+
+    my $log;
+    my $revs;
+    my $date;
+    my $auth;
+    my $chng;
+    my $file;
+    my $inlog;
+    my %logs;
+    my %logdate;
+
+    open(F, "< $wrkdir/$thisdate/wgs/updates-raw");
+    while (<F>) {
+        if (m/^==========.*========$/) {
+            $inlog = 0;
+            if (!defined($logs{$log})) {
+                $logs{$log}  = "$date\t$revs\t$chng\t$auth\t$file\n";
+            } else {
+                $logs{$log} .= "$date\t$revs\t$chng\t$auth\t$file\n";
+            }
+            $logdate{"$date\0$log"}++;
+            undef $log;
+        }
+        if (m/^----------------------------$/) {
+            $inlog = 0;
+            if (!defined($logs{$log})) {
+                $logs{$log}  = "$date\t$revs\t$chng\t$auth\t$file\n";
+            } else {
+                $logs{$log} .= "$date\t$revs\t$chng\t$auth\t$file\n";
+            }
+            $logdate{"$date\0$log"}++;
+            undef $log;
+        }
+        if ($inlog) {
+            $log .= $_;
+        }
+        if (m/^RCS\s+file:\s+(.*),v/) {
+            $file = $1;
+            $file =~ s!$cvsdir/src/!!g;
+        }
+        if (m/^revision\s+(\d+.\d+)/) {
+            $revs = $1;
+        }
+        if (m/^date:\s+(.*);\s+author:\s+(.*);\s+state.*lines:\s+(.*)\s*$/) {
+            $date = $1;
+            $auth = $2;
+            $chng = $3;
+            $inlog = 1;
+        }
     }
-    if (m/^RCS\s+file:\s+(.*),v/) {
-        $file = $1;
+    close(F);
+
+
+    open(F, "> $wrkdir/$thisdate/wgs/updates");
+    my @keys = sort keys %logdate;
+    foreach my $l (@keys) {
+        my ($d, $l) = split '\0', $l;
+    
+        if ((defined($logs{$l})) && (length($logs{$l} > 0))) {
+            print F "----------------------------------------\n";
+            print F "$logs{$l}\n";
+            print F "$l\n";
+            undef $logs{$l};
+        }
     }
-    if (m/^revision\s+(\d+.\d+)/) {
-        $revs = $1;
-    }
-    if (m/^date:\s+(.*);\s+author:\s+(.*);\s+state/) {
-        $date = $1;
-        $auth = $2;
-        $inlog = 1;
-    }
+    close(F);
+
+    print STDERR "$thisdate checked out!\n";
 }
-close(F);
 
 
-#  Get outta here if there are no updates
-#
-if (scalar(keys %logdate) == 0) {
-    unlink $message;
-    exit(0);
-}
+sub build ($) {
+    my $thisdate = shift @_;
 
+    print STDERR "Building $wrkdir/$thisdate\n";
 
-#  Update our chatter.
-#
-open(F, ">> update-history");
-print F "$daten\n";
-close(F);
-
-
-#  Report what changed.
-#
-open(F, "> $message");
-my @keys = sort keys %logdate;
-foreach my $l (@keys) {
-    my ($d, $l) = split '\0', $l;
-
-    if ((defined($logs{$l})) && (length($logs{$l} > 0))) {
-        print F "----------------------------------------\n";
-        print F "$logs{$l}\n";
-        print F "$l\n";
-        undef $logs{$l};
+    if (-e "$wrkdir/$thisdate/wgs/src/make.err") {
+        print STDERR "$wrkdir/$thisdate was already built once (successuflly or not).  Please cleanup first.\n";
+        return;
     }
-}
-close(F);
 
+    runCommand("$wrkdir/$thisdate/wgs/src", "gmake > make.out.raw 2> make.err.raw");
 
-#  Update the source
-#
-runCommand($srcdir, "cvs update -A -D '$daten' -d");
+    my %lines;
 
-
-#  Clean and build it.
-#
-runCommand(".", "rm -rf FreeBSD* Darwin* Linux*");
-runCommand($srcdir, "gmake > /dev/null 2>> \"../$message\"");
-
-
-#  Run our sanity checks.
-#
-system("rm -rf f1 f1.out f2 f2.out");
-
-if (runCommand(".", "perl */bin/runCA-OBT.pl -p f1 -d f1 fakeUIDs=1 vectorIntersect=HUREF-D-01-1P5-3KB.vector HUREF-D-01-1P5-3KB.version1.frg > f1.out 2>&1")) {
-    system("cat $message f1.out");
+    open(F, "< $wrkdir/$thisdate/wgs/src/make.err.raw");
+    open(G, "> $wrkdir/$thisdate/wgs/src/make.err");
+    while (<F>) {
+        chomp;
+        next if (m/^ar:\s+creating/);
+        if (!exists($lines{$_})) {
+            $lines{$_}++;
+            print G "$_\n";
+        }
+    }
+    close(F);
+    close(G);
 }
 
-if (runCommand(".", "perl */bin/runCA-OBT.pl -p f2 -d f2 fakeUIDs=1 HUREF-D-02-1P5-3KB.version2.frg > f2.out 2>&1")) {
-    system("cat $message f2.out");
+
+
+sub daily ($) {
+    my $thisdate = shift @_;
+
+    print STDERR "Submitting daily for $wrkdir/$thisdate\n";
+}
+
+
+
+sub weekly ($) {
+    my $thisdate = shift @_;
+
+    print STDERR "Submitting weekly for $wrkdir/$thisdate\n";
+}
+
+
+
+sub monthly ($) {
+    my $thisdate = shift @_;
+
+    print STDERR "Submitting monthly for $wrkdir/$thisdate\n";
 }
 
 
