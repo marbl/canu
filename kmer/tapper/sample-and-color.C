5eddb4711d0a7807c4af5d3ff42c03c415bdb338
Brian P. Walenz <thebri@gmail.com>
2008-06-25 10:57:34 +0000

    Likely lots of tiny bugs fixed.  Parse the colorspace alignment, correcting errors or committing to ACGT changes.

--- kmer/tapper/sample-and-color.C
+++ kmer/tapper/sample-and-color.C
@@ -3,7 +3,8 @@
 
 //  Reads a fastafile, samples randomly and outputs colorspace tags.
 
-#define MS 25
+//  Max of 33 for now.
+#define MS 33
 
 int
 main(int argc, char **argv) {
@@ -13,7 +14,7 @@ main(int argc, char **argv) {
     exit(1);
   }
 
-  int         N = atoi(argv[1]);
+  u32bit      N = atoi(argv[1]);
 
   seqFile    *F = openSeqFile(argv[2]);
   seqInCore  *s = F->getSequenceInCore();
@@ -21,43 +22,66 @@ main(int argc, char **argv) {
   u32bit  pos = 0;
   u32bit  len = s->sequenceLength();
 
+  char    cor[1024] = {0};
   char    seq[1024] = {0};
+
+  char    acgt[4] = {'A', 'C', 'G', 'T'};
 
   mt_s   *mtctx = mtInit(time(0));
 
   for (u32bit i=0; i<N; i++) {
     pos = mtRandom32(mtctx) % (len - MS);
 
-    char  n = 'N';
-    char  l = 'N';
+    char  n = acgt[mtRandom32(mtctx) % 4];
+    char  l = n;
+
+    cor[0] = n;
+    seq[0] = n;
+
+    bool   doForward = (mtRandom32(mtctx) & 0x1000) == 0x1000;
+
+    if (doForward) {
+      //  Forward
+      u32bit sp = pos;
+      for (u32bit x=1; x<=MS; x++) {
+        n = s->sequence()[sp++];
+        cor[x] = n;
+        seq[x] = baseToColor[l][n];
+        l = n;
+      }
+    } else {
+      //  Reverse
+      u32bit sp = pos + MS;
+      for (u32bit x=1; x<=MS; x++) {
+        n = s->sequence()[sp--];
+        cor[x] = n;
+        seq[x] = baseToColor[l][n];
+        l = n;
+      }
+    }
+
+    //  Insert 0 to 3 errors.
+
+    char     errors[256] = {0};
+    char     errort[256] = {0};
+    u32bit   nerr = 3;
 
 #if 1
-    for (u32bit x=0; x<MS; x++) {
-      n = s->sequence()[pos++];
-      seq[x] = baseToColor[l][n];
-      l = n;
-    }
-#else
-    pos += MS;
-    for (u32bit x=0; x<MS; x++) {
-      n = s->sequence()[pos--];
-      seq[x] = baseToColor[l][n];
-      l = n;
+    for (u32bit xx=mtRandom32(mtctx) % nerr + 1; xx-- > 0; ) {
+      u32bit e = mtRandom32(mtctx) % MS + 1;
+      char   o = seq[e];
+      seq[e] = seq[e] + 1;
+      if (seq[e] > '3')
+        seq[e] = '0';
+      sprintf(errort, "_%c-%c@%d", o, seq[e], e);
+      strcat(errors, errort);
     }
 #endif
 
-    //  Insert an error.
+    //seq[ 1]++;   if (seq[ 1] > '3')   seq[ 1] = '0';
+    //seq[33]++;   if (seq[33] > '3')   seq[33] = '0';
 
-    if (0) {
-      u32bit e = mtRandom32(mtctx) % MS;
-      seq[e] = seq[e] + 1;
-      if (seq[e] > '4')
-        seq[e] = '0';
-    }
-
-    //seq[10] = 3 - seq[10];
-
-    fprintf(stdout, ">idx"u32bitFMT"pos"u32bitFMT"\nN%s\n", i, pos - MS, seq);
+    fprintf(stdout, ">i"u32bitFMT"_p"u32bitFMT"_%s%s\n%s\n", i, pos, cor+1, errors, seq);
   }
   
 }
--- kmer/tapper/tapper.C
+++ kmer/tapper/tapper.C
@@ -8,9 +8,19 @@
 #include "tapperHit.H"
 #include "tapperComputation.H"
 
+//  To make this work for single reads, add a flag to global that says
+//  we're doing single reads.  Modify tapperReader to read a single
+//  read.
+//
+//  Use a whole new tapperWorker.  It's simple compared to the mated
+//  one (basically done now).
+//
+//  Tapper output hopefully will work for both single and paired
+//  reads.
+
 
 void*
-tapperReader(void *G) {
+tapperReaderMated(void *G) {
   tapperGlobalData  *g = (tapperGlobalData  *)G;
   tapperComputation *s = 0L;
 
@@ -39,6 +49,36 @@ tapperReader(void *G) {
 }
 
 
+void*
+tapperReaderSingle(void *G) {
+  tapperGlobalData  *g = (tapperGlobalData  *)G;
+  tapperComputation *s = 0L;
+
+ again:
+  seqInCore *a = g->QF->getSequenceInCore();
+
+  if (a == 0L)
+    return(0L);
+
+#warning need to get rid of duplicate a
+  s = new tapperComputation(a, a);
+
+  delete a;
+
+  if (s->tag1size != g->tagSize) {
+    fprintf(stderr, "tag size mismatch for '%s' ("u32bitFMT"); expected "u32bitFMT".\n",
+            a->header(), s->tag1size, g->tagSize);
+    delete s;
+    goto again;
+  }
+
+  return(s);
+}
+
+
+
+
+
 void
 tapperWriter(void *G, void *S) {
   tapperGlobalData  *g = (tapperGlobalData  *)G;
@@ -51,6 +91,13 @@ tapperWriter(void *G, void *S) {
 
 
 
+//  The big value of this function is to convert from a chained
+//  position to a (seqID,pos), and save the hit onto a bitpacked list.
+//  This list can then be numerically sorted to order all hits.  Of
+//  course, we could have just sorted the original chained positions.
+//
+//  It saves (seqID,pos,isTag2,isReverse)
+//
 inline
 u32bit
 tapperWorker_addHits(u64bit *hits, u32bit hitsLen, u64bit *posn, u64bit posnLen, tapperGlobalData *g, bool ta2, bool rev) {
@@ -68,13 +115,12 @@ tapperWorker_addHits(u64bit *hits, u32bit hitsLen, u64bit *posn, u64bit posnLen,
     pos -= g->SS->startOf(seq);
     seq  = g->SS->IIDOf(seq);
 
-    //  seqIdx | positionInSeq | isTag2 | isReversed
-
     hits[hitsLen++] = (seq << 33) | (pos << 2) | ta2rev;
   }
 
   return(hitsLen);
 }
+
 
 
 int
@@ -87,8 +133,206 @@ u64bitcompare(void const *a, void const *b) {
 }
 
 
+
+//  Compose the colors from beg to end.
+char
+composeColors(char *colors, u32bit beg, u32bit end) {
+  char c = colors[beg];
+
+  for (u32bit x=beg; x<end; x++)
+    c = baseToColor[c][colors[x]];
+
+  return(c);
+}
+
+
+
+//  Reencode the genomic sequence, using the same reference start as the tag.
+//
+//  tag          T3 1 0 2 1 1   encoded seeding with T
+//  tag          A C C T G T    decoded
+//
+//  genomic A 3 2 2 1 0 2 3 1   encoded seeding with A, and at the wrong spot, notice the tag matches
+//  genomic  T C T G G A T G C  real bases, we just have this around
+//  genomic      T1 0 2 3 1 3   reencode, seeding with a T at the same start as the tag
+//
+//  After reencoding, there is no agreement with the colorspace tag.
+//
+//  genomic  T C A C C T G T C  the correct sequence
+//  genomic   2 1 1 0 2 1 1 2   in color, note the first 3 in the tag doesn't match
+//  genomic     TA C C T G T C  reencoding with the first tag letter
+//  genomic     T31 0 2 1 1 2   now a match
+//
+//  Thus, if we locally reencode the genomic, we should end up
+//  with a color space sequence with the allowed number of
+//  errors.  At this point we can correct any color encoding
+//  issues (isolated changes) or detect any real base changes.
+//
+//  Count the number of differences between the tag and the reencoded genomic
+//
+u32bit
+alignToReference(tapperGlobalData *g,
+                 u64bit  so,
+                 u64bit  po,
+                 char   *tag, char *tagACGT, u32bit len,
+                 char   *ref, char *refACGT) {
+
+  char       *seq       = g->GS->getSequenceInCore(so)->sequence();
+  u32bit      ti = 0;
+  u32bit      si = po;
+  u32bit      errs = 0;  //  number of errors
+  u32bit      errp[64];  //  location of the errors
+
+  //  Analyze color[] and t[], correct differences, call base changes.
+  //  Return an ACGT sequence for the tag.
+
+  //  Compose the colors together.  At points where the compositions
+  //  disagree, the base at that point is different.  The composition
+  //  tells us how to transform the reference letter to the base at
+  //  this position, in one step.
+  //
+  //  If our final composed value is different, then either we end on
+  //  a SNP, or we have an error somewhere.  The choice here is
+  //  arbitrary, and made depending on where that error is.
+
+  ref[0] = tag[0];
+  ref[1] = baseToColor[ref[0]][seq[po]];
+
+  if (ref[1] != tag[1]) {
+    errp[errs] = 1;
+    errs++;
+  }
+
+  for (ti=2, si=po+1; ti<len; ti++, si++) {
+    ref[ti] = baseToColor[seq[si-1]][seq[si]];
+
+    if (tag[ti] != ref[ti]) {
+      errp[errs] = ti;
+      errs++;
+    }
+  }
+
+  //  Note that errp[] is actaully 1-based; the first position is never
+  //  an error; it's the reference base.
+
+  if ((errs == 0) || (g->maxError < errs))
+    return(errs);
+
+  //  Compose the colors.  If these are the same, we call the two
+  //  colors 'consistent' - the ACGT reads begin and end with the same
+  //  base.
+  //
+  u32bit  tagc = composeColors(tag, 1, len);
+  u32bit  refc = composeColors(ref, 1, len);
+
+  //  Copy the tag colors so we can correct single color flaws to
+  //  generate the final ACGT.
+  //
+  char        cor[64];
+
+  strcpy(cor, tag);
+
+
+
+  if (errs == 1) {
+
+    //  len-1 because we lose one letter when converting from color to acgt.
+    //  errp[] is 1-based, so == len-1 is the last base in the tag.
+    //
+    if ((errp[0]) == (len - 1)) {
+      //  Exactly at the end, believe it's the start of a SNP.  For a
+      //  SNP to be at the start, we'll need two color changes.  One
+      //  color change at the start is a color error.  The end is
+      //  different, because we just don't know what the next color
+      //  would have been.  The beginning has the reference base to
+      //  anchor it.
+      //
+    } else {
+      //  Correct it.
+      cor[errp[0]] = ref[errp[0]];
+    }
+  }
+
+
+  //  If there are two errors, and the whole string is consistent,
+  //  we'll call it a single mismatch block, if it is small.
+  //  Otherwise, we'll fix the two errors.
+  //
+  if (errs == 2) {
+    if ((tagc == refc) && (errp[1] - errp[0] < 4)) {
+      //  MNP of size 4.
+    } else if (tagc == refc) {
+      //  Big MNP?  What to do?  Correct, I guess.
+      cor[errp[0]] = ref[errp[0]];
+      cor[errp[1]] = ref[errp[1]];
+    } else {
+      //  Correct 'em.
+      cor[errp[0]] = ref[errp[0]];
+      cor[errp[1]] = ref[errp[1]];
+    }
+  }
+
+
+  //  If there are three errors...
+  //
+  //  String consistent, and MNP block is small, let it through.
+  //
+  //  String not consistent, we need to see if the first two or last
+  //  two errors are consistent.  If so, declare the third error is a
+  //  color error and correct it.
+  //
+  if (errs == 3) {
+    if        ((tagc == refc) && (errp[2] - errp[0] < 5)) {
+      //  MNP of size 5
+    } else if (tagc == refc) {
+      //  Big MNP?  What to do?  Correct, I Guess.
+      cor[errp[0]] = ref[errp[0]];
+      cor[errp[1]] = ref[errp[1]];
+      cor[errp[2]] = ref[errp[2]];
+    } else if (composeColors(tag, 0, errp[2]) == composeColors(ref, 0, errp[2])) {
+      //  First two ok, fix the third.
+      cor[errp[2]] = ref[errp[2]];
+    } else if (composeColors(tag, errp[0]+1, len) == composeColors(ref, errp[0]+1, len)) {
+      //  Last two ok, fix the first.
+      cor[errp[0]] = ref[errp[0]];
+    } else {
+      //  Nothing consistent, fix all of 'em.
+      cor[errp[0]] = ref[errp[0]];
+      cor[errp[1]] = ref[errp[1]];
+      cor[errp[2]] = ref[errp[2]];
+    }
+  }
+
+
+  if (errs == 4) {
+    fprintf(stderr, "Four errors detected.  Code doesn't know what to do.\n");
+    exit(1);
+  }
+
+  tagACGT[0] = baseToColor[cor[0]][cor[1]];
+  refACGT[0] = baseToColor[ref[0]][ref[1]];
+  for (ti=1, si=2; si<len; ti++, si++) {
+    tagACGT[ti] = baseToColor[tagACGT[ti-1]][cor[si]];
+    refACGT[ti] = baseToColor[refACGT[ti-1]][ref[si]];
+    if (tagACGT[ti] != refACGT[ti]) {
+      tagACGT[ti] = toUpper[tagACGT[ti]];
+      refACGT[ti] = toUpper[refACGT[ti]];
+    }
+  }
+  tagACGT[len-1] = 0;
+  refACGT[len-1] = 0;
+
+  fprintf(stderr, "tag:%s/%s\nref:%s/%s\n", tag, tagACGT, ref, refACGT);
+
+  
+  return(errs);
+}
+
+
+
 void
-tapperWorker(void *G, void *T, void *S) {
+tapperWorkerMated(void *G, void *T, void *S) {
+#if 0
   tapperGlobalData  *g = (tapperGlobalData  *)G;
   tapperThreadData  *t = (tapperThreadData  *)T;
   tapperComputation *s = (tapperComputation *)S;
@@ -104,73 +348,104 @@ tapperWorker(void *G, void *T, void *S) {
   g->PS->getUpToNMismatches(s->tag2f, 3, t->posn2f, t->posn2fMax, t->posn2fLen);
   g->PS->getUpToNMismatches(s->tag2r, 3, t->posn2r, t->posn2rMax, t->posn2rLen);
 
-  //  Now need to make sense of this stuff.
-  //
-  //  1.  convert from positions to seqId/pos
-  //  2.  build list of: refId[31] refPos[31]  tag2?  rev?
-  //  3.  sort numerically (and so by refId and refPos).
-  //  4.  scan the list
-  //      - report tags that map to any scaffold without their mate
-  //      - rewrite the list to have scaffolds with mated tags
-
-  u32bit  hitsLen = t->posn1fLen + t->posn1rLen + t->posn2fLen + t->posn2rLen;
-
-  if (hitsLen == 0)
+  if (t->posn1fLen + t->posn1rLen + t->posn2fLen + t->posn2rLen == 0)
     return;
 
-  fprintf(stderr, "Found "u64bitFMT","u64bitFMT" hits for %s and "u64bitFMT","u64bitFMT" for %s\n",
-          t->posn1fLen, t->posn1rLen, s->tag1name,
-          t->posn2fLen, t->posn2rLen, s->tag2name);
-
-  u64bit *hits    = new u64bit [hitsLen];
-
-  hitsLen = 0;
-  hitsLen = tapperWorker_addHits(hits, hitsLen, t->posn1f, t->posn1fLen, g, false, false);
-  hitsLen = tapperWorker_addHits(hits, hitsLen, t->posn1r, t->posn1rLen, g, false, true);
-  hitsLen = tapperWorker_addHits(hits, hitsLen, t->posn2f, t->posn2fLen, g, true,  false);
-  hitsLen = tapperWorker_addHits(hits, hitsLen, t->posn2r, t->posn2rLen, g, true,  true);
-
-#warning use a real sort
-  qsort(hits, hitsLen, sizeof(u64bit), u64bitcompare);
-
-  for (u32bit i=0; i<hitsLen; i++) {
-    u64bit  so  = (hits[i] >> 33) & u64bitMASK(31);
-    u64bit  po  = (hits[i] >> 2)  & u64bitMASK(31);
-    u64bit  ta2 = (hits[i] >> 1)  & 0x01;
-    u64bit  rev = (hits[i] >> 0)  & 0x01;
-
-    fprintf(stderr, "hit: "u64bitFMT" @ "u64bitFMT" tag2:"u64bitFMT" rev:"u64bitFMT"\n", so, po, ta2, rev);
+  if (t->hits1Max < t->posn1fLen + t->posn1rLen) {
+    t->hits1Max = t->posn1fLen + t->posn1rLen;
+    delete [] t->hits1;
+    t->hits1 = new u64bit [t->posn1fLen + t->posn1rLen];
   }
+  t->hits1Len = 0;
+  t->hits1Len = tapperWorker_addHits(t->hits1, t->hits1Len, t->posn1f, t->posn1fLen, g, false, false);
+  t->hits1Len = tapperWorker_addHits(t->hits1, t->hits1Len, t->posn1r, t->posn1rLen, g, false, true);
 
-  //
-  //  Do something useful with the hits, like verify them against the ACGT reference.
-  //
-  
+  if (t->hits2Max < t->posn2fLen + t->posn2rLen) {
+    t->hits2Max = t->posn2fLen + t->posn2rLen;
+    delete [] t->hits2;
+    t->hits2 = new u64bit [t->posn2fLen + t->posn2rLen];
+  }
+  t->hits2Len = 0;
+  t->hits2Len = tapperWorker_addHits(t->hits2, t->hits2Len, t->posn2f, t->posn2fLen, g, true,  false);
+  t->hits2Len = tapperWorker_addHits(t->hits2, t->hits2Len, t->posn2r, t->posn2rLen, g, true,  true);
 
-  //  Stuff all the good hits into tappeComputation's hits entry.
+#warning score hits
+  //  Then score hits individually, throwing out any that are junk.
+  //  Probably should do that before we addHits?
 
-  for (u32bit i=0; i<hitsLen; i++) {
-    tapperHit  h = {0};
+  //  Sort so we can do mate rescue.
+#warning use a real sort
+  qsort(t->hits1, t->hits1Len, sizeof(u64bit), u64bitcompare);
+  qsort(t->hits2, t->hits2Len, sizeof(u64bit), u64bitcompare);
+#endif
+}
 
-    u64bit  so  = (hits[i] >> 33) & u64bitMASK(31);
-    u64bit  po  = (hits[i] >> 2)  & u64bitMASK(31);
-    u64bit  ta2 = (hits[i] >> 1)  & 0x01;
-    u64bit  rev = (hits[i] >> 0)  & 0x01;
 
-    h.seqIdx     = so;
+void
+tapperWorkerSingle(void *G, void *T, void *S) {
+  tapperGlobalData  *g = (tapperGlobalData  *)G;
+  tapperThreadData  *t = (tapperThreadData  *)T;
+  tapperComputation *s = (tapperComputation *)S;
+
+  t->posn1fLen = 0;
+  t->posn1rLen = 0;
+
+  g->PS->getUpToNMismatches(s->tag1f, g->maxError, t->posn1f, t->posn1fMax, t->posn1fLen);
+  g->PS->getUpToNMismatches(s->tag1r, g->maxError, t->posn1r, t->posn1rMax, t->posn1rLen);
+
+  if (t->posn1fLen + t->posn1rLen == 0)
+    return;
+
+  if (t->hits1Max < t->posn1fLen + t->posn1rLen) {
+    t->hits1Max = t->posn1fLen + t->posn1rLen;
+    delete [] t->hits1;
+    t->hits1 = new u64bit [t->posn1fLen + t->posn1rLen];
+  }
+  t->hits1Len = 0;
+  t->hits1Len = tapperWorker_addHits(t->hits1, t->hits1Len, t->posn1f, t->posn1fLen, g, false, false);
+  t->hits1Len = tapperWorker_addHits(t->hits1, t->hits1Len, t->posn1r, t->posn1rLen, g, false, true);
+
+  for (u32bit i=0; i<t->hits1Len; i++) {
+    tapperHit  h   = {0};
+    u64bit     so  = (t->hits1[i] >> 33) & u64bitMASK(31);  //  Sequence it hits
+    u64bit     po  = (t->hits1[i] >> 2)  & u64bitMASK(31);  //  Position in sequence
+    u64bit     ta2 = (t->hits1[i] >> 1)  & 0x01;            //  isTag2
+    u64bit     rev = (t->hits1[i] >> 0)  & 0x01;            //  isReversed
+
+    //  We're ignoring the first base, so adjust the position to
+    //  include it.
+    po--;
+
+    //
+    //  Do something useful with the hits, like verify them against the ACGT reference.
+    //
+
+    u32bit mm = 0;
+    char   tagACGT[64];
+    char   refACGT[64];
 
     if (ta2) {
-      h.setOrientation(0, 0, 1, rev);
-      h.tag2pos = po;
+      mm = alignToReference(g, so, po, (rev) ? s->tag2rseq : s->tag2fseq, tagACGT, s->tag2size+2, s->refseq, refACGT);
     } else {
-      h.setOrientation(1, rev, 0, 0);
-      h.tag1pos = po;
+      mm = alignToReference(g, so, po, (rev) ? s->tag1rseq : s->tag1fseq, tagACGT, s->tag1size+2, s->refseq, refACGT);
     }
 
-    s->addHit(h);
-  }
+    //fprintf(stderr, "hit: "u64bitFMT" @ "u64bitFMT" tag2:"u64bitFMT" rev:"u64bitFMT"\n", so, po, ta2, rev);
 
-  delete [] hits;
+    if (mm <= g->maxError) {
+      h.seqIdx     = so;
+
+      if (ta2) {
+        h.setOrientation(0, 0, 1, rev);
+        h.tag2pos = po;
+      } else {
+        h.setOrientation(1, rev, 0, 0);
+        h.tag1pos = po;
+      }
+
+      s->addHit(h);
+    }
+  }
 }
 
 
@@ -195,6 +470,9 @@ main(int argc, char **argv) {
     } else if (strcmp(argv[arg], "-numthreads") == 0) {
       g->numThreads = atoi(argv[++arg]);
 
+    } else if (strcmp(argv[arg], "-mated") == 0) {
+      g->isMated = true;
+
     } else if (strcmp(argv[arg], "-verbose") == 0) {
       g->beVerbose = true;
     } else {
@@ -210,7 +488,9 @@ main(int argc, char **argv) {
 
   g->initialize();
 
-  sweatShop *ss = new sweatShop(tapperReader, tapperWorker, tapperWriter);
+  sweatShop *ss = new sweatShop((g->isMated) ? tapperReaderMated : tapperReaderSingle,
+                                (g->isMated) ? tapperWorkerMated : tapperWorkerSingle,
+                                tapperWriter);
 
   ss->loaderQueueSize(10240);
   ss->writerQueueSize(10240);
@@ -220,7 +500,7 @@ main(int argc, char **argv) {
   for (u32bit w=0; w<g->numThreads; w++)
     ss->setThreadData(w, new tapperThreadData(g));
 
-  ss->run(g, true);  //  true == verbose
+  ss->run(g, g->beVerbose);
 
   delete g;
   delete ss;
