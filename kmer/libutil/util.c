b3cab790b04254c0c0dddbbf4dab21c91d084366
Brian P. Walenz <thebri@gmail.com>
2008-09-12 20:50:11 +0000

    Forgot to add libutil/util.c in earlier commit.

---
+++ kmer/libutil/util.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+
+#include "util.h"
+
+
+double
+getTime(void) {
+  struct timeval  tp;
+  gettimeofday(&tp, NULL);
+  return(tp.tv_sec + (double)tp.tv_usec / 1000000.0);
+}
+
+
+void *
+memdup(const void *orig, size_t size) {
+  void *rslt = NULL;
+
+  if ((orig != NULL) && (size > 0)) {
+    errno = 0;
+    rslt = malloc(size);
+    if (errno) {
+      //  Some ugliness to print out a size_t.  This might be useless,
+      //  as it might be determined by TRUE64BIT.
+      //
+      if (sizeof(size_t) == 8)
+        fprintf(stderr, "memdup()-- can't allocate "s64bitFMT" bytes.\n%s\n", (s64bit)size, strerror(errno));
+      else
+        fprintf(stderr, "memdup()-- can't allocate "u32bitFMT" bytes.\n%s\n", (u32bit)size, strerror(errno));
+      exit(1);
+    }
+    memcpy(rslt, orig, size);
+  }
+  return(rslt);
+}
+
+
+int
+fileExists(const char *path) {
+  struct stat  s;
+
+  return(stat(path, &s) == 0);
+}
+
+
+off_t
+sizeOfFile(const char *path) {
+  struct stat s;
+
+  errno = 0;
+  if (stat(path, &s) != 0)
+    fprintf(stderr, "Couldn't stat() '%s'\n%s\n", path, strerror(errno)), exit(1);
+
+  return(s.st_size);
+}
+
+
+u64bit
+timeOfFile(const char *path) {
+  struct stat s;
+
+  errno = 0;
+  if (stat(path, &s) != 0)
+    fprintf(stderr, "Couldn't stat() '%s'\n%s\n", path, strerror(errno)), exit(1);
+
+  return(s.st_mtime);
+}
