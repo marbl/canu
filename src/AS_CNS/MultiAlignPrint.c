978e7b3a0f15d23c731d28e34a858574c78ab5f0
Brian P. Walenz <thebri@gmail.com>
2009-10-05 04:45:44 +0000

    If no consensus sequence, don't crash.

--- src/AS_CNS/MultiAlignPrint.c
+++ src/AS_CNS/MultiAlignPrint.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
 
-static const char *rcsid = "$Id: MultiAlignPrint.c,v 1.6 2009-09-07 07:40:57 brianwalenz Exp $";
+static const char *rcsid = "$Id: MultiAlignPrint.c,v 1.7 2009-10-05 04:45:44 brianwalenz Exp $";
 
 #include <assert.h>
 #include <stdio.h>
@@ -53,7 +53,10 @@ PrintMultiAlignT(FILE *out,
 
   gkFragment rsp;
 
-  length = strlen(consensus);
+  if (consensus)
+    length = strlen(consensus);
+  else
+    length = 0;
 
   rc = IMP2Array(GetIntMultiPos(ma->f_list,0),
                  GetNumIntMultiPoss(ma->f_list),
