023d81fbf2608638db5e44dc72f3d88e9f6eb1dd
Brian P. Walenz <thebri@gmail.com>
2009-11-04 17:21:08 +0000

    Don't report progress for singleton unitigs.

--- src/AS_CNS/utgcns.C
+++ src/AS_CNS/utgcns.C
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
 
-const char *mainid = "$Id: utgcns.C,v 1.5 2009-10-12 04:03:18 brianwalenz Exp $";
+const char *mainid = "$Id: utgcns.C,v 1.6 2009-11-04 17:21:08 brianwalenz Exp $";
 
 #include "AS_global.h"
 #include "MultiAlign.h"
@@ -107,8 +107,9 @@ main (int argc, char **argv) {
       //  Not in our partition, or deleted.
       continue;
 
-    fprintf(stderr, "Working on unitig %d (%d unitigs and %d fragments)\n",
-            ma->maID, ma->data.num_unitigs, ma->data.num_frags);
+    if (ma->data.num_frags > 1)
+      fprintf(stderr, "Working on unitig %d (%d unitigs and %d fragments)\n",
+              ma->maID, ma->data.num_unitigs, ma->data.num_frags);
 
     if (MultiAlignUnitig(ma, gkpStore, printwhat, &options)) {
       tigStore->insertMultiAlign(ma, TRUE, FALSE);
