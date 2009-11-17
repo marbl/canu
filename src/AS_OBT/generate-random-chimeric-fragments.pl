e653d443bf9e19bc94cfc3b884ac908629224be1
Brian P. Walenz <thebri@gmail.com>
2009-11-17 08:59:29 +0000

    Add a few test scripts for stressing the chimera detection code.

---
+++ src/AS_OBT/generate-random-chimeric-fragments.pl
@@ -0,0 +1,142 @@
+#!/usr/bin/perl
+use strict;
+
+#
+###########################################################################
+#
+# This file is part of Celera Assembler, a software program that
+# assembles whole-genome shotgun reads into contigs and scaffolds.
+# Copyright (C) 2009, J. Craig Venter Instititue.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received (LICENSE.txt) a copy of the GNU General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+###########################################################################
+#
+#  Generates random chimera and spur fragments from a supplied reference
+#  genome.
+#
+#  User unfriendly.
+#
+#  A spur:     [junk][sequence]
+#  A chimer:   [junk][sequence][junk][sequence][junk]
+
+my $genome     = shift @ARGV;
+my $readLength = 400;
+
+my $leaff = "/work/wgs/kmer/leaff/leaff";
+my $cvtto = "perl /work/wgs/src/AS_MSG/convert-fasta-to-v2.pl";
+
+die "Reference sequence '$genome' not found.\n" if (! -e $genome);
+
+#  generate some random reads from the genome, both forward and reverse.
+
+system("$leaff --errors $readLength 10000 1 0.01 $genome > sample3.fasta");
+system("$leaff -f sample3.fasta -R -C -W > sample1.fasta");
+system("$leaff --errors $readLength 10000 1 0.01 $genome > sample2.fasta");
+
+#  generate random small bits of sequence from those samples; leaff can't do this nicely.
+
+my @sequences;
+
+open(F, "< sample1.fasta");
+while (!eof(F)) {
+    my $h = <F>;  chomp $h;
+    my $s = <F>;  chomp $s;
+    my $r = rand();
+    push @sequences, "$r\0seq\0$s";
+}
+close(F);
+
+open(F, "< sample2.fasta");
+while (!eof(F)) {
+    my $h = <F>;  chomp $h;
+    my $s = <F>;  chomp $s;
+    my $r = rand();
+    push @sequences, "$r\0rev\0$s";
+}
+close(F);
+
+@sequences = sort { $a <=> $b } @sequences;
+
+open(F, "> chimer.fasta");
+open(R, "$leaff -G 100000 $readLength $readLength |");
+
+my $iid    = 0;
+my $def    = "";
+my $seq    = "";
+my $seqLen = 0;
+
+while (scalar(@sequences) > 0) {
+    my $np = int(rand() * 3 + 1);
+    my $gl;
+
+    $iid++;
+    $def    = "$iid";
+    $seq    = "";
+    $seqLen = 0;
+
+    $gl = int(rand() * 200 - 50);  #  between -50 and 150, junk on the front
+    if ($gl > 0) {
+        my $h = <R>;
+        my $s = <R>;
+
+        $def    .= "-jnk-$gl";
+        $seq    .= substr($s, 0, $gl);
+        $seqLen += $gl;
+    }
+
+    for (my $pp=0; $pp < $np; $pp++) {
+        my $S = pop @sequences;
+        my ($r, $h, $s) = split '\0', $S;
+
+        my $l = int(rand() * 200 + 50);  #  between 50 and 250.
+
+        $def    .= "-$h-$l";
+        $seq    .= substr($s, 0, $l);
+        $seqLen += $l;
+
+        if ($pp + 1 == $np) {
+            $gl = 200;
+        } else {
+            $gl = 100;
+        }
+
+        $gl = int(rand() * $gl - 50);  #  between -50 and 50, junk in the middle
+        if ($gl > 0) {
+            my $h = <R>;
+            my $s = <R>;
+
+            $def    .= "-jnk-$gl";
+            $seq    .= substr($s, 0, $gl);
+            $seqLen += $gl;
+        }
+    }
+
+    print F ">$def\n$seq\n";
+}
+
+close(F);
+
+system("tr 'ACGT' 'NNNN' < chimer.fasta > chimer.qv");
+system("perl /work/wgs/src/AS_MSG/convert-fasta-to-v2.pl -l CHIMER -s chimer.fasta -q chimer.qv > chimer.frg");
+
+unlink "sample1.fasta";
+unlink "sample2.fasta";
+unlink "sample3.fasta";
+unlink "sample1.fastaidx";
+unlink "sample2.fastaidx";
+unlink "sample3.fastaidx";
+unlink "chimer.fasta";
+unlink "chimer.qv";
---
+++ src/AS_OBT/test-random-chimeric-fragments.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+use strict;
+
+#
+###########################################################################
+#
+# This file is part of Celera Assembler, a software program that
+# assembles whole-genome shotgun reads into contigs and scaffolds.
+# Copyright (C) 2009, J. Craig Venter Instititue.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received (LICENSE.txt) a copy of the GNU General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+###########################################################################
+#
+#  Figures out what pieces ended up after trimming.  Use with generate-random-chimeric-fragments.pl
+#
+#  User unfriendly.
+
+my $uid;
+my $iid;
+
+open(F, "/work/wgs/FreeBSD-amd64/bin/gatekeeper -dumpfragments /work/test/test.gkpStore |") or die;
+while (<F>) {
+    if (m/^fragmentIdent\s+=\s+(.*),(\d+)$/) {
+        $uid = $1;
+        $iid = $2;
+    }
+    if (m/^fragmentIsDeleted\s+=\s+1$/) {
+        undef $uid;
+        undef $iid;
+    }
+
+    if (m/^fragmentClear\s+=\s+OBTCHIMERA,(\d+),(\d+)$/) {
+        my $bgn = $1;
+        my $end = $2;
+
+        my $b = 0;
+        my $e = 0;
+
+        if ($uid =~ m/(\d+)-/) {
+            my @v = split '-', $uid;
+
+            shift @v;  #  iid of read
+
+            my $numMatches = 0;
+            my $strMatches = "";
+            my $badMatches = 0;
+
+            while (scalar(@v) > 0) {
+                my $typ = shift @v;
+                my $len = shift @v;
+
+                $e = $b + $len;
+
+                if (($bgn < $e - 5) && ($b + 5 < $end)) {
+                    $numMatches++;
+                    $strMatches .= "$b-$e-$typ ";
+
+                    if (($typ ne "seq") && ($typ ne "rev")) {
+                        $badMatches++;
+                    }
+                }
+
+                $b = $e;
+            }
+
+            if (($numMatches > 1) || ($badMatches > 0)) {
+                my $ovl = `/work/wgs/FreeBSD-amd64/bin/overlapStore -g /work/test/test.gkpStore -dp $iid /work/test/0-overlaptrim/test.obtStore`;
+
+                if (length($ovl) > 0) {
+                    print "$uid\n";
+                    print "$bgn-$end\n";
+                    print "$strMatches\n";
+                    print $ovl;
+                    print "========================================================================================================================\n";
+                }
+            }
+        }
+    }
+}
+close(F);
