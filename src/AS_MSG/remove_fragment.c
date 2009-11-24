0f48f21fa020c5e69a07390fcd3a004d4b20552d
Brian P. Walenz <thebri@gmail.com>
2009-11-24 01:36:15 +0000

    Fix braindeadness; logic was negated, so any UID listed as one to keep was output, and unlisted UIDs were discarded.  Now, provide options for both behaviors.

--- src/AS_MSG/remove_fragment.c
+++ src/AS_MSG/remove_fragment.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
 
-const char *mainid = "$Id: remove_fragment.c,v 1.17 2008-12-05 19:06:12 brianwalenz Exp $";
+const char *mainid = "$Id: remove_fragment.c,v 1.18 2009-11-24 01:36:15 brianwalenz Exp $";
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,6 +33,14 @@ const char *mainid = "$Id: remove_fragment.c,v 1.17 2008-12-05 19:06:12 brianwal
 
 int
 main(int argc, char ** argv) {
+  char           *inputName    = NULL;
+  char           *strippedName = NULL;
+  char           *removedName  = NULL;
+  char           *UIDname      = NULL;
+
+  FILE           *I = NULL;
+  FILE           *S = NULL;
+  FILE           *R = NULL;
 
   char            line[1024] = {0};
 
@@ -43,57 +51,121 @@ main(int argc, char ** argv) {
   FragMesg       *frg = NULL;
   LinkMesg       *lkg = NULL;
 
+  uint32          frgRemoved = 0;
+  uint32          lkgRemoved = 0;
+
   argc = AS_configure(argc, argv);
 
   int arg = 1;
   int err = 0;
   while (arg < argc) {
-    if (strcmp(argv[arg], "-f") == 0) {
-      errno = 0;
-      FILE *F = fopen(argv[++arg], "r");
-      if (errno) {
-      }
-      while (fgets(line, 1024, F)) {
-        AS_UID  uid = AS_UID_lookup(line, NULL);
-        InsertInHashTable_AS(frag_uids_desired, AS_UID_toInteger(uid), 0, 0, 0);
-      }
-      fclose(F);
+    if        (strcmp(argv[arg], "-f") == 0) {
+      UIDname = argv[++arg];
+
+    } else if (strcmp(argv[arg], "-i") == 0) {
+      inputName = argv[++arg];
+
+    } else if (strcmp(argv[arg], "-o") == 0) {
+      strippedName = argv[++arg];
+
+    } else if (strcmp(argv[arg], "-O") == 0) {
+      removedName = argv[++arg];
+
     } else {
-      AS_UID  uid = AS_UID_lookup(argv[arg], NULL);
-      InsertInHashTable_AS(frag_uids_desired, AS_UID_toInteger(uid), 0, 0, 0);
+      fprintf(stderr, "%s: unknown option '%s'\n", argv[0], argv[arg]);
+      err++;
     }
+
     arg++;
   }
-  if (err) {
-    fprintf(stderr, "usage: %s [-f UIDfile] UID UID .... < in > out\n", argv[0]);
-    fprintf(stderr, "  -f UIDfile    one uid per line\n");
+  if ((err) ||
+      (inputName == NULL) ||
+      ((strippedName == NULL) && (removedName == NULL))) {
+    fprintf(stderr, "usage: %s -f UIDfile -i original.frg [-o stripped.frg] [-O removed.frg]\n", argv[0]);
+    fprintf(stderr, "  -f UIDfile       one uid per line\n");
+    fprintf(stderr, "  -i original.frg  fragments input\n");
+    fprintf(stderr, "  -o stripped.frg  fragments NOT listed in UIDfile are saved here\n");
+    fprintf(stderr, "  -O removed.frg   fragments     listed in UIDfile are saved here\n");
     fprintf(stderr, "\n");
-    fprintf(stderr, "Strips messages (FRG, LKG only) from the input file.\n");
+    fprintf(stderr, "Removes fragments and links of supplied UIDs from the input fragment file.\n");
     exit(1);
   }
 
+  errno = 0;
+  FILE *F = fopen(UIDname, "r");
+  if (errno) {
+    fprintf(stderr, "%s: failed to open '%s' for reading UIDs: %s\n", argv[0], UIDname, strerror(errno));
+    exit(1);
+  }
+  while (fgets(line, 1024, F)) {
+    AS_UID  uid = AS_UID_lookup(line, NULL);
+    InsertInHashTable_AS(frag_uids_desired, AS_UID_toInteger(uid), 0, 0, 0);
+  }
+  fclose(F);
 
-  while (ReadProtoMesg_AS(stdin, &gen) != EOF) {
+  if (inputName) {
+    I = fopen(inputName, "r");
+    if (errno) {
+      fprintf(stderr, "%s: failed to open input '%s' for reading: %s\n", argv[0], inputName, strerror(errno));
+      exit(1);
+    }
+  }
+
+  if (strippedName) {
+    S = fopen(strippedName, "w");
+    if (errno) {
+      fprintf(stderr, "%s: failed to open output '%s' for writing: %s\n", argv[0], strippedName, strerror(errno));
+      exit(1);
+    }
+  }
+
+  if (removedName) {
+    R = fopen(removedName, "w");
+    if (errno) {
+      fprintf(stderr, "%s: failed to open output '%s' for writing: %s\n", argv[0], removedName, strerror(errno));
+      exit(1);
+    }
+  }
+
+
+  while (ReadProtoMesg_AS(I, &gen) != EOF) {
     if (gen->t == MESG_FRG) {
       frg = (FragMesg *)gen->m;
       if (ExistsInHashTable_AS(frag_uids_desired, AS_UID_toInteger(frg->eaccession), 0)) {
+        frgRemoved++;
         InsertInHashTable_AS(frag_uids_found, AS_UID_toInteger(frg->eaccession), 0, 0, 0);
-        WriteProtoMesg_AS(stdout, gen);
+        if (R)
+          WriteProtoMesg_AS(R, gen);
       } else {
-        //WriteProtoMesg_AS(stdout, gen);
+        if (S)
+          WriteProtoMesg_AS(S, gen);
       }
     } else if (gen->t == MESG_LKG) {
       lkg = (LinkMesg *)gen->m;
       if (ExistsInHashTable_AS(frag_uids_found, AS_UID_toInteger(lkg->frag1), 0) &&
           ExistsInHashTable_AS(frag_uids_found, AS_UID_toInteger(lkg->frag2), 0)) {
-        WriteProtoMesg_AS(stdout, gen);
+        lkgRemoved++;
+        if (R)
+          WriteProtoMesg_AS(R, gen);
       } else {
-        //WriteProtoMesg_AS(stdout, gen);
+        if (S)
+          WriteProtoMesg_AS(S, gen);
       }
     } else {
-      WriteProtoMesg_AS(stdout, gen);
+      if (R)
+        WriteProtoMesg_AS(R, gen);
+      if (S)
+        WriteProtoMesg_AS(S, gen);
     }
   }
+
+  fclose(I);
+  if (R)
+    fclose(R);
+  if (S)
+    fclose(S);
+
+  fprintf(stderr, "Removed %u fragments and %u links.\n", frgRemoved, lkgRemoved);
 
   return(0);
 }
