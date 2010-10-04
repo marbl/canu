3d8dc1d2c9b78834ef629cbbd881476abf06e251
Brian P. Walenz <thebri@gmail.com>
2010-10-04 04:48:34 +0000

    Rename AS_BOG_Unitig_PlaceFrag.cc (just added, no history there) to AS_BOG_Unitig_PlaceFragUsingEdges.cc

--- src/AS_BOG/AS_BOG_Unitig_PlaceFragUsingEdges.cc
+++ src/AS_BOG/AS_BOG_Unitig_PlaceFragUsingEdges.cc
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
 
-static const char *rcsid = "$Id: AS_BOG_Unitig_PlaceFrag.cc,v 1.1 2010-10-04 04:41:33 brianwalenz Exp $";
+static const char *rcsid = "$Id: AS_BOG_Unitig_PlaceFragUsingEdges.cc,v 1.1 2010-10-04 04:48:34 brianwalenz Exp $";
 
 #include "AS_BOG_Datatypes.hh"
 #include "AS_BOG_Unitig.hh"
