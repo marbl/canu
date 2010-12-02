14b618f2dec5d616fbfdc2a2b9a5870b70108740
Brian P. Walenz <thebri@gmail.com>
2010-12-02 20:49:59 +0000

    Add scripts to run bogart analyses.

---
+++ src/AS_BAT/bogart-build-fasta.pl
@@ -0,0 +1,163 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $prefix;
+my $FILE     = "test.004.buildUnitigs";
+my $REF      = "../../AE015924.fasta";
+my $withSGE  = 0;
+my $withPart = 0;
+
+while (scalar(@ARGV) > 0) {
+    if ($ARGV[0] eq "-sge") {
+        $withSGE = 1;
+
+    } elsif ($ARGV[0] eq "-partition") {
+        $withPart = 1;
+
+    } else {
+        $FILE = $ARGV[0];
+    }
+
+    shift @ARGV;
+}
+
+if ($FILE =~ m/^(.*)\.\d\d\d\./) {
+    $prefix = $1;
+} else {
+    die "Failed to find prefix in '$FILE'\n";
+}
+
+my %detected1;
+my %detected2;
+
+
+if (! -e "$FILE.tigStore") {
+    my $cmd;
+
+    $cmd  = "bogart \\n";
+    $cmd .= "  -O ../$prefix.ovlStore \\n";
+    $cmd .= "  -G ../$prefix.gkpStore \\n";
+    $cmd .= "  -T ../$prefix.tigStore \\n";
+    $cmd .= "  -B 75000 \\n";
+    $cmd .= "  -e 0.03 \\n";
+    $cmd .= "  -E 0 \\n";
+    $cmd .= "  -b \\n";
+    $cmd .= "  -m 7 \\n";
+    $cmd .= "  -U \\n";
+    $cmd .= "  -o $prefix \\n";
+    $cmd .= "  -D all \\n";
+    $cmd .= "  -d overlapQuality \\n";
+
+    print "$cmd\n";
+
+    die "Will not run command.\n";
+}
+
+########################################
+
+if ($withPart) {
+    my $cmd;
+
+    $cmd .= "gatekeeper";
+    $cmd .= " -P $FILE.tigStore.partitioning ../$prefix.gkpStore";
+
+    system($cmd);
+
+    print STDERR "Partitioned.\n";
+    exit;
+}
+
+########################################
+
+print STDERR "Running consensus\n";
+open(R, "> $FILE.sge.sh") or die;
+print R "#!/bin/sh\n";
+print R "idx=`printf %03d \$SGE_TASK_ID`\n";
+print R "utgcns -f -g ../$prefix.gkpStore -t $FILE.tigStore 1 \$idx > $FILE.tigStore/seqDB.v002.p\${idx}.utg.err 2>&1\n";
+close(R);
+
+open(R, "> $FILE.tmp.sh") or die;
+open(F, "ls $FILE.tigStore/seqDB.v001.p???.dat |");
+while (<F>) {
+    chomp;
+
+    if (m/seqDB.v001.p(\d\d\d).dat$/) {
+        my $idx = $1;
+
+        if (! -e "$FILE.tigStore/seqDB.v002.p${idx}.dat") {
+            print STDERR " $_\n";
+            print R "utgcns -f -g ../$prefix.gkpStore -t $FILE.tigStore 1 $idx > $FILE.tigStore/seqDB.v002.p${idx}.utg.err 2>&1 &\n";
+        } else {
+            print STDERR " $_ finished.\n";
+        }
+    } else {
+        die "No '$_'\n";
+    }
+}
+close(F);
+
+print R "wait\n";
+close(R);
+
+if ($withSGE) {
+    #system("qsub -cwd -j y -o /dev/null -l memory=2g -t 1-9 -q servers.q,black.q $FILE.sge.sh");
+    exit;
+} else {
+    system("sh $FILE.tmp.sh");
+}
+#unlink "$FILE.tmp.sh";
+#unlink "$FILE.sge.sh";
+
+########################################
+
+print STDERR "Dumping fasta\n";
+if (! -e "$FILE.fasta") {
+    my $cmd;
+
+    $cmd .= "tigStore";
+    $cmd .= " -g ../$prefix.gkpStore";
+    $cmd .= " -t $FILE.tigStore 2";
+    $cmd .= " -U -d consensus";
+    $cmd .= "> $FILE.fasta";
+
+    system($cmd);
+}
+
+print STDERR "Dumping layout\n";
+if (! -e "$FILE.layout") {
+    my $cmd;
+
+    $cmd .= "tigStore";
+    $cmd .= " -g ../$prefix.gkpStore";
+    $cmd .= " -t $FILE.tigStore 2";
+    $cmd .= " -U -d layout";
+    $cmd .= "> $FILE.layout";
+
+    system($cmd);
+}
+
+########################################
+
+print STDERR "Running nucmer\n";
+if (! -e "$FILE.delta") {
+    my $cmd;
+
+    $cmd .= "nucmer";
+    $cmd .= " --maxmatch --coords -p $FILE";
+    $cmd .= " $REF";
+    $cmd .= " $FILE.fasta";
+
+    system($cmd);
+}
+
+if (! -e "$FILE.png") {
+    my $cmd;
+
+    $cmd .= "mummerplot";
+    $cmd .= " --layout --filter -p $FILE -t png";
+    #$cmd .= " --layout          -p $FILE -t png";
+    $cmd .= " $FILE.delta";
+
+    system($cmd);
+}
---
+++ src/AS_BAT/bogusness-run.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $prefix = "test.004.buildUnitigs";
+my $IDEAL  = "../../ideal/porphyromonas_gingivalis_w83.flx.fragment.E9T0MN001.ideal.intervals";
+
+$prefix = $ARGV[0]  if (scalar(@ARGV) > 0);
+$IDEAL  = $ARGV[1]  if (scalar(@ARGV) > 1);
+
+if (! -e "$prefix.bogus.out") {
+    my $cmd;
+
+    $cmd  = "bogusness \\\n";
+    $cmd .= " -nucmer $prefix.coords \\\n";
+    $cmd .= " -ideal  $IDEAL \\\n";
+    $cmd .= " > $prefix.bogusness.out";
+
+    system($cmd);
+}
+
+my $lastUtg;
+
+open(F, "sort -k10n -k18n < $prefix.bogusness.out |") or die;
+open(O, "> $prefix.bogusness.wiki") or die;
+
+print O "{| class=\"wikitable\" border=1\n";
+print O "! unitig !! align num !! utg coords !! gen coords !! status !! ideal type !! ideal index !! ideal coords !! length !! utg cov !! ideal cov !! annotation\n";
+print O "|-\n";
+
+while (<F>) {
+    chomp;
+
+    if (m/^\|\s(utg\d+)\s/) {
+        if (!defined($lastUtg)) {
+            $lastUtg = $1;
+        }
+        if ($lastUtg ne $1) {
+            print O "| colspan=12 bgcolor=#666666 |\n";
+            print O "|-\n";
+            $lastUtg = $1;
+        }
+
+        s!BEGINSin  \|\| UNIQ!bgcolor="FireBrick" \| BEGINSin  \|\| bgcolor="FireBrick" \| UNIQ!;
+        s!ENDSin    \|\| UNIQ!bgcolor="FireBrick" \| ENDSin    \|\| bgcolor="FireBrick" \| UNIQ!;
+
+        s!BEGINSin  \|\| REPT!bgcolor="ForestGreen" \| BEGINSin  \|\| bgcolor="ForestGreen" \| REPT!;
+        s!ENDSin    \|\| REPT!bgcolor="ForestGreen" \| ENDSin    \|\| bgcolor="ForestGreen" \| REPT!;
+
+        s!CONTAINED \|\| UNIQ!bgcolor="Indigo" \| CONTAINED \|\| bgcolor="Indigo" \| UNIQ!;
+
+        s!CONTAINED \|\| REPT!bgcolor="SteelBlue" \| CONTAINED \|\| bgcolor="SteelBlue" \| REPT!;
+
+        print O "$_\n";
+        print O "|-\n";
+    }
+}
+
+print O "|}\n";
+
+close(O);
+close(F);
