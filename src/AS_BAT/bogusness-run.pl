3cb025be9f1b470068e11edaf4590ad22d94d173
Brian P. Walenz <thebri@gmail.com>
2011-05-06 17:34:57 +0000

    Streamline execution, build jbrowse instance.

--- src/AS_BAT/bogusness-run.pl
+++ src/AS_BAT/bogusness-run.pl
@@ -6,6 +6,7 @@ my $prefix = undef;    #  output file name prefix
 my $SEQ    = undef;    #  input sequences, fasta
 my $REF    = undef;    #  input reference, fasta
 my $IDEAL  = undef;    #  input ideal, output from bogus, *.ideal.intervals
+my $IGFF3  = undef;    #  input ideal, output from bogus, *.ideal.gff3
 
 while (scalar(@ARGV) > 0) {
     my $arg = shift @ARGV;
@@ -31,12 +32,21 @@ if (!defined($prefix) || !defined($SEQ) || !defined($REF) || !defined($IDEAL)) {
     exit(1);
 }
 
+if ($IDEAL =~ m/(.*).intervals/) {
+    $IDEAL = "$1.intervals";
+    $IGFF3 = "$1.gff3"
+}
+die "IDEAL '$IDEAL' not found.\n"  if (! -e "$IDEAL");
+die "IGFF3 '$IGFF3' not found.\n"  if (! -e "$IGFF3");
+
+
+
 #
 #  Map assembly to reference
 #
 
-print STDERR "Running nucmer\n";
 if (! -e "$prefix.delta") {
+    print STDERR "Running nucmer\n";
     my $cmd;
 
     $cmd .= "nucmer";
@@ -48,6 +58,7 @@ if (! -e "$prefix.delta") {
 }
 
 if (! -e "$prefix.png") {
+    print STDERR "Running mummerplot\n";
     my $cmd;
 
     $cmd .= "mummerplot";
@@ -63,6 +74,7 @@ if (! -e "$prefix.png") {
 #
 
 if (! -e "$prefix.bogusness.out") {
+    print STDERR "Running bogusness\n";
     my $cmd;
 
     $cmd  = "bogusness \\\n";
@@ -79,49 +91,49 @@ if (! -e "$prefix.bogusness.out") {
 #  Build wiki-friendly output
 #
 
-my $lastUtg;
+if (! -e "$prefix.bogusness.wiki") {
+    my $lastUtg;
 
-open(F, "sort -k10n -k18n < $prefix.bogusness.out |") or die "Failed to read sort output.\n";
-open(O, "> $prefix.bogusness.wiki") or die "Failed to open '$prefix.bogusness.wiki' for writing.\n";
+    open(F, "sort -k10n -k18n < $prefix.bogusness.out |") or die "Failed to read sort output.\n";
+    open(O, "> $prefix.bogusness.wiki") or die "Failed to open '$prefix.bogusness.wiki' for writing.\n";
 
-print O "{| class=\"wikitable\" border=1\n";
-print O "! unitig !! align num !! utg coords !! gen coords !! status !! ideal type !! ideal index !! ideal coords !! length !! utg cov !! ideal cov !! annotation\n";
-print O "|-\n";
+    print O "{| class=\"wikitable\" border=1\n";
+    print O "! unitig !! align num !! utg coords !! gen coords !! status !! ideal type !! ideal index !! ideal coords !! length !! utg cov !! ideal cov !! annotation\n";
+    print O "|-\n";
 
-while (<F>) {
-    chomp;
+    while (<F>) {
+        chomp;
 
-    if (m/^\|\s(utg\d+)\s/) {
-        if (!defined($lastUtg)) {
-            $lastUtg = $1;
-        }
-        if ($lastUtg ne $1) {
-            print O "| colspan=12 bgcolor=#666666 |\n";
+        if (m/^\|\s(utg\d+)\s/) {
+            if (!defined($lastUtg)) {
+                $lastUtg = $1;
+            }
+            if ($lastUtg ne $1) {
+                print O "| colspan=12 bgcolor=#666666 |\n";
+                print O "|-\n";
+                $lastUtg = $1;
+            }
+
+            s!BEGINSin  \|\| UNIQ!bgcolor="FireBrick" \| BEGINSin  \|\| bgcolor="FireBrick" \| UNIQ!;
+            s!ENDSin    \|\| UNIQ!bgcolor="FireBrick" \| ENDSin    \|\| bgcolor="FireBrick" \| UNIQ!;
+
+            s!BEGINSin  \|\| REPT!bgcolor="ForestGreen" \| BEGINSin  \|\| bgcolor="ForestGreen" \| REPT!;
+            s!ENDSin    \|\| REPT!bgcolor="ForestGreen" \| ENDSin    \|\| bgcolor="ForestGreen" \| REPT!;
+
+            s!CONTAINED \|\| UNIQ!bgcolor="Indigo" \| CONTAINED \|\| bgcolor="Indigo" \| UNIQ!;
+
+            s!CONTAINED \|\| REPT!bgcolor="SteelBlue" \| CONTAINED \|\| bgcolor="SteelBlue" \| REPT!;
+
+            print O "$_\n";
             print O "|-\n";
-            $lastUtg = $1;
         }
-
-        s!BEGINSin  \|\| UNIQ!bgcolor="FireBrick" \| BEGINSin  \|\| bgcolor="FireBrick" \| UNIQ!;
-        s!ENDSin    \|\| UNIQ!bgcolor="FireBrick" \| ENDSin    \|\| bgcolor="FireBrick" \| UNIQ!;
-
-        s!BEGINSin  \|\| REPT!bgcolor="ForestGreen" \| BEGINSin  \|\| bgcolor="ForestGreen" \| REPT!;
-        s!ENDSin    \|\| REPT!bgcolor="ForestGreen" \| ENDSin    \|\| bgcolor="ForestGreen" \| REPT!;
-
-        s!CONTAINED \|\| UNIQ!bgcolor="Indigo" \| CONTAINED \|\| bgcolor="Indigo" \| UNIQ!;
-
-        s!CONTAINED \|\| REPT!bgcolor="SteelBlue" \| CONTAINED \|\| bgcolor="SteelBlue" \| REPT!;
-
-        print O "$_\n";
-        print O "|-\n";
     }
+
+    print O "|}\n";
+
+    close(O);
+    close(F);
 }
-
-print O "|}\n";
-
-close(O);
-close(F);
-
-exit;
 
 #
 #  Attempt to create a jbrowse instance.
@@ -133,10 +145,32 @@ exit;
 #  cd ../..
 #
 
-#open(F, "> $prefix.jbrowse.sh") or die "Failed to open '$prefix.jbrowse.sh' for writing.\n";
-#print F "#!/bin/sh\n";
-#print F "cd $prefix\n";
-#print F "tar -xf ../jbrowse.tar\n";
-#print F "mv jbrowse/* .\n";
-#print F "rmdir jbrowse\n";
-#print f "ln -s ../bogusness-ctg.gff3 
+system("mkdir $prefix.jbrowse");
+
+
+open(F, "> $prefix.jbrowse/create.sh") or die "Failed to open '$prefix.jbrowse/create.sh' for writing.\n";
+print F "#!/bin/sh\n";
+print F "\n";
+print F "cd $prefix.jbrowse\n";
+print F "\n";
+print F "unzip -q /work/wgs/src/AS_BAT/jbrowse-1.2.1.zip\n";
+print F "\n";
+print F "mv     jbrowse-1.2.1/* .\n";
+print F "rm -rf jbrowse-1.2.1\n";
+print F "\n";
+print F "cp -p /work/wgs/src/AS_BAT/bogus-genome.css  genome.css\n";
+print F "cp -p /work/wgs/src/AS_BAT/bogus-genome.json bogus.json\n";
+print F "\n";
+print F "ln -s ../$prefix.gff3\n";
+print F "\n";
+print F "ln -s ../$IGFF3 .\n";
+print F "\n";
+print F "echo bin/prepare-refseqs.pl -fasta ../$REF\n";
+print F "bin/prepare-refseqs.pl -fasta ../$REF\n";
+print F "\n";
+print F "echo bin/biodb-to-json.pl   --conf bogus.json\n";
+print F "bin/biodb-to-json.pl   --conf bogus.json\n";
+print F "\n";
+close(F);
+
+system("sh $prefix.jbrowse/create.sh");
