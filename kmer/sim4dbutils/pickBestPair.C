e3510bd0527ce47689646c1051ea8680073fac52
Brian P. Walenz <thebri@gmail.com>
2012-07-18 06:47:26 +0000

    Util to report possible pairings between two mapped files.

---
+++ kmer/sim4dbutils/pickBestPair.C
@@ -0,0 +1,205 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "bio.h"
+#include "sim4.H"
+
+#include <vector>
+using namespace std;
+
+struct mapResult {
+  u32bit seqIdx;
+  char   seqName[32];
+
+  u32bit refIdx;
+  char   refName[32];
+  u32bit refBgn;
+  u32bit refEnd;
+
+  bool   forward;
+};
+
+
+
+
+bool
+readMR(FILE *in, mapResult &mr) {
+  static  char           line[1024];
+  static  splitToWords   W;
+
+  fgets(line, 1024, in);
+
+  if (feof(in))
+    return(false);
+
+  W.split(line);
+
+  assert(strlen(W[0]) < 32);
+  assert(strlen(W[6]) < 32);
+
+  mr.seqIdx = W(1);
+  mr.refIdx = W(7);
+
+  mr.refBgn = W(8);
+  mr.refEnd = W(9);
+      
+  mr.forward = (W(4) < W(5)) ? true : false;
+
+  strcpy(mr.seqName, W[0]);
+  strcpy(mr.refName, W[6]);
+
+  return(true);
+}
+
+
+int
+main(int argc, char **argv) {
+  char  *in1 = NULL;
+  char  *in2 = NULL;
+  char  *out = NULL;
+
+  int arg = 1;
+  int err = 0;
+  while (arg < argc) {
+    if      (strcmp(argv[arg], "-1") == 0)
+      in1  = argv[++arg];
+
+    else if (strcmp(argv[arg], "-2") == 0)
+      in2  = argv[++arg];
+
+    else if (strcmp(argv[arg], "-o") == 0)
+      out  = argv[++arg];
+
+    else
+      err++;
+
+    arg++;
+  }
+  if ((in1 == NULL) || (in2 == NULL) || (out == NULL)) {
+    fprintf(stderr, "usage: %s -1 in1.extent -2 in2.extent -o prefix\n", argv[0]);
+    exit(1);
+  }
+
+  vector<mapResult>   mr1;
+  vector<mapResult>   mr2;
+
+  mapResult           mr;
+
+  FILE  *IN1 = fopen(in1, "r");
+  FILE  *IN2 = fopen(in2, "r");
+
+  while (readMR(IN1, mr) == true)
+    mr1.push_back(mr);
+
+  while (readMR(IN2, mr) == true)
+    mr2.push_back(mr);
+
+  fclose(IN1);
+  fclose(IN2);
+
+  FILE  *LOG = NULL;
+  FILE  *STA = NULL;
+
+  char   name[10240];
+
+  sprintf(name, "%s.pairLog", out);
+  LOG = fopen(name, "w");
+
+  sprintf(name, "%s.stats", out);
+  STA = fopen(name, "w");
+
+  u32bit   mr1bgn = 0;
+  u32bit   mr1end = 0;
+  u32bit   mr1END = mr1.size();
+
+  u32bit   mr2bgn = 0;
+  u32bit   mr2end = 0;
+  u32bit   mr2END = mr2.size();
+
+  while ((mr1bgn < mr1END) && (mr2bgn < mr2END)) {
+
+    if (mr1[mr1bgn].seqIdx < mr2[mr2bgn].seqIdx)
+      mr1bgn++;
+
+    if (mr1[mr1bgn].seqIdx > mr2[mr2bgn].seqIdx)
+      mr2bgn++;
+
+    mr1end = mr1bgn + 1;
+    mr2end = mr2bgn + 1;
+
+    while (mr1[mr1bgn].seqIdx == mr1[mr1end].seqIdx)
+      mr1end++;
+
+    while (mr2[mr2bgn].seqIdx == mr2[mr2end].seqIdx)
+      mr2end++;
+
+    //  Group of reads from mr1bgn-mr1end and mr2bgn-mr2end need to be compared.
+
+    for (u32bit i1=mr1bgn; i1<mr1end; i1++) {
+      for (u32bit i2=mr2bgn; i2<mr2end; i2++) {
+        if (mr1[i1].refIdx != mr2[i2].refIdx)
+          continue;
+
+        //validParis++;
+
+        u32bit  df  = 0;
+        u32bit  dr  = 0;
+        char    ori = 'X';
+
+        if (mr1[i1].refBgn < mr2[i2].refEnd)
+          df = mr2[i2].refEnd - mr1[i1].refBgn;
+
+        if (mr2[i2].refBgn < mr1[i1].refEnd)
+          dr = mr1[i1].refEnd - mr2[i2].refBgn;
+
+        assert(df + dr > 0);
+
+        if (df > dr) {
+          if ((mr1[i1].forward == true)  && (mr2[i2].forward == true))
+            ori = 'N';
+          if ((mr1[i1].forward == true)  && (mr2[i2].forward == false))
+            ori = 'I';
+          if ((mr1[i1].forward == false) && (mr2[i2].forward == true))
+            ori = 'O';
+          if ((mr1[i1].forward == false) && (mr2[i2].forward == false))
+            ori = 'A';
+
+          fprintf(LOG, "%c "u32bitFMT" "u32bitFMT" %s ("u32bitFMT","u32bitFMT") "u32bitFMT" %s ("u32bitFMT","u32bitFMT") "u32bitFMT" %s\n",
+                  ori,
+                  df,
+                  mr1[i1].seqIdx, mr1[i1].seqName, mr1[i1].refBgn, mr1[i1].refEnd,
+                  mr2[i2].seqIdx, mr2[i2].seqName, mr2[i2].refBgn, mr2[i2].refEnd,
+                  mr1[i1].refIdx, mr1[i1].refName);
+
+        } else {
+          if ((mr2[i2].forward == true)  && (mr1[i1].forward == true))
+            ori = 'N';
+          if ((mr2[i2].forward == true)  && (mr1[i1].forward == false))
+            ori = 'I';
+          if ((mr2[i2].forward == false) && (mr1[i1].forward == true))
+            ori = 'O';
+          if ((mr2[i2].forward == false) && (mr1[i1].forward == false))
+            ori = 'A';
+
+          fprintf(LOG, "%c "u32bitFMT" "u32bitFMT" %s ("u32bitFMT","u32bitFMT") "u32bitFMT" %s ("u32bitFMT","u32bitFMT") "u32bitFMT" %s\n",
+                  ori,
+                  dr,
+                  mr2[i2].seqIdx, mr2[i2].seqName, mr2[i2].refBgn, mr2[i2].refEnd,
+                  mr1[i1].seqIdx, mr1[i1].seqName, mr1[i1].refBgn, mr1[i1].refEnd,
+                  mr2[i2].refIdx, mr2[i2].refName);
+        }
+      }
+    }
+
+    mr1bgn = mr1end;
+    mr2bgn = mr2end;
+  }
+
+  fprintf(STA, "alignments:  "u32bitFMT" "u32bitFMT"\n", mr1END, mr2END);
+
+  fclose(LOG);
+  fclose(STA);
+
+  exit(0);
+}
