b8b9c5ea7aa7ab7648b904b1e84cbe921670f13b
Brian P. Walenz <thebri@gmail.com>
2013-11-25 17:33:05 +0000

    Use a reduced matrix for dynamic programming.  This version extends the current implementation to run both the full and reduced matrix at the same time.

--- src/AS_ALN/AS_ALN_bruteforcedp.C
+++ src/AS_ALN/AS_ALN_bruteforcedp.C
@@ -27,10 +27,10 @@ static const char *rcsid = "$Id$";
 
 #undef DEBUG
 
-#define MATCH            0
-#define GAPA             1
-#define GAPB             2
-#define STOP             3
+#define STOP             0  //  IMPORTANT; default is STOP
+#define MATCH            1
+#define GAPA             2
+#define GAPB             3
 
 //  3, -2, -2 original
 //  5, -4, -3 suggested
@@ -84,6 +84,21 @@ alignLinker(char           *alignA,
 
   memset(a, 0, sizeof(alignLinker_s));
 
+  dpActions  ACT;
+
+#define NEWSCORE
+
+#ifdef NEWSCORE
+  uint32     SCO[4][AS_READ_MAX_NORMAL_LEN * 2];
+
+  memset(SCO, 0, sizeof(uint32) * AS_READ_MAX_NORMAL_LEN * 2);
+
+  uint32    *lastCol = SCO[0];
+  uint32    *thisCol = SCO[1];
+  uint32    *iFinal  = SCO[2];
+  uint32    *jFinal  = SCO[3];
+#endif
+
   if ((lenA > AS_READ_MAX_NORMAL_LEN) || (lenB > AS_READ_MAX_NORMAL_LEN)) {
     fprintf(stderr, "alignLinker()-- Reads too long.  %d or %d > %d\n", lenA, lenB, AS_READ_MAX_NORMAL_LEN);
     return;
@@ -97,6 +112,27 @@ alignLinker(char           *alignA,
 
   //  Set the edges.
 
+#ifdef NEWSCORE
+  for (int32 i=ibgn-1; i<=iend+1; i++) {
+    iFinal[i]  = DP_ZERO;
+  }
+
+  for (int32 j=jbgn-1; j<=jend+1; j++) {
+    lastCol[j] = DP_ZERO;
+    thisCol[j] = DP_ZERO;
+    jFinal[j]  = DP_ZERO;
+  }
+
+  //ACT.set(i, jbgn-1, STOP);  //  lastCol
+  //ACT.set(i, jbgn,   STOP);  //  thisCol
+  //ACT.set(i, jend,   STOP);  //  thisCol
+
+  //assert(ACT.get(i, jbgn-1) == STOP);
+  //assert(ACT.get(i, jbgn  ) == STOP);
+  //assert(ACT.get(i, jend  ) == STOP);
+
+  //#else
+
   for (int32 i=ibgn-1, j=jbgn-1; i<=iend+1; i++) {
     M[i][j].score  = DP_ZERO;
     M[i][j].action = STOP;
@@ -106,6 +142,7 @@ alignLinker(char           *alignA,
     M[i][j].score  = DP_ZERO;
     M[i][j].action = STOP;
   }
+#endif
 
   //  Catch an invalid use case
 
@@ -130,9 +167,26 @@ alignLinker(char           *alignA,
 
       //  Pick the max of these
 
-      int ul = M[i-1][j-1].score + ((stringA[i-1] == stringB[j-1]) ? MATCHSCORE : MISMATCHSCORE);
-      int lf = M[i-1][j].score + GAPSCORE;  //  Gap in B
-      int up = M[i][j-1].score + GAPSCORE;  //  Gap in A
+      //  i-1 -> lastCol
+      //  i   -> thisCol
+
+#ifdef NEWSCORE
+      uint32 ul = lastCol[j-1] + ((stringA[i-1] == stringB[j-1]) ? MATCHSCORE : MISMATCHSCORE);
+      uint32 lf = lastCol[j  ] + GAPSCORE;  //  Gap in B
+      uint32 up = thisCol[j-1] + GAPSCORE;  //  Gap in A
+#else
+      uint32 ul = M[i-1][j-1].score + ((stringA[i-1] == stringB[j-1]) ? MATCHSCORE : MISMATCHSCORE);
+      uint32 lf = M[i-1][j  ].score + GAPSCORE;  //  Gap in B
+      uint32 up = M[i  ][j-1].score + GAPSCORE;  //  Gap in A
+#endif
+
+      assert(ul == M[i-1][j-1].score + ((stringA[i-1] == stringB[j-1]) ? MATCHSCORE : MISMATCHSCORE));
+      assert(lf == M[i-1][j  ].score + GAPSCORE);
+      assert(up == M[i  ][j-1].score + GAPSCORE);
+
+      assert(thisCol[j-1] == M[i  ][j-1].score);
+      assert(lastCol[j-1] == M[i-1][j-1].score);
+      assert(lastCol[j  ] == M[i-1][j  ].score);
 
       //  When computing unitig consensus, the N letter is used to indicate a gap.  This might be a
       //  match, or it might be a mismatch.  We just don't know.  Don't count it as anything.  This
@@ -142,12 +196,51 @@ alignLinker(char           *alignA,
         if ((stringA[i-1] == 'N') ||
             (stringA[i-1] == 'n') ||
             (stringB[j-1] == 'N') ||
-            (stringB[j-1] == 'n'))
+            (stringB[j-1] == 'n')) {
+#ifdef NEWSCORE
+          ul = lastCol[j-1] + MATCHSCORE;
+#else
           ul = M[i-1][j-1].score + MATCHSCORE;
+#endif
+        }
 
       //  For unitig consensus, if the consensus sequence (stringA) is lowercase, count it as a
       //  match, otherwise ignore the gap it induces in stringB.
       //  
+#warning stop complaing that this is ambiguous, please.
+#ifdef NEWSCORE
+      if (stringA[i-1] == 'a')
+        if (stringB[j-1] == 'A')
+          ul = lastCol[j-1] + MATCHSCORE;
+        else {
+          ul = lastCol[j-1];
+          lf = lastCol[j];
+        }
+
+      if (stringA[i-1] == 'c')
+        if (stringB[j-1] == 'C')
+          ul = lastCol[j-1] + MATCHSCORE;
+        else {
+          ul = lastCol[j-1];
+          lf = lastCol[j];
+        }
+
+      if (stringA[i-1] == 'g')
+        if (stringB[j-1] == 'G')
+          ul = lastCol[j-1] + MATCHSCORE;
+        else {
+          ul = lastCol[j-1];
+          lf = lastCol[j];
+        }
+
+      if (stringA[i-1] == 't')
+        if (stringB[j-1] == 'T')
+          ul = lastCol[j-1] + MATCHSCORE;
+        else {
+          ul = lastCol[j-1];
+          lf = lastCol[j];
+        }
+#else
       if (stringA[i-1] == 'a')
         if (stringB[j-1] == 'A')
           ul = M[i-1][j-1].score + MATCHSCORE;
@@ -179,33 +272,87 @@ alignLinker(char           *alignA,
           ul = M[i-1][j-1].score;
           lf = M[i-1][j].score;
         }
+#endif
+
+
+#ifdef NEWSCORE
+      if (endToEnd) {
+        //  Set score to the smallest value possible; we will then ALWAYS pick an action below.
+        //
+        thisCol[j]  = 0;
+        ACT.set(i, j, MATCH);
+        assert(ACT.get(i, j) == MATCH);
+      } else {
+        //  (i,j) is the beginning of a subsequence, our default behavior.  If we're looking for
+        //  local alignments, make the alignment stop here.
+        //
+        thisCol[j]  = DP_ZERO;
+        ACT.set(i, j, STOP);
+        assert(ACT.get(i, j) == STOP);
+      }
+
+      if (thisCol[j] < ul) {
+        thisCol[j]  = ul;
+        ACT.set(i, j, MATCH);
+        assert(ACT.get(i, j) == MATCH);
+      }
+
+      if (thisCol[j] < lf) {
+        thisCol[j]  = lf;
+        ACT.set(i, j, GAPB);
+        assert(ACT.get(i, j) == GAPB);
+      }
+
+      if (thisCol[j] < up) {
+        thisCol[j]  = up;
+        ACT.set(i, j, GAPA);
+        assert(ACT.get(i, j) == GAPA);
+      }
+
+      if (scoreMax < thisCol[j]) {
+        scoreMax  = thisCol[j];
+        endI = curI = i;
+        endJ = curJ = j;
+      }
+
+//#else
 
       if (endToEnd) {
         //  Set score to the smallest value possible; we will then ALWAYS pick an action below.
         //
         M[i][j].score  = 0;
         M[i][j].action = MATCH;
+        ACT.set(i, j, MATCH);
+        assert(ACT.get(i, j) == MATCH);
       } else {
         //  (i,j) is the beginning of a subsequence, our default behavior.  If we're looking for
         //  local alignments, make the alignment stop here.
         //
         M[i][j].score  = DP_ZERO;
         M[i][j].action = STOP;
+        ACT.set(i, j, STOP);
+        assert(ACT.get(i, j) == STOP);
       }
 
       if (M[i][j].score < ul) {
         M[i][j].score  = ul;
         M[i][j].action = MATCH;
+        ACT.set(i, j, MATCH);
+        assert(ACT.get(i, j) == MATCH);
       }
 
       if (M[i][j].score < lf) {
         M[i][j].score  = lf;
         M[i][j].action = GAPB;
+        ACT.set(i, j, GAPB);
+        assert(ACT.get(i, j) == GAPB);
       }
 
       if (M[i][j].score < up) {
         M[i][j].score  = up;
         M[i][j].action = GAPA;
+        ACT.set(i, j, GAPA);
+        assert(ACT.get(i, j) == GAPA);
       }
 
       if (scoreMax < M[i][j].score) {
@@ -213,12 +360,61 @@ alignLinker(char           *alignA,
         endI = curI = i;
         endJ = curJ = j;
       }
-    }
-  }
+#endif
+
+#ifdef NEWSCORE
+      if (j == jend)
+        jFinal[i] = thisCol[j];
+      if (i == iend)
+        iFinal[j] = thisCol[j];  //  Not really needed (just a copy of thisCol), but simplifies later code
+#endif
+    }  //  Over j
+
+
+#ifdef NEWSCORE
+    //  Swap the columns
+    uint32  *tc = thisCol;
+    thisCol = lastCol;
+    lastCol = tc;
+#endif
+  }  //  Over i
 
   //  If we're not looking for local alignments, scan the end points for the best value.  If we are
   //  looking for local alignments, we've already found and remembered the best end point.
+  //
+  //  lastCol - [iend][]
+  //  jFinal  - [][jend]
+  //
 
+#ifdef NEWSCORE
+  if (endToEnd) {
+    //fprintf(stderr, "ENDTOEND  curI %u curJ %u\n", curI, curJ);
+    scoreMax    = 0;
+    endI = curI = 0;
+    endJ = curJ = 0;
+
+    for (int32 i=ibgn, j=jend; i<=iend; i++) {
+      if (scoreMax < jFinal[i]) {
+        scoreMax = jFinal[i];
+        endI = curI = i;
+        endJ = curJ = j;
+        //fprintf(stderr, "IscoreMax = %d at %d,%d\n", scoreMax - DP_ZERO, i, j);
+      }
+    }
+
+    for (int32 j=jbgn, i=iend; j<=jend; j++) {
+      if (scoreMax < iFinal[j]) {
+        scoreMax = iFinal[j];
+        endI = curI = i;
+        endJ = curJ = j;
+        //fprintf(stderr, "JscoreMax = %d at %d,%d\n", scoreMax - DP_ZERO, i, j);
+      }
+    }
+
+    //  Not sure what this is for.
+    //M[endI][endJ].score = 0;
+  }
+#else
   if (endToEnd) {
     scoreMax    = 0;
     endI = curI = 0;
@@ -244,6 +440,9 @@ alignLinker(char           *alignA,
 
     M[endI][endJ].score = 0;
   }
+#endif
+
+  //fprintf(stderr, "FINAL  curI %u curJ %u\n", curI, curJ);
 
   int32  alignLen  = 0;
 
@@ -255,7 +454,25 @@ alignLinker(char           *alignA,
   int32  terminate = 0;
 
   while (terminate == 0) {
-    switch (M[curI][curJ].action) {
+#ifndef NEWSCORE
+    if (M[curI][curJ].action != ACT.get(curI, curJ)) {
+      fprintf(stderr, "ACTION MISMATCH at curI=%u curJ=%u -- correct %u bits %lu\n",
+              curI, curJ,
+              M[curI][curJ].action,
+              ACT.get(curI, curJ));
+    }
+    assert(M[curI][curJ].action == ACT.get(curI, curJ));
+#endif
+
+#ifdef NEWSCORE
+    uint32 act = ACT.get(curI, curJ);
+#else
+    uint32 act = M[curI][curJ].action;
+#endif
+
+    //fprintf(stderr, "ACTION %2u curI %u curJ %u\n", act, curI, curJ);
+
+    switch (act) {
       case STOP:
         terminate = 1;
         break;
