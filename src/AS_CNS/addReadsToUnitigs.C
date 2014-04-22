1a0a48d4bb25f82582c548d529094cd22ac24144
Brian P. Walenz <thebri@gmail.com>
2014-04-22 15:49:33 +0000

    Require fastqUIDmap to convert read names to IIDs.

--- src/AS_CNS/addReadsToUnitigs.C
+++ src/AS_CNS/addReadsToUnitigs.C
@@ -31,34 +31,36 @@ const char *mainid = "$Id:  $";
 #include "MultiAlignment_CNS_private.H"
 
 #include <vector>
+#include <string>
+#include <map>
 
 using namespace std;
 
 class readMap {
 public:
   readMap() {
-    rIID = UINT32_MAX;
+    good = false;
+    proc = false;
+
     rFWD = false;
+    rIID = UINT32_MAX;
     rCNT = 0;
 
     tIID = UINT32_MAX;
     tBGN = 0;
     tEND = 0;
-
-    good = false;
-    proc = false;
   };
 
-  uint32  rIID;
+  bool    good;
+  bool    proc;
+
   bool    rFWD;
+  uint32  rIID;
   uint32  rCNT;
 
   uint32  tIID;
   uint32  tBGN;
   uint32  tEND;
-
-  bool    good;
-  bool    proc;
 };
 
 
@@ -81,10 +83,24 @@ main(int argc, char **argv) {
   bool   doConsensus = false;
   bool   doModify    = true;
 
+#ifdef UNFINISHED_ADD_TO_SINGLETON
+  bool   doPlaceUnmapped  = true;
+  bool   doDeleteUnmapped = false;
+#endif
+
   int32  numFailures = 0;
   int32  numSkipped  = 0;
 
   bool   showResult = false;
+
+  char               *lookupFile = NULL;
+  map<string,uint32>  lookupIID;
+
+#ifdef UNFINISHED_ADD_TO_SINGLETON
+  vector<bool>        iidInTig;       //  true if the read is already in a unitig
+  vector<uint32>      iidInTigByLib;  //  count of reads in tigs, by library
+  vector<uint32>      iidInLib;       //  count of reads, by library
+#endif
 
   bool   loadall = false;
 
@@ -116,8 +132,11 @@ main(int argc, char **argv) {
         fprintf(stderr, "invalid tigStore version (-t store version) '-t %s %s'.\n", argv[arg-1], argv[arg]), exit(1);
 
     } else if (strcmp(argv[arg], "-m") == 0) {
-      while (AS_UTL_fileExists(argv[arg+1], false, false) == true)
+      while ((argv[arg+1] != NULL) && (AS_UTL_fileExists(argv[arg+1], false, false) == true))
         alignMapNames.push_back(argv[++arg]);
+
+    } else if (strcmp(argv[arg], "-lookup") == 0) {
+      lookupFile = argv[++arg];
 
     } else if (strcmp(argv[arg], "-r") == 0) {
       doConsensus = true;
@@ -128,11 +147,11 @@ main(int argc, char **argv) {
     } else if (strcmp(argv[arg], "-V") == 0) {
       VERBOSE_MULTIALIGN_OUTPUT++;
 
-    } else if (strcmp(argv[arg], "-n") == 0) {
-      doModify = false;
-
     } else if (strcmp(argv[arg], "-loadall") == 0) {
       loadall = true;
+
+    } else if (strcmp(argv[arg], "-n") == 0) {
+      doModify = false;
 
     } else {
       err++;
@@ -146,19 +165,112 @@ main(int argc, char **argv) {
     err++;
   if (alignMapNames.size() == 0)
     err++;
+  if (lookupFile == NULL)
+    err++;
   if (err) {
     fprintf(stderr, "usage: %s -g gkpStore -t tigStore version -m coords\n", argv[0]);
     fprintf(stderr, "  -g gkpStore           gatekeeper store\n");
     fprintf(stderr, "  -t tigStore version   tigStore and version to modify\n");
+    fprintf(stderr, "\n");
     fprintf(stderr, "  -m map-file           input map coords\n");
-    fprintf(stderr, "  -r                    rebuild consensus, including the new reads\n");
-    fprintf(stderr, "  -v                    consensus: show result\n");
-    fprintf(stderr, "  -V                    consensus: verbose\n");
+    fprintf(stderr, "  -M fastqUIDmap        gatekeeper output fastqUIDmap for read name to IID translation\n");
+    fprintf(stderr, "\n");
+#if 0
+    fprintf(stderr, "unmapped reads:  default is to promote to singleton unitigs\n");
+    fprintf(stderr, "  -U                    leave unmapped reads alone (will crash CGW)\n");
+    fprintf(stderr, "  -D                    delete unmapped reads from gkpStore\n");
+#else
+    fprintf(stderr, "unmapped reads: all reads that are mapped and eligible for addition must be\n");
+    fprintf(stderr, "marked as deleted before running this program.  reads that are added will be\n");
+    fprintf(stderr, "undeleted.  reads that are not added will remain deleted.\n");
+#endif
+    fprintf(stderr, "\n");
+    fprintf(stderr, "consensus:  default is to not rebuild consensus\n");
+    fprintf(stderr, "  -r                    rebuild consensus including the new reads\n");
+    fprintf(stderr, "  -v                      show result\n");
+    fprintf(stderr, "  -V                      verbose\n");
+    fprintf(stderr, "  -loadall                load all reads in gkpStore into memory (faster consensus)\n");
+    fprintf(stderr, "\n");
     fprintf(stderr, "  -n                    do all the work, but discard the result\n");
-    fprintf(stderr, "  -loadall              load all reads in gkpStore into memory (faster consensus)\n");
+
+    if (gkpName == NULL)
+      fprintf(stderr, "ERROR: no gkpStore (-g) supplied.\n");
+    if (tigName == NULL)
+      fprintf(stderr, "ERROR: no tigStore (-t) supplied.\n");
+    if (alignMapNames.size() == 0)
+      fprintf(stderr, "ERROR: no map-file (-m) inputs supplied.\n");
+    if (lookupFile == NULL)
+      fprintf(stderr, "ERROR: no fasqUIDmap (-M) supplied.\n");
 
     exit(1);
   }
+
+
+  {
+    fprintf(stderr, "Loading Name to IID map from '%s'\n", lookupFile);
+
+    errno = 0;
+    FILE *LF = fopen(lookupFile, "r");
+    if (errno)
+      fprintf(stderr, "Failed to open fastqUIDmap '%s'\n", lookupFile);
+
+    char  LL[1024];
+    fgets(LL, 1024, LF);
+
+    while (!feof(LF)) {
+      chomp(LL);  //  Shouldn't be necessary, but splitToWords isn't doing it!
+
+      splitToWords  SW(LL);
+
+      if        (SW.numWords() == 3) {
+        lookupIID[string(SW[2])] = SW(1);
+
+      } else if (SW.numWords() == 6) {
+        lookupIID[string(SW[2])] = SW(1);
+        lookupIID[string(SW[5])] = SW(4);
+        //fprintf(stderr, "'%s' - %u -- '%s' - %u\n",
+        //        SW[2], SW(1), SW[5], SW(4));
+
+      } else {
+      }
+
+      fgets(LL, 1024, LF);
+    }
+
+    fprintf(stderr, "Loaded "F_SIZE_T" name to IIDs\n", lookupIID.size());
+  }
+
+
+#ifdef UNFINISHED_ADD_TO_SINGLETON
+  {
+    fprintf(stderr, "Loading tig placement from tigStore '%s'\n", tigName);
+
+    tigStore = new MultiAlignStore(tigName, tigVers, 0, 0, false, false, false);  //  Read only
+
+    for (uint32 ti=0; ti<tigStore->numUnitigs(); ti++) {
+      if (tigStore->isDeleted(ti, true))
+        continue;
+
+      MultiAlignT *ma = tigStore->loadMultiAlign(ti, true);
+
+      if (ma == NULL)
+        continue;
+
+      uint32   fiMax = GetNumIntMultiPoss(ma->f_list);
+
+      for (uint32 fi=0; fi<fiMax; fi++) {
+        IntMultiPos  *imp = GetIntMultiPos(ma->f_list, fi);
+
+        iidInTig[imp->ident] = true;
+      }
+
+      tigStore->unloadMultiAlign(ti, true);
+    }
+
+    delete tigStore;
+  }
+#endif
+
 
   //
   //  Load the alignment map.  The challenge here is to parse the unitig and read names
@@ -168,9 +280,14 @@ main(int argc, char **argv) {
   //    Alignments are in the convertToExtent -extended format
   //
 
+
   uint32  totAligns = 0;
+  uint32  totUnmap  = 0;
   uint32  totUnique = 0;
   uint32  totDups   = 0;
+
+  uint32  lowID     = UINT32_MAX;
+  uint32  maxID     = 0;
 
   for (uint32 an=0; an<alignMapNames.size(); an++) {
     uint32  numAligns = 0;
@@ -190,33 +307,47 @@ main(int argc, char **argv) {
     while (!feof(M)) {
       numAligns++;
 
-      uint32  cF = 1;
-
-      for (uint32 xx=0; !isspace(L[xx]); xx++)
-        if (L[xx] == ',') {
+      //  Some gatekeeper dumps contain NAME,IID.  Strip out the ,IID, BEFORE converting to words.
+      bool  cF = false;
+      for (uint32 xx=0; !isspace(L[xx]); xx++) {
+        if (L[xx] == ',')
+          cF = true;
+        if (cF == true)
           L[xx] = ' ';
-          cF = 0;
-        }
+      }
 
       splitToWords S(L);
       readMap      rm;
 
       rm.rIID = S(1);
-      rm.rFWD = (S(5-cF) < S(6-cF));
+      rm.rFWD = (S(4) < S(5));
       rm.rCNT = 1;
 
-      rm.tIID = atoi(S[7-cF] + 3);
-      rm.tBGN = S(9-cF);
-      rm.tEND = S(10-cF);
+      assert(S[6][0] == 'u');  //  Unitig must be from a tigStore dump, and will be
+      assert(S[6][1] == 't');  //  named 'utg#######'.
+      assert(S[6][2] == 'g');
+
+      rm.tIID = atoi(S[6] + 3);
+      rm.tBGN = S(8);
+      rm.tEND = S(9);
+
+      //  We no longer use the encoded IID, but always lookup the read name in the map.
+      rm.rIID = lookupIID[string(S[0])];
+
+      if (rm.rIID == 0)
+        fprintf(stderr, "FAILED to find IID for read '%s'\n", S[0]);
 
       //fprintf(stderr, "%d %d %d -- %d %d %d\n", rm.rIID, rm.rFWD, rm.rCNT, rm.tIID, rm.tBGN, rm.tEND);
 
-      if (RM.size() < rm.rIID)
+      if (RM.size() <= rm.rIID)
         RM.resize(2 * rm.rIID);
 
       rm.rCNT = RM[rm.rIID].rCNT + 1;
 
       RM[rm.rIID] = rm;
+
+      lowID = (lowID < rm.rIID) ? lowID : rm.rIID;
+      maxID = (maxID < rm.rIID) ? rm.rIID : maxID;
 
       fgets(L, 1024, M);
     }
@@ -228,36 +359,47 @@ main(int argc, char **argv) {
     totAligns += numAligns;
   }
 
-  for (uint32 xx=0; xx<RM.size(); xx++) {
+  for (uint32 xx=lowID; xx<=maxID; xx++) {
+    if (RM[xx].rCNT == 0)
+      totUnmap++;
     if (RM[xx].rCNT == 1)
       totUnique++;
     if (RM[xx].rCNT > 1)
       totDups++;
   }
 
-  fprintf(stderr, "Loaded %u aligns, %u reads had a unique alignment and %u reads had multiple aligns.\n",
-          totAligns, totUnique, totDups);
+  fprintf(stderr, "Loaded %u aligns from ID %u to %u, %u reads unmapped, %u reads had a unique alignment and %u reads had multiple aligns.\n",
+          totAligns, lowID, maxID, totUnmap, totUnique, totDups);
 
   //
   //  Update deletion status in gkpStore.  This processes every read, regardless.
   //
 
   fprintf(stderr, "Processing mate pairs, updating gkpStore.\n");
+  gkpStore = new gkStore(gkpName, false, true);  //  last arg - TRUE - writable
 
   uint32   unpaired    = 0;
   uint32   multiple    = 0;
   uint32   pairsToSame = 0;
   uint32   pairsToDiff = 0;
 
-  gkpStore = new gkStore(gkpName, FALSE, TRUE);  //  last arg - TRUE - writable
+#ifdef UNFINISHED_ADD_TO_SINGLETON
+  //  Need to process all reads, since we don't know where the first/last unmapped read is!
+  //  We could instead process from the first to last deleted read in gkpStore, or ask which
+  //  libraries were being added.
+  lowID = 1;
+  maxID = gkpStore->gkStore_getNumFragments();
+#endif
 
-  for (uint32 ff=0, mm=0; ff<RM.size(); ff++) {
+  for (uint32 ff=lowID, mm=lowID; ff<=maxID; ff++) {
     gkFragment  read;
     gkFragment  mate;
 
-    if (RM[ff].rCNT != 1)
-      //  Not mapped, mapped too much
-      continue;
+    //  I think this is just a short circuit of the two checks of 'one or both reads has too
+    //  few/many mappings' below.
+    //if (RM[ff].rCNT != 1)
+    //  //  Not mapped, mapped too much
+    //  continue;
 
     gkpStore->gkStore_getFragment(ff, &read, GKFRAGMENT_INF);
 
@@ -268,6 +410,7 @@ main(int argc, char **argv) {
       continue;
 
     if (mm < ff)
+      //  Already processed.
       continue;
 
     gkpStore->gkStore_getFragment(mm, &mate, GKFRAGMENT_INF);
@@ -316,8 +459,8 @@ main(int argc, char **argv) {
   //  Open stores.  gkpStore cannot be opened for writing, because then we can't loadall.
   //
 
-  gkpStore = new gkStore(gkpName, FALSE, FALSE);  //  last arg - FALSE - not writable
-  tigStore = new MultiAlignStore(tigName, tigVers, 0, 0, TRUE, TRUE, FALSE);  //  Write back to the same version
+  gkpStore = new gkStore(gkpName, false, false);                              //  last arg - false - not writable
+  tigStore = new MultiAlignStore(tigName, tigVers, 0, 0, true, true, false);  //  Write back to the same version
 
   if (loadall) {
     fprintf(stderr, "Loading all reads into memory.\n");
@@ -337,7 +480,7 @@ main(int argc, char **argv) {
         (RM[bb].proc == true))           //  already added
       continue;
 
-    MultiAlignT *ma = tigStore->loadMultiAlign(RM[bb].tIID, TRUE);
+    MultiAlignT *ma = tigStore->loadMultiAlign(RM[bb].tIID, true);
 
     uint32   ungapLength = GetMultiAlignUngappedLength(ma);
     uint32   gapLength   = GetMultiAlignLength(ma);
@@ -363,7 +506,7 @@ main(int argc, char **argv) {
 
       readsAdded++;
 
-      fprintf(stderr, "bb=%u ee=%u ADD frag %u to unitig %u at %u,%u (from ungapped %u,%u)\n",
+      fprintf(stdout, "bb=%u ee=%u ADD frag %u to unitig %u at %u,%u (from ungapped %u,%u)\n",
               bb, ee,
               RM[ee].rIID, RM[ee].tIID, bgn, end, RM[ee].tBGN, RM[ee].tEND);
 
@@ -408,7 +551,7 @@ main(int argc, char **argv) {
 
     if (doModify) {
       fprintf(stderr, "Updating unitig %u\n", ma->maID);
-      tigStore->insertMultiAlign(ma, TRUE, FALSE);
+      tigStore->insertMultiAlign(ma, true, false);
     }
   }
 
