1bd0eb4d58e0840e1e0b6478f69bef0c080d28bf
Sergey Koren <sergek@umd.edu>
2014-06-11 20:40:19 +0000

    Update to dump from partitions

--- src/AS_RUN/runCA.pl
+++ src/AS_RUN/runCA.pl
@@ -4850,10 +4850,10 @@ sub createPostUnitiggerConsensusJobs (@) {
         print F "&& \\\n";
         print F "touch $wrk/5-consensus/${asm}_\$jobid.success\n";
     } elsif ($consensusType eq "pbdagcon" || $consensusType eq "pbutgcns") {
-        system("$bin/gatekeeper -dumpfasta $wrk/5-consensus/$asm.shared $wrk/$asm.gkpStore");
-        system("rm -f $wrk/5-consensus/$asm.shared.q*");
+        print F "cat $wrk/4-unitigger/$asm.partitioning |awk -v JOB=\$jobid '{if (\$1 == JOB) print \$NF}' > $wrk/5-consensus/$asm.\$jobid.iid\n";
+        print F "$bin/gatekeeper -dumpfasta $wrk/5-consensus/$asm.\$jobid -iid $wrk/5-consensus/$asm.\$jobid.iid $wrk/$asm.gkpStore\n";
+        print F "rm -f $wrk/5-consensus/$asm.\$jobid.q*\n";
         print F "\$bin/tigStore -d layout -U -t $wrk/$asm.tigStore 1 -up \$jobid -g $wrk/$asm.gkpStore > $wrk/5-consensus/$asm.\$jobid.lay\n";
-        print F "ln -s $wrk/5-consensus/$asm.shared.fasta $wrk/5-consensus/$asm.\$jobid.fasta\n";
         print F "\$bin/convertToPBCNS -path $blasr -consensus $consensusType -coverage 1 -threads " . getGlobal("cnsConcurrency") . " -prefix $wrk/5-consensus/$asm.\$jobid.tmp -length 500 -sequence $wrk/5-consensus/$asm.\$jobid.fasta -input $wrk/5-consensus/$asm.\$jobid.lay -output $wrk/5-consensus/$asm.\$jobid.fa\n";
         print F "\$bin/addCNSToStore -path \$bin -input $wrk/5-consensus/$asm.\$jobid.fa -lay $wrk/5-consensus/$asm.\$jobid.lay -output $wrk/5-consensus/$asm.\$jobid.cns -prefix $wrk/$asm -sequence $wrk/5-consensus/$asm.\$jobid.fasta -partition \$jobid && \$bin/utgcnsfix -g $wrk/$asm.gkpStore  -t $wrk/$asm.tigStore 2 \$jobid -o $wrk/5-consensus/${asm}_\$jobid.fixes > $wrk/5-consensus/${asm}_\$jobid.fix.err 2>&1 && touch $wrk/5-consensus/${asm}_\$jobid.success\n";
         print F "if [ -e $wrk/5-consensus/${asm}_\$jobid.success ]; then\n";
