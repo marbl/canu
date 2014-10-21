0470adeed26840ceb39d5efe2a9954a9f5f725a3
Brian P. Walenz <thebri@gmail.com>
2014-10-21 14:39:03 +0000

    Change loadOverlaps() to return zero if no overlaps are loaded for THIS IID, instead of the number of overlaps loaded for whatever IID was loaded.

--- src/AS_OBT/AS_OBT_overlaps.C
+++ src/AS_OBT/AS_OBT_overlaps.C
@@ -23,6 +23,9 @@ static const char *rcsid = "$Id:  $";
 
 #include "AS_OBT_overlaps.H"
 
+//  Loads overlaps for whatever read is next in the store.
+//  Returns 0 if the overlaps are NOT for the iid supplied, otherwise, returns the number of overlaps.
+
 uint32
 loadOverlaps(uint32         iid,
              OVSoverlap   *&ovl,
@@ -39,9 +42,13 @@ loadOverlaps(uint32         iid,
     ovl    = new OVSoverlap [ovlMax];
   }
 
-  //  Return if the overlaps are for the current read, or some future read.
 
-  if (iid <= ovl[0].a_iid)
+  if (iid < ovl[0].a_iid)
+    //  Overlaps loaded are for a future read.
+    return(0);
+
+  if (iid == ovl[0].a_iid)
+    //  Overlaps loaded are for this read, YAY!
     return(ovlLen);
 
   //  Until we load the correct overlap, repeat.
@@ -53,8 +60,10 @@ loadOverlaps(uint32         iid,
     ovlLen += AS_OVS_readOverlapsFromStore(ovlSecondary, NULL, 0, AS_OVS_TYPE_ANY);
 
     //  Quit now if there are no overlaps.  This simplifies the rest of the loop.
-    if (ovlLen == 0)
+    if (ovlLen == 0) {
+      //fprintf(stderr, "no more overlaps.\n");
       return(0);
+    }
 
     //  Allocate space for these overlaps.
     while (ovlMax < ovlLen) {
@@ -71,24 +80,45 @@ loadOverlaps(uint32         iid,
     //fprintf(stderr, "LOADED %d overlaps for a_iid %d\n", ovlLen, ovl[0].a_iid);
 
     //  If we read overlaps for a fragment after 'iid', we're done.  The client will properly save
-    //  these overlaps until the iid becomes active.  And just in case it doesn't, we return above
-    //  if the iid passed in is less than the current overlap.
+    //  these overlaps until the iid becomes active.
     //
-    if (iid <= ovl[0].a_iid)
-      return(ovlLen);
+    if (iid < ovl[0].a_iid) {
+      //fprintf(stderr, "looking for iid %u, read iid %u, stop, return no overlaps.\n", iid, ovl[0].a_iid);
+      return(0);
+    }
 
-    //  On the otherhand, if we read overlaps for a fragment before 'iid', we can either keep reading
-    //  until we find the overlaps for this fragment, or jump to the correct spot to read overlaps.
+    //  If we've found the overlaps, we're still done.
+    //
+    if (iid == ovl[0].a_iid) {
+      //fprintf(stderr, "looking for iid %u, read iid %u, stop, return %u overlaps.\n", iid, ovl[0].a_iid, ovlLen);
+      return(ovlLen);
+    }
+
+    //  On the otherhand, if we read overlaps for a fragment before 'iid', we can either keep
+    //  reading until we find the overlaps for this fragment, or jump to the correct spot to read
+    //  overlaps.
     //
     //  The rule is simple.  If we're within 50 of the correct IID, keep streaming.  Otherwise, make
-    //  a jump.  AS_OVS_setRangeOverlapStore() seems to ALWAYS close and open a file, which is somewhat
-    //  expensive, especially if the file doesn't actually change.
+    //  a jump.  AS_OVS_setRangeOverlapStore() seems to ALWAYS close and open a file, which is
+    //  somewhat expensive, especially if the file doesn't actually change.
     //
     if (50 < iid - ovl[0].a_iid) {
+      //fprintf(stderr, "looking for iid %u, read iid %u, skip ahead to iid.\n", iid, ovl[0].a_iid);
       AS_OVS_setRangeOverlapStore(ovlPrimary,   iid, UINT32_MAX);
       AS_OVS_setRangeOverlapStore(ovlSecondary, iid, UINT32_MAX);
+    //} else {
+    //  fprintf(stderr, "looking for iid %u, read iid %u, skip ahead by 1.\n", iid, ovl[0].a_iid);
     }
+
   } while (ovl[0].a_iid < iid);
+
+  //  Code can't get here.
+  //    If we ran out of overlaps, the first return is used.
+  //    If we read past the iid we're looking for, the second return is used.
+  //    If we found the overlaps we're looking for, the thrd return is used.
+  //    If we didn't find the overlaps, we loop.
+
+  assert(0);
 
   return(ovlLen);
 }
