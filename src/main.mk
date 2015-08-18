53de6aeb0f2f27a9ebd138439f8903e623014533
Brian P. Walenz <thebri@gmail.com>
2015-08-18 19:40:31 +0000

    Tidy up main.mk, remove several unused main.mk from subdirectories.

--- src/main.mk
+++ src/main.mk
@@ -8,147 +8,139 @@ ifeq "$(strip ${TARGET_DIR})" ""
   TARGET_DIR   := ../$(OSTYPE)-$(MACHINETYPE)/bin
 endif
 
-TARGET   := libCA.a
+TARGET       := libCA.a
 
-#  unitigger uses AS_UTL_alloc, AS_UTL_Var and AS_UTL_Hash
-#  AS_UTL_Hash needs AS_UTL_heap
+SOURCES      := AS_global.C \
+                \
+                AS_UTL/AS_UTL_decodeRange.C \
+                AS_UTL/AS_UTL_fasta.C \
+                AS_UTL/AS_UTL_fileIO.C \
+                AS_UTL/AS_UTL_reverseComplement.C \
+                AS_UTL/AS_UTL_stackTrace.C \
+                \
+                AS_UTL/AS_UTL_Var.C \
+                AS_UTL/AS_UTL_Hash.C \
+                AS_UTL/AS_UTL_heap.C \
+                AS_UTL/AS_UTL_alloc.C \
+                \
+                AS_UTL/bitEncodings.C \
+                AS_UTL/bitPackedFile.C \
+                AS_UTL/bitPackedArray.C \
+                AS_UTL/dnaAlphabets.C \
+                AS_UTL/md5.C \
+                AS_UTL/mt19937ar.C \
+                AS_UTL/readBuffer.C \
+                AS_UTL/speedCounter.C \
+                AS_UTL/stddev.C \
+                AS_UTL/sweatShop.C \
+                AS_UTL/timeAndSize.C \
+                AS_UTL/kMer.C \
+                \
+                stores/gkStore.C \
+                \
+                stores/ovOverlap.C \
+                stores/ovStore.C \
+                stores/ovStoreFile.C \
+                \
+                stores/tgStore.C \
+                stores/tgTig.C \
+                stores/tgTigSizeAnalysis.C \
+                stores/tgTigMultiAlignDisplay.C \
+                \
+                meryl/libmeryl.C \
+                \
+                overlapInCore/overlapReadCache.C \
+                \
+                overlapErrorAdjustment/analyzeAlignment.C \
+                \
+                overlapInCore/liboverlap/Binomial_Bound.C \
+                overlapInCore/liboverlap/Display_Alignment.C \
+                overlapInCore/liboverlap/prefixEditDistance.C \
+                overlapInCore/liboverlap/prefixEditDistance-allocateMoreSpace.C \
+                overlapInCore/liboverlap/prefixEditDistance-extend.C \
+                overlapInCore/liboverlap/prefixEditDistance-forward.C \
+                overlapInCore/liboverlap/prefixEditDistance-reverse.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-5000.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4900.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4800.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4700.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4600.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4500.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4400.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4300.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4200.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4100.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-4000.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3900.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3800.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3700.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3600.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3500.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3400.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3300.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3200.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3100.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-3000.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2900.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2800.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2700.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2600.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2500.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2400.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2300.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2200.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2100.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-2000.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1900.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1800.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1700.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1600.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1500.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1400.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1300.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1200.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1100.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-1000.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0900.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0800.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0700.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0600.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0500.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0400.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0300.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0200.C \
+                overlapInCore/liboverlap/prefixEditDistance-matchLimit-0100.C \
+                \
+                utgcns/libNDalign/NDalign.C \
+                \
+                utgcns/libNDalign/Binomial_Bound.C \
+                utgcns/libNDalign/NDalgorithm.C \
+                utgcns/libNDalign/NDalgorithm-allocateMoreSpace.C \
+                utgcns/libNDalign/NDalgorithm-extend.C \
+                utgcns/libNDalign/NDalgorithm-forward.C \
+                utgcns/libNDalign/NDalgorithm-reverse.C \
+                \
+                utgcns/libcns/abAbacus-addRead.C \
+                utgcns/libcns/abAbacus-applyAlignment.C \
+                utgcns/libcns/abAbacus-baseCall.C \
+                utgcns/libcns/abAbacus-mergeRefine.C \
+                utgcns/libcns/abAbacus-refine.C \
+                utgcns/libcns/abAbacus-refreshMultiAlign.C \
+                utgcns/libcns/abAbacus.C \
+                utgcns/libcns/abColumn.C \
+                utgcns/libcns/abMultiAlign.C \
+                utgcns/libcns/unitigConsensus.C
 
-SOURCES  := AS_global.C \
-            \
-            AS_UTL/AS_UTL_decodeRange.C \
-            AS_UTL/AS_UTL_fasta.C \
-            AS_UTL/AS_UTL_fileIO.C \
-            AS_UTL/AS_UTL_reverseComplement.C \
-            AS_UTL/AS_UTL_stackTrace.C \
-            \
-            AS_UTL/AS_UTL_Var.C \
-            AS_UTL/AS_UTL_Hash.C \
-            AS_UTL/AS_UTL_heap.C \
-            AS_UTL/AS_UTL_alloc.C \
-            \
-            AS_UTL/bitEncodings.C \
-            AS_UTL/bitPackedFile.C \
-            AS_UTL/bitPackedArray.C \
-            AS_UTL/dnaAlphabets.C \
-            AS_UTL/md5.C \
-            AS_UTL/mt19937ar.C \
-            AS_UTL/readBuffer.C \
-            AS_UTL/speedCounter.C \
-            AS_UTL/stddev.C \
-            AS_UTL/sweatShop.C \
-            AS_UTL/timeAndSize.C \
-            AS_UTL/kMer.C \
-            \
-            stores/gkStore.C \
-            \
-            stores/ovOverlap.C \
-            stores/ovStore.C \
-            stores/ovStoreFile.C \
-            \
-            stores/tgStore.C \
-            stores/tgTig.C \
-            stores/tgTigSizeAnalysis.C \
-            stores/tgTigMultiAlignDisplay.C \
-            \
-            meryl/libmeryl.C \
-            \
-            overlapInCore/overlapReadCache.C \
-            \
-            overlapErrorAdjustment/analyzeAlignment.C \
-            \
-            overlapInCore/liboverlap/Binomial_Bound.C \
-            overlapInCore/liboverlap/Display_Alignment.C \
-            overlapInCore/liboverlap/prefixEditDistance.C \
-            overlapInCore/liboverlap/prefixEditDistance-allocateMoreSpace.C \
-            overlapInCore/liboverlap/prefixEditDistance-extend.C \
-            overlapInCore/liboverlap/prefixEditDistance-forward.C \
-            overlapInCore/liboverlap/prefixEditDistance-reverse.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-5000.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4900.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4800.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4700.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4600.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4500.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4400.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4300.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4200.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4100.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-4000.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3900.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3800.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3700.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3600.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3500.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3400.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3300.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3200.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3100.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-3000.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2900.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2800.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2700.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2600.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2500.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2400.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2300.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2200.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2100.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-2000.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1900.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1800.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1700.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1600.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1500.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1400.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1300.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1200.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1100.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-1000.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0900.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0800.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0700.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0600.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0500.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0400.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0300.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0200.C \
-            overlapInCore/liboverlap/prefixEditDistance-matchLimit-0100.C \
-            \
-            utgcns/libNDalign/NDalign.C \
-            \
-            utgcns/libNDalign/Binomial_Bound.C \
-            utgcns/libNDalign/NDalgorithm.C \
-            utgcns/libNDalign/NDalgorithm-allocateMoreSpace.C \
-            utgcns/libNDalign/NDalgorithm-extend.C \
-            utgcns/libNDalign/NDalgorithm-forward.C \
-            utgcns/libNDalign/NDalgorithm-reverse.C \
-            \
-            utgcns/libcns/abAbacus-addRead.C \
-            utgcns/libcns/abAbacus-applyAlignment.C \
-            utgcns/libcns/abAbacus-baseCall.C \
-            utgcns/libcns/abAbacus-mergeRefine.C \
-            utgcns/libcns/abAbacus-refine.C \
-            utgcns/libcns/abAbacus-refreshMultiAlign.C \
-            utgcns/libcns/abAbacus.C \
-            utgcns/libcns/abColumn.C \
-            utgcns/libcns/abMultiAlign.C \
-            utgcns/libcns/unitigConsensus.C
+SRC_INCDIRS  := . \
+                AS_UTL \
+                stores \
+                alignment \
+                utgcns/libNDalign \
+                utgcns/libcns \
+                meryl/libleaff \
+                overlapInCore \
+                overlapInCore/liboverlap
 
-
-#  Not complete, just snippets of code
-#            AS_UTL/decodeBooleanString.C
-#            AS_UTL/AS_UTL_stackTraceTest.C
-#
-#  Test code
-#            AS_UTL/testHashTable.C
-#            AS_UTL/testRand.C
-#            AS_UTL/testVar.C
-#            AS_UTL/memoryMappedFileTest.C
-
-SRC_INCDIRS  := . AS_UTL stores alignment utgcns/libNDalign utgcns/libcns   meryl/libleaff overlapInCore overlapInCore/liboverlap
-
-#  Cannot be the 'main.mk' from subdirectories, as that redefines libCA.a to
-#  update just those objects.  Instead, this must list each binary indifidually.
 SUBMAKEFILES := stores/gatekeeperCreate.mk \
                 stores/gatekeeperDumpFASTQ.mk \
                 stores/gatekeeperDumpMetaData.mk \
--- src/overlapInCore/main.mk
+++
@@ -1,1 +0,0 @@
-overlapInCore.mk
