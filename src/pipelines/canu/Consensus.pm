035851951825ccdd9e3da6f3af6b6c73ac579128
Sergey Koren <sergek@umd.edu>
2015-12-17 18:41:14 -0500

    Revert quick mode on second try

--- src/pipelines/canu/Consensus.pm
+++ src/pipelines/canu/Consensus.pm
@@ -247,13 +247,9 @@ sub consensusCheck ($$) {
     #  If not the first attempt, report the jobs that failed, and that we're recomputing.
 
     if ($attempt > 1) {
-        setGlobal("cnsConsensus", "quick");
-        utgcns($wrk, $asm, $jobs);
         print STDERR "--\n";
         print STDERR "-- ", scalar(@failedJobs), " consensus jobs failed:\n";
         print STDERR $failureMessage;
-        print STDERR "--\n";
-        print STDERR "-- Retrying with " . getGlobal("cnsConsensus") . "\n";
         print STDERR "--\n";
     }
 
