53b70fad4c35bc048bcd5783d60ad8e69db3ca29
Sergey Koren <sergek@umd.edu>
2015-12-29 09:02:24 -0500

    Update boost path

--- src/utgcns/libboost/boost/copy.sh
+++
@@ -1,5 +0,0 @@
-for file in `cat list |awk '{print $1}'`; do
-   path=`echo $file |sed s/utgcns//g |sed s/libboost//g |sed s/boost//g`
-   echo $path
-   cp --parents /usr/local/BOOST/1_56_0/include/boost/$path ./
-done
