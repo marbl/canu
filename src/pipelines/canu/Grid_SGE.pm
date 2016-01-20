7e2816e8be030e682b533871be4f8ae9acce7641
Brian P. Walenz <thebri@gmail.com>
2016-01-20 10:08:34 -0500

    Improve the error messages when SGE threads/memory can't be autoconfigured.

--- src/pipelines/canu/Grid_SGE.pm
+++ src/pipelines/canu/Grid_SGE.pm
@@ -69,10 +69,11 @@ sub configureSGE () {
     #  Try to figure out the name of the threaded job execution environment.
     #  It's the one with allocation_rule of $pe_slots.
 
+    my $configError = 0;
+
     if (!defined(getGlobal("gridEngineThreadsOption"))) {
         my @env = `qconf -spl`;  chomp @env;
-        my $thr = undef;
-        my $nth = 0;
+        my @thr;
 
         foreach my $env (@env) {
             my $ar = 0;
@@ -87,34 +88,48 @@ sub configureSGE () {
             }
             close(F);
 
-            if (($ar == 1) && ($cs == 1) && ($jf == 1)) {
-                $thr = $env;
-                $nth++;
-            }
+            #if (($ar == 1) && ($cs == 1) && ($jf == 1)) {
+                push @thr, $env;
+            #}
         }
 
-        if ($nth == 1) {
-            print STDERR "-- Detected Grid Engine environment '$thr'.\n";
+        if (scalar(@thr) == 1) {
+            print STDERR "-- Detected Grid Engine environment '$thr[0]'.\n";
+            setGlobal("gridEngineThreadsOption", "-pe $thr[0] THREADS");
+        }
 
-            setGlobal("gridEngineThreadsOption", "-pe $thr THREADS");
-
-        } else {
+        elsif (scalar(@thr) > 1) {
+            print STDERR "--\n";
             print STDERR "-- WARNING:  Couldn't determine the SGE parallel environment to run multi-threaded codes.\n";
-            print STDERR "--          Set 'gridEngineThreadsOption' manually; example: '-pe threaded THREADS'.\n";
+            print STDERR "--           Valid choices are (pick one and supply it to canu):\n";
+            foreach my $thr (@thr) {
+                print STDERR "--             gridEngineThreadsOption=\"-pe $thr THREADS\"\n";
+            }
+            print STDERR "--\n";
+            $configError++;
         }
+
+        else {
+            print STDERR "--\n";
+            print STDERR "-- WARNING:  Couldn't determine the SGE parallel environment to run multi-threaded codes.\n";
+            print STDERR "--           No valid choices found!  Find an appropriate Parallel Environment name (qconf -spl) and set:\n";
+            print STDERR "--             gridEngineThreadsOption=\"-pe <name> THREADS\"\n";
+            print STDERR "--\n";
+
+            $configError++;
+        }
+
+    } elsif (getGlobal("gridEngineThreadsOption") =~ m/-pe\s+(.*)\s+THREADS$/) {
+        print STDERR "-- User supplied Grid Engine environment '", getGlobal("gridEngineThreadsOption"), "'.\n";
+
     } else {
-        if (getGlobal("gridEngineThreadsOption") =~ m/^-pe\s+(.*)$/) {
-            print STDERR "-- User supplied Grid Engine environment '$1'.\n";
-        } else {
-            caFailure("Couldn't parse gridEngineThreadsOption='" . getGlobal("gridEngineThreadsOption") . "'", undef);
-        }
+        caFailure("Couldn't parse gridEngineThreadsOption='" . getGlobal("gridEngineThreadsOption") . "'", undef);
     }
 
     #  Try to figure out the name of the memory resource.
 
     if (!defined(getGlobal("gridEngineMemoryOption"))) {
-        my $mem = undef;
-        my $nmm = 0;
+        my @mem;
 
         open(F, "qconf -sc |");
         while (<F>) {
@@ -125,33 +140,44 @@ sub configureSGE () {
             next  if ($vals[0] =~ m/swap/);      #  Don't care about swap.
             next  if ($vals[0] =~ m/virtual/);   #  Don't care about vm space.
 
-            $mem .= " " if (defined($mem));
-            $mem .= $vals[0];
-            $nmm++;
+            push @mem, $vals[0];
         }
         close(F);
 
-        if      ($nmm == 1) {
-            print STDERR "-- Detected Grid Engine consumable '$mem'.\n";
-
-            setGlobal("gridEngineMemoryOption", "-l $mem=MEMORY");
-
-        } elsif ($nmm > 1) {
-            print STDERR "-- WARNING:  Couldn't determine the SGE resource to request memory.\n";
-            print STDERR "--          Found $nmm choices: $mem\n";
-            print STDERR "--          Set 'gridEngineMemoryOption' manually; example: '-l mem=MEMORY'.\n";
-
-        } else {
-            print STDERR "-- WARNING:  Couldn't determine the SGE resource to request memory.\n";
-            print STDERR "--          Set 'gridEngineMemoryOption' manually; example: '-l mem=MEMORY'.\n";
+        if      (scalar(@mem) == 1) {
+            print STDERR "-- Detected Grid Engine consumable '$mem[0]'.\n";
+            setGlobal("gridEngineMemoryOption", "-l $mem[0]=MEMORY");
         }
+
+        elsif (scalar(@mem) > 1) {
+            print STDERR "--\n";
+            print STDERR "-- WARNING:  Couldn't determine the SGE resource to request memory.\n";
+            print STDERR "--           Valid choices are (pick one and supply it to canu):\n";
+            foreach my $mem (@mem) {
+                print STDERR "--             gridEngineMemoryOption=\"-l $mem=MEMORY\"\n";
+            }
+            print STDERR "--\n";
+            $configError++;
+        }
+
+        else {
+            print STDERR "--\n";
+            print STDERR "-- WARNING:  Couldn't determine the SGE resource to request memory.\n";
+            print STDERR "--           No valid choices found!  Find an appropriate complex name (qconf -sc) and set:\n";
+            print STDERR "--             gridEngineMemoryOption=\"-l <name>=MEMORY\"\n";
+            print STDERR "--\n";
+            $configError++;
+        }
+    } elsif (getGlobal("gridEngineMemoryOption") =~ m/^-l\s+.*=MEMORY$/) {
+        print STDERR "-- User supplied Grid Engine consumable '", getGlobal("gridEngineMemoryOption"), "'.\n";
+
     } else {
-        if (getGlobal("gridEngineMemoryOption") =~ m/^-l\s+(.*)=MEMORY$/) {
-            print STDERR "-- User supplied Grid Engine consumable '$1'.\n";
-        } else {
-            caFailure("Couldn't parse gridEngineMemoryOption='" . getGlobal("gridEngineMemoryOption") . "'", undef);
-        }
+        caFailure("Couldn't parse gridEngineMemoryOption='" . getGlobal("gridEngineMemoryOption") . "'", undef);
     }
+
+
+    caExit("can't configure for SGE", undef)  if ($configError);
+
 
     #  Build a list of the resources available in the grid.  This will contain a list with keys
     #  of "#CPUs-#GBs" and values of the number of nodes With such a config.  Later on, we'll use this
@@ -193,5 +219,6 @@ sub configureSGE () {
         $hosts{"$cpus-$mem"}++    if ($cpus gt 0);
     }
     close(F);
+
     setGlobal("availableHosts", formatAllowedResources(%hosts, "Sun Grid Engine"));
 }
