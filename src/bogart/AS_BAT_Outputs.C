04cf4418d545dc8624de7a17bcace1125e9b904a
Brian P. Walenz <thebri@gmail.com>
2016-01-27 15:52:42 -0500

    Change labels on bogart iid -> contig id map; what's an IUM?

--- src/bogart/AS_BAT_Outputs.C
+++ src/bogart/AS_BAT_Outputs.C
@@ -244,7 +244,7 @@ writeUnitigsToStore(UnitigVector  &unitigs,
     assert(tigid < unitigs.size());
     partmap[tigid] = prt_count;
 
-    fprintf(iidm, "Unitig "F_U32" == IUM "F_U32" (in partition "F_U32" with "F_U32" frags)\n",
+    fprintf(iidm, "bogart "F_U32" -> tig "F_U32" (in partition "F_U32" with "F_U32" frags)\n",
             utg->id(),
             (tigid),
             partmap[(tigid)],
