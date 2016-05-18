72d516ddc7a9233de0375d9ca9eef920abc8db2f
Brian P. Walenz <thebri@gmail.com>
2016-05-18 07:00:55 -0400

    Notes and comments.

--- src/overlapErrorAdjustment/findErrors-Analyze_Alignment.C
+++ src/overlapErrorAdjustment/findErrors-Analyze_Alignment.C
@@ -89,7 +89,6 @@ Matching_Vote(char ch) {
 //   a_len  and  b_len  are the lengths of the prefixes of  a_part  and
 //   b_part , resp., that align.
 
-
 void
 Analyze_Alignment(Thread_Work_Area_t *wa,
                   char   *a_part, int32 a_len, int32 a_offset,
@@ -215,7 +214,19 @@ Analyze_Alignment(Thread_Work_Area_t *wa,
 
 
   //  For each identified change, add votes for some region around the change.
-
+  //
+  //  This is adding extra votes if the distance between two errors is larger than a kmer.
+  //  Not sure why there are no 'matching bases' in this region.
+  //
+  //  X == changes, mismatch or indel
+  //
+  //                          ------- <- confirmed count added
+  //                          -----   <- no_insert count added
+  //  matching-bases} X 1 2 3 1 2 3 4 3 2 1 X {matching-bases
+  //                    -----         -----
+  //                    match         match
+  //                    votes         votes
+  //
 
   for (int32 i=1; i<=ct; i++) {
     int32  prev_match = wa->globalvote[i].align_sub - wa->globalvote[i - 1].align_sub - 1;
