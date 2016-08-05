e2028ce20341ab39894406d304b78961619274ea
Brian P. Walenz <thebri@gmail.com>
2016-08-05 13:45:57 -0400

    Given read ID and store, report what tigs the overlapping reads are in.

---
+++ src/bogart/findOverlappingReads.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/env perl
+
+use strict;
+
+if (scalar(@ARGV) == 0) {
+    die "usage: $0 assembly-prefix readID [tigStore-bogart-stage]\n";
+}
+
+my $prefix = shift @ARGV;
+my $readID = shift @ARGV;
+my $store  = shift @ARGV;
+
+my $gkpStore = "$prefix.gkpStore";
+my $ovlStore = "$prefix.ovlStore";
+my $tigStore = "$prefix.tigStore";
+my $tigVers  = 1;
+
+$tigStore = "$prefix.003.buildUnitigs.tigStore"    if ($store eq "003");
+$tigStore = "$prefix.004.placeContains.tigStore"   if ($store eq "004");
+$tigStore = "$prefix.005.mergeOrphans.tigStore"    if ($store eq "005");
+$tigStore = "$prefix.007.breakRepeats.tigStore"    if ($store eq "007");
+$tigStore = "$prefix.009.generateOutputs.tigStore" if ($store eq "009");
+
+$gkpStore = "../$gkpStore"  if (! -d $gkpStore);
+$gkpStore = "../$gkpStore"  if (! -d $gkpStore);
+$gkpStore = "../$gkpStore"  if (! -d $gkpStore);
+
+$ovlStore = "../$ovlStore"  if (! -d $ovlStore);
+$ovlStore = "../$ovlStore"  if (! -d $ovlStore);
+$ovlStore = "../$ovlStore"  if (! -d $ovlStore);
+
+$tigStore = "../$tigStore"  if (! -d $tigStore);
+$tigStore = "../$tigStore"  if (! -d $tigStore);
+$tigStore = "../$tigStore"  if (! -d $tigStore);
+
+die "failed to find gkpStore $prefix.gkpStore"  if (! -d $gkpStore);
+die "failed to find ovlStore $prefix.ovlStore"  if (! -d $ovlStore);
+die "failed to find tigStore $prefix.tigStore"  if (! -d $tigStore);
+
+
+my %readOvl;
+
+my $nOvl = 0;
+my $nTig = 0;
+
+open(F, "ovStoreDump -G $gkpStore -O $ovlStore -p $readID |");
+while (<F>) {
+    chomp;
+
+    #  For -d dumps
+    if (m/^\s*\d+\s+(\d+)\s+/) {
+        $nOvl++;
+        $readOvl{$1} = $_;
+    }
+
+    #  For -p dumps
+    if (m/^\s*(\d+)\s+A:\s+\d+\s+/) {
+        $nOvl++;
+        $readOvl{$1} = $_;
+    }
+}
+close(F);
+
+
+system("ovStoreDump -G $gkpStore -O $ovlStore -p $readID");
+print "\n";
+
+
+my $tig;
+my $len;
+my $num;
+
+open(F, "tgStoreDump -G $gkpStore -T $tigStore $tigVers -layout |");
+while (<F>) {
+    chomp;
+
+    $tig = $1  if (m/^tig\s+(\d+)$/);
+    $len = $1  if (m/^len\s+(\d+)$/);
+    $num = $1  if (m/^numChildren\s+(\d+)$/);
+
+    if (m/^read\s+(\d+)\s+/) {
+        my $r = $1;
+
+        if      ($r == $readID) {
+            print "tig $tig len $len -- $_\n";
+        } elsif (exists($readOvl{$1})) {
+            $nTig++;
+            printf "tig %6d len %8d -- %s -- %s\n", $tig, $len, $_, $readOvl{$1};
+        }
+    }
+}
+close(F);
+
+print "\n";
+print STDERR "Found $nOvl overlaps in $ovlStore (with $gkpStore).\n";
+print STDERR "Found $nTig placements in $tigStore.\n";
