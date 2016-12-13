e1bd0d72152140d2805a2b8fe0ea2307a6cf4b76
Brian P. Walenz <thebri@gmail.com>
2016-12-13 16:22:05 -0500

    Script to build binaries for release.

---
+++ buildRelease.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+version=$1
+
+if [ x$version = x ] ; then
+  echo usage: $0 numeric-version
+  exit
+fi
+
+git clone git@github.com:marbl/canu.git
+
+mv canu canu-$version
+
+cd canu-$version
+git tag v$version
+cd src
+gmake -j 12 > ../Darwin-amd64.out 2>&1
+cd ../..
+
+rm -f canu-$version/linux.sh
+
+echo >> canu-$version/linux.sh  \#\!/bin/bash
+echo >> canu-$version/linux.sh  yum install -y git
+echo >> canu-$version/linux.sh  cd /build/canu-$version/src
+echo >> canu-$version/linux.sh  gmake -j 12 \> ../Linux-amd64.out 2\>\&1
+echo >> canu-$version/linux.sh  cd ../..
+echo >> canu-$version/linux.sh  tar -cf canu-$version.Darwin-amd64.tar canu-$version/Darwin-amd64
+echo >> canu-$version/linux.sh  tar -cf canu-$version.Linux-amd64.tar  canu-$version/Linux-amd64
+
+chmod 755 canu-$version/linux.sh
+
+docker run -v `pwd`:/build -t -i --rm phusion/holy-build-box-64:latest /hbb_exe/activate-exec bash /build/canu-$version/linux.sh
+
+xz -9v canu-$version.Darwin-amd64.tar
+xz -9v canu-$version.Linux-amd64.tar
+
+exit
