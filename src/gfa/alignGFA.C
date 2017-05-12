ca4e38557578a4d7841271f503a3b60ffa477167
Brian P. Walenz <thebri@gmail.com>
2017-05-12 11:52:52 -0400

    Big change to process bed, and to convert bed to gfa.

--- src/gfa/alignGFA.C
+++ src/gfa/alignGFA.C
@@ -33,6 +33,11 @@
 #include "AS_UTL_reverseComplement.H"
 
 #include "gfa.H"
+#include "bed.H"
+
+#define IS_GFA   1
+#define IS_BED   2
+
 
 
 class sequence {
@@ -56,6 +61,55 @@ public:
 
   char   *seq;
   uint32  len;
+};
+
+
+
+class sequences {
+public:
+  sequences(char *tigName, uint32 tigVers) {
+    tgStore *tigStore = new tgStore(tigName, tigVers);
+
+    b    = 0;
+    e    = tigStore->numTigs();
+    seqs = new sequence [e+1];
+    used = new uint32   [e+1];
+
+    for (uint32 ti=b; ti < e; ti++) {
+      tgTig *tig = tigStore->loadTig(ti);
+
+      used[ti] = 0;
+
+      if (tig == NULL)
+        continue;
+
+      seqs[ti].set(tig);
+
+      tigStore->unloadTig(ti);
+    }
+
+    delete tigStore;
+  };
+
+  ~sequences() {
+    delete [] seqs;
+    delete [] used;
+  };
+
+  sequence &operator[](uint32 xx) {
+    if (xx < e)
+      return(seqs[xx]);
+
+    fprintf(stderr, "ERROR: sequence id %u out of range b=%u e=%u\n", xx, b, e);
+
+    assert(xx < e);
+    return(seqs[0]);
+  };
+
+  uint32    b;
+  uint32    e;
+  sequence *seqs;
+  uint32   *used;
 };
 
 
@@ -99,12 +153,11 @@ dotplot(uint32 Aid, bool Afwd, char *Aseq,
 
 
 
-
 bool
-checkLink(gfaLink  *link,
-          sequence *seqs,
-          bool      beVerbose,
-          bool      doPlot) {
+checkLink(gfaLink   *link,
+          sequences &seqs,
+          bool       beVerbose,
+          bool       doPlot) {
 
   char   *Aseq = seqs[link->_Aid].seq;
   char   *Bseq = seqs[link->_Bid].seq;
@@ -268,123 +321,162 @@ checkLink(gfaLink  *link,
 
 
 
+bool
+checkRecord(bedRecord   *record,
+            sequences   &ctgs,
+            sequences   &utgs,
+            bool         beVerbose,
+            bool         UNUSED(doPlot)) {
 
-int
-main (int argc, char **argv) {
-  char    *tigName         = NULL;
-  uint32   tigVers         = UINT32_MAX;
+  char   *Aseq = ctgs[record->_Aid].seq;
+  char   *Bseq = utgs[record->_Bid].seq;
 
-  char    *inGFA           = NULL;
-  char    *otGFA           = NULL;
+  int32  Abgn, Aend, Alen = ctgs[record->_Aid].len;
+  int32  Bbgn, Bend, Blen = utgs[record->_Bid].len;
 
-  uint32    verbosity      = 0;
+  EdlibAlignResult  result  = { 0, NULL, NULL, 0, NULL, 0, 0 };
 
-  argc = AS_configure(argc, argv);
+  int32  editDist    = 0;
+  int32  alignLen    = 0;
+  int32  alignScore  = 0;
+  int32  maxEdit     = (int32)ceil(Blen * 0.03);  //  Should be the same sequence, but allow for a little difference.
+  int32  step        = (int32)ceil(Blen * 0.15);
+  bool   success     = false;
 
-  int arg=1;
-  int err=0;
-  while (arg < argc) {
-    if        (strcmp(argv[arg], "-T") == 0) {
-      tigName = argv[++arg];
-      tigVers = atoi(argv[++arg]);
+  //  Regardless of if we find an new alignment or not, remove the old one.
+  //  If we don't find a new one, we'll discard the link.
 
-      if (tigVers == 0)
-        fprintf(stderr, "invalid tigStore version (-T store version partition) '-t %s %s %s'.\n", argv[arg-2], argv[arg-1], argv[arg]), exit(1);
+  record->_score = 0;
 
-    } else if (strcmp(argv[arg], "-i") == 0) {
-      inGFA = argv[++arg];
+  if (record->_Bfwd == false)
+    reverseComplementSequence(Bseq, Blen);
 
-    } else if (strcmp(argv[arg], "-o") == 0) {
-      otGFA = argv[++arg];
+  //  Align B to A, allowing the A positions to float.
 
-    } else if (strcmp(argv[arg], "-V") == 0) {
-      verbosity++;
+  Abgn = record->_bgn;
+  Aend = record->_end;
 
-    } else if (strcmp(argv[arg], "-t") == 0) {
-      omp_set_num_threads(atoi(argv[++arg]));
+  Abgn = max(Abgn - 2 * step, 0);
+  Aend = min(Aend + 2 * step, Alen);
 
-    } else {
-      fprintf(stderr, "%s: Unknown option '%s'\n", argv[0], argv[arg]);
-      err++;
+  Bbgn = 0;
+  Bend = Blen;
+
+ tryAgain:
+  if (beVerbose)
+    fprintf(stderr, "ALIGN utg %s len=%7d to ctg %s %9d-%9d len=%9d",
+            record->_Bname, Blen,
+            record->_Aname, Abgn, Aend, Alen);
+
+  result = edlibAlign(Bseq + Bbgn, Bend-Bbgn,  //  The 'query'
+                      Aseq + Abgn, Aend-Abgn,  //  The 'target'
+                      edlibNewAlignConfig(maxEdit, EDLIB_MODE_HW, EDLIB_TASK_LOC));
+
+  //  Got an alignment?  Process and report, and maybe try again.
+
+  if (result.numLocations > 0) {
+    int32  nAbgn = Abgn + result.startLocations[0];
+    int32  nAend = Abgn + result.endLocations[0] + 1;  // 0-based to space-based
+
+    editDist   = result.editDistance;
+    alignLen   = result.alignmentLength;     //  Edlib 'alignmentLength' is populated only for TASK_PATH
+    alignLen   = ((nAend - nAbgn) + (Bend - Bbgn) + (editDist)) / 2;
+    alignScore = 100 - (int32)(1000.0 * editDist / alignLen);
+
+    edlibFreeAlignResult(result);
+
+    if (beVerbose)
+      fprintf(stderr, " - POSITION from %9d-%-9d to %9d-%-9d score %5d/%9d %4d\n",
+              record->_bgn, record->_end, nAbgn, nAend, editDist, alignLen, alignScore);
+
+    //  If it's a full alignment -- if the A region was big enough to have unaligned bases -- then
+    //  we're done.  Update the record and get out of here.
+
+    if (((Abgn  < nAbgn) || (Abgn == 0)) &&
+        ((nAend < Aend)  || (Aend == Alen))) {
+      success = true;
+
+      record->_bgn   = nAbgn;
+      record->_end   = nAend;
+      record->_score = alignScore;
+
+      goto finishCheck;
     }
 
-    arg++;
+    //  Otherwise, we ran out of A sequence to align to before we ran out of stuff to align.  Extend
+    //  the A region and try again.
+
+    if (Abgn == nAbgn)
+      Abgn = max(Abgn - step, 0);
+
+    if (Aend == nAend)
+      Aend = min(Aend + step, Alen);
+
+    goto tryAgain;
   }
 
-  if (tigName == NULL)
-    err++;
-  if (inGFA == NULL)
-    err++;
-  if (otGFA == NULL)
-    err++;
+  //  Didn't get a good alignment.
 
-  if (err) {
-    fprintf(stderr, "usage: %s [opts]\n", argv[0]);
-    fprintf(stderr, "  Validates a GFA by generating alignments.\n");
-    fprintf(stderr, "  Optionally writes new GFA with updated CIGAR string (NOT IMPLEMENTED).\n");
-    fprintf(stderr, "\n");
-    fprintf(stderr, "    -G g           Load reads from gkStore 'g'\n");
-    fprintf(stderr, "    -T t v         Load tigs from tgStore 't', version 'v'.\n");
-    fprintf(stderr, "                     Consensus sequence must exist (usually in v=2)\n");
-    fprintf(stderr, "    -i input.gfa\n");
-    fprintf(stderr, "    -o output.gfa\n");
-    fprintf(stderr, "\n");
-    fprintf(stderr, "\n");
-    fprintf(stderr, "    -V             Increase chatter\n");
-    fprintf(stderr, "\n");
-    fprintf(stderr, "    -t threads     Use 'threads' computational threads.\n");
-    fprintf(stderr, "\n");
+  //  We fail for one of two reasons - either not enough bases in the reference, or too high of
+  //  error.  Unitigs are supposed to be from the same sequence, but they might be lower coverage
+  //  and therefore higher error.  It's more likely they are misplaced.
 
-    if (tigName == NULL)
-      fprintf(stderr, "ERROR: no tigStore (-T) supplied.\n");
-    if (inGFA == NULL)
-      fprintf(stderr, "ERROR: no input GFA (-i) supplied.\n");
-    if (otGFA == NULL)
-      fprintf(stderr, "ERROR: no output GFA (-o) supplied.\n");
 
-    exit(1);
+  if ((Aend - Abgn < 4 * Blen) &&
+      (maxEdit < Blen * 0.25)) {
+    if (beVerbose)
+      fprintf(stderr, " - FAILED, RELAX\n");
+
+    Abgn = max(Abgn - step, 0);
+    Aend = min(Aend + step, Alen);
+
+    maxEdit *= 1.2;
+
+    goto tryAgain;
   }
 
-  fprintf(stderr, "-- Opening tigStore '%s' version %u.\n", tigName, tigVers);
-  tgStore *tigStore = new tgStore(tigName, tigVers);
+  if (beVerbose)
+    fprintf(stderr, " - ABORT, ABORT, ABORT!\n");
+
+  //  Cleanup for the next record
+
+ finishCheck:
+  if (record->_Bfwd == false)
+    reverseComplementSequence(Bseq, Blen);
+
+  return(success);
+}
+
+
+
+//
+//  Try to find an alignment for each link in the GFA file.  If found, output a new link
+//  with correct CIGAR string.  If not found, discard the link.
+//
+void
+processGFA(char     *tigName,
+           uint32    tigVers,
+           char     *inGFA,
+           char     *otGFA,
+           uint32    verbosity) {
 
   //  Load the GFA file.
 
   fprintf(stderr, "-- Reading GFA '%s'.\n", inGFA);
-  gfaFile  *gfa = new gfaFile(inGFA);
 
-  //  Load all consensus sequences
+  gfaFile   *gfa  = new gfaFile(inGFA);
 
-  uint32  b = 0;
-  uint32  e = tigStore->numTigs();
+  fprintf(stderr, "-- Loading sequences from tigStore '%s' version %u.\n", tigName, tigVers);
 
-  sequence  *seqs = new sequence [e+1];
-
-  fprintf(stderr, "-- Loading tigs %u to %u.\n", b, e);
-
-  for (uint32 ti=b; ti < e; ti++) {
-    tgTig *tig = tigStore->loadTig(ti);
-
-    if (tig == NULL)
-      continue;
-
-    seqs[ti].set(tig);
-
-    tigStore->unloadTig(ti);
-  }
+  sequences *seqsp = new sequences(tigName, tigVers);
+  sequences &seqs  = *seqsp;
 
   //  Set GFA lengths based on the sequences we loaded.
 
-  fprintf(stderr, "-- Resetting sequence lengths.\n", inGFA);
+  fprintf(stderr, "-- Resetting sequence lengths.\n");
 
   for (uint32 ii=0; ii<gfa->_sequences.size(); ii++)
     gfa->_sequences[ii]->_length = seqs[gfa->_sequences[ii]->_id].len;
-
-  //  Done with the stores.
-
-  fprintf(stderr, "-- Closing tigStore '%s'.\n", tigName);
-
-  delete tigStore;
 
   //  Align!
 
@@ -453,12 +545,297 @@ main (int argc, char **argv) {
 
   fprintf(stderr, "-- Cleaning up.\n");
 
-  delete [] seqs;
-  delete    gfa;
+  delete seqsp;
+  delete gfa;
 
   fprintf(stderr, "-- Aligned %6u ciruclar tigs, failed %6u\n", passCircular, failCircular);
   fprintf(stderr, "-- Aligned %6u   linear tigs, failed %6u\n", passNormal,   failNormal);
-  fprintf(stderr, "-- Bye.\n");
+}
+
+
+
+//
+//  Find an alignment between the unitig (the feature) and the contig (the 'chromosome').
+//  Output updated coordiates.
+//
+void
+processBED(char   *tigName,
+           uint32  tigVers,
+           char   *seqName,
+           uint32  seqVers,
+           char   *inBED,
+           char   *otBED,
+           uint32  verbosity) {
+
+  //  Load the BED file.
+
+  fprintf(stderr, "-- Reading BED '%s'.\n", inBED);
+
+  bedFile   *bed  = new bedFile(inBED);
+
+  fprintf(stderr, "-- Loading sequences from tigStore '%s' version %u.\n", tigName, tigVers);
+
+  sequences *utgsp = new sequences(tigName, tigVers);
+  sequences &utgs  = *utgsp;
+
+  fprintf(stderr, "-- Loading sequences from tigStore '%s' version %u.\n", seqName, seqVers);
+
+  sequences *ctgsp = new sequences(seqName, seqVers);
+  sequences &ctgs  = *ctgsp;
+
+  //  Align!
+
+  uint32  pass = 0;
+  uint32  fail = 0;
+
+  uint32  iiLimit      = bed->_records.size();
+  uint32  iiNumThreads = omp_get_max_threads();
+  uint32  iiBlockSize  = (iiLimit < 1000 * iiNumThreads) ? iiNumThreads : iiLimit / 999;
+
+  fprintf(stderr, "-- Aligning " F_U32 " records using " F_U32 " threads.\n", iiLimit, iiNumThreads);
+
+#pragma omp parallel for schedule(dynamic, iiBlockSize)
+  for (uint32 ii=0; ii<iiLimit; ii++) {
+    bedRecord *record = bed->_records[ii];
+
+    if (checkRecord(record, ctgs, utgs, (verbosity > 0), false)) {
+      pass++;
+    } else {
+      delete bed->_records[ii];
+      bed->_records[ii] = NULL;
+      fail++;
+    }
+  }
+
+  fprintf(stderr, "-- Writing BED '%s'.\n", otBED);
+
+  bed->saveFile(otBED);
+
+  fprintf(stderr, "-- Cleaning up.\n");
+
+  delete utgsp;
+  delete ctgsp;
+  delete bed;
+
+  fprintf(stderr, "-- Aligned %6u unitigs to contigs, failed %6u\n", pass, fail);
+}
+
+
+
+//
+//  Infer a graph from the positions of unitigs (features) in contigs (chromosomes).  Generate a GFA
+//  input and toss that up to processGFA.
+//
+void
+processBED(char   *tigName,
+           uint32  tigVers,
+           char   *inBED,
+           char   *otGFA,
+           uint32  verbosity) {
+
+  int32  minOlap = 100;
+
+  //  We only really need the sequence lengths here, but eventually, we'll want to generate
+  //  alignments for all the overlaps, and so we'll need the sequences too.
+
+  fprintf(stderr, "-- Loading sequences from tigStore '%s' version %u.\n", tigName, tigVers);
+
+  sequences *seqsp = new sequences(tigName, tigVers);
+  sequences &seqs  = *seqsp;
+
+  //  Load the BED file.
+
+  fprintf(stderr, "-- Reading BED '%s'.\n", inBED);
+
+  bedFile   *bed  = new bedFile(inBED);
+  gfaFile   *gfa  = new gfaFile("H\tVN:Z:bogart/edges");
+
+  //  Iterate over sequences, looking for overlaps in contigs.  Stupid, O(n^2) but seems fast enough.
+
+  char   cigar[81];
+  uint32 maxID = 0;
+
+  for (uint64 ii=0; ii<bed->_records.size(); ii++) {
+    for (uint64 jj=ii+1; jj<bed->_records.size(); jj++) {
+
+      if (bed->_records[ii]->_Aid != bed->_records[jj]->_Aid)                 //  Different contigs?
+        continue;                                                             //  No overlap.
+
+      if ((bed->_records[ii]->_end < bed->_records[jj]->_bgn + minOlap) ||    //  No (thick) intersection?
+          (bed->_records[jj]->_end < bed->_records[ii]->_bgn + minOlap))      //
+        continue;                                                             //  No overlap.
+
+      //  Overlap!
+
+      //fprintf(stderr, "OVERLAP %s %d-%d - %s %d-%d\n",
+      //        bed->_records[ii]->_Bname, bed->_records[ii]->_bgn, bed->_records[ii]->_end,
+      //        bed->_records[jj]->_Bname, bed->_records[jj]->_bgn, bed->_records[jj]->_end);
+
+      int32  olapLen = 0;
+
+      if (bed->_records[ii]->_bgn < bed->_records[jj]->_end)
+        olapLen = bed->_records[ii]->_end - bed->_records[jj]->_bgn;
+
+      if (bed->_records[jj]->_bgn < bed->_records[ii]->_end)
+        olapLen = bed->_records[jj]->_end - bed->_records[ii]->_bgn;
+
+      assert(olapLen > 0);
+
+      sprintf(cigar, "%dM", olapLen);
+
+      gfaLink *link = new gfaLink(bed->_records[ii]->_Bname, bed->_records[ii]->_Bid, true,
+                                  bed->_records[jj]->_Bname, bed->_records[jj]->_Bid, true,
+                                  cigar);
+
+      bool  pN = checkLink(link, seqs, (verbosity > 0), false);
+
+      if (pN)
+        gfa->_links.push_back(link);
+      else
+        gfa->_links.push_back(link);
+
+      //  Remember sequences we've hit.
+
+      seqs.used[bed->_records[ii]->_Bid]++;
+      seqs.used[bed->_records[jj]->_Bid]++;
+    }
+  }
+
+  //  Add sequences.  We could have done this as we're running through making edges, but we then
+  //  need to figure out if we've seen a sequence already.
+
+  char   seqName[80];
+
+  for (uint32 ii=0; ii<seqs.e; ii++)
+    if (seqs.used[ii] > 0) {
+      sprintf(seqName, "utg%08u", ii);
+      gfa->_sequences.push_back(new gfaSequence(seqName, ii, seqs[ii].len));
+    }
+
+  //  Write the file, cleanup, done!
+
+  gfa->saveFile(otGFA);
+
+  delete gfa;
+  delete bed;
+}
+
+
+
+int
+main (int argc, char **argv) {
+  char     *tigName         = NULL;         //  For GFA and BED, the source of the tigs
+  uint32    tigVers         = UINT32_MAX;
+
+  char     *seqName         = NULL;         //  For BED, the source of the 'chromosomes'
+  uint32    seqVers         = UINT32_MAX;   //  The -C option (either chromosome or container)
+
+  char     *inGraph         = NULL;
+  char     *otGraph         = NULL;
+
+  uint32    graphType       = IS_GFA;
+
+  uint32    verbosity      = 0;
+
+  argc = AS_configure(argc, argv);
+
+  int arg=1;
+  int err=0;
+  while (arg < argc) {
+    if        (strcmp(argv[arg], "-T") == 0) {
+      tigName = argv[++arg];
+      tigVers = atoi(argv[++arg]);
+
+    } else if (strcmp(argv[arg], "-C") == 0) {
+      seqName = argv[++arg];
+      seqVers = atoi(argv[++arg]);
+
+    } else if (strcmp(argv[arg], "-gfa") == 0) {
+      graphType = IS_GFA;
+    } else if (strcmp(argv[arg], "-bed") == 0) {
+      graphType = IS_BED;
+
+    } else if (strcmp(argv[arg], "-i") == 0) {
+      inGraph = argv[++arg];
+    } else if (strcmp(argv[arg], "-o") == 0) {
+      otGraph = argv[++arg];
+
+    } else if (strcmp(argv[arg], "-V") == 0) {
+      verbosity++;
+
+    } else if (strcmp(argv[arg], "-t") == 0) {
+      omp_set_num_threads(atoi(argv[++arg]));
+
+    } else {
+      fprintf(stderr, "%s: Unknown option '%s'\n", argv[0], argv[arg]);
+      err++;
+    }
+
+    arg++;
+  }
+
+  if (tigName == NULL)
+    err++;
+  if (inGraph == NULL)
+    err++;
+  if (otGraph == NULL)
+    err++;
+
+  if ((tigName) && (tigVers == 0))
+    err++;
+  if ((seqName) && (seqVers == 0))
+    err++;
+
+  if (err) {
+    fprintf(stderr, "usage: %s [opts]\n", argv[0]);
+    fprintf(stderr, "  Validates a GFA by generating alignments.\n");
+    fprintf(stderr, "  Optionally writes new GFA with updated CIGAR string (NOT IMPLEMENTED).\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "    -G g           Load reads from gkStore 'g'.\n");
+    fprintf(stderr, "    -T t v         Load tigs from tgStore 't', version 'v'.\n");
+    fprintf(stderr, "    -C t v         For BED format, the source of the 'chromosomes'.  Similar to -T.\n");
+    fprintf(stderr, "                     Consensus sequence must exist for -T and -C (usually in v=2)\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "    -i input       Input graph.\n");
+    fprintf(stderr, "    -o output      Output graph.\n");
+    fprintf(stderr, "                     Graph are either GFA (v1) or BED format.\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "    -gfa           The input and output graphs are in GFA (v1) format.\n");
+    fprintf(stderr, "    -bed           The input graph is in BED format.  If -C is supplied, the\n");
+    fprintf(stderr, "                   output will also be BED, and will have updated positions.\n");
+    fprintf(stderr, "                   If -C is not supplied, the output will be GFA (v1) of the\n");
+    fprintf(stderr, "                   overlaps inferred from the BED positions.\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "    -V             Increase chatter.\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "    -t threads     Use 'threads' computational threads.\n");
+    fprintf(stderr, "\n");
+
+    if (tigName == NULL)
+      fprintf(stderr, "ERROR: no tigStore (-T) supplied.\n");
+    if (inGraph == NULL)
+      fprintf(stderr, "ERROR: no input GFA (-i) supplied.\n");
+    if (otGraph == NULL)
+      fprintf(stderr, "ERROR: no output GFA (-o) supplied.\n");
+
+    if ((tigName) && (tigVers == 0))
+      fprintf(stderr, "ERROR: invalid tigStore version (-T) supplied.\n");
+    if ((seqName) && (seqVers == 0))
+      fprintf(stderr, "ERROR: invalid tigStore version (-C) supplied.\n");
+
+    exit(1);
+  }
+
+  if (graphType == IS_GFA)
+    processGFA(tigName, tigVers, inGraph, otGraph, verbosity);
+
+  if ((graphType == IS_BED) && (seqName != NULL))
+    processBED(tigName, tigVers, seqName, seqVers, inGraph, otGraph, verbosity);
+
+  if ((graphType == IS_BED) && (seqName == NULL))
+    processBED(tigName, tigVers, inGraph, otGraph, verbosity);
+
+  fprintf(stderr, "Bye.\n");
 
   exit(0);
 }
