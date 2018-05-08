03b512fa158b7617217c7cf67fb09c88d86fb532
Brian P. Walenz <thebri@gmail.com>
2018-05-08 14:34:08 -0400

    Fix edlib issue Martinsos/edlib#85, Martinsos/edlib@c12b5e1159ccd7e42cbfd3fb21b43c354c2797f8.

--- src/overlapInCore/libedlib/edlib.C
+++ src/overlapInCore/libedlib/edlib.C
@@ -613,13 +613,24 @@ static int myersCalcEditDistanceSemiGlobal(const Word* const Peq, const int W, c
             }
         }
 
-        // Every some columns, do some expensive but also more efficient block reducing -> this is important!
+        // Every some columns, do some expensive but also more efficient block reducing.
+        // This is important!
+        //
+        // Reduce the band by decreasing last block if possible.
         if (c % STRONG_REDUCE_NUM == 0) {
-            while (lastBlock >= firstBlock && allBlockCellsLarger(*bl, k)) {
+            while (lastBlock >= 0 && lastBlock >= firstBlock && allBlockCellsLarger(*bl, k)) {
                 lastBlock--; bl--; Peq_c--;
             }
         }
+        // For HW, even if all cells are > k, there still may be solution in next
+        // column because starting conditions at upper boundary are 0.
+        // That means that first block is always candidate for solution,
+        // and we can never end calculation before last column.
+        if (mode == EDLIB_MODE_HW && lastBlock == -1) {
+            lastBlock++; bl++; Peq_c++;
+        }
 
+        // Reduce band by increasing first block if possible. Not applicable to HW.
         if (mode != EDLIB_MODE_HW) {
             while (firstBlock <= lastBlock && blocks[firstBlock].score >= k + WORD_SIZE) {
                 firstBlock++;
@@ -629,14 +640,6 @@ static int myersCalcEditDistanceSemiGlobal(const Word* const Peq, const int W, c
                     firstBlock++;
                 }
             }
-        }
-
-        // For HW, even if all cells are > k, there still may be solution in next
-        // column because starting conditions at upper boundary are 0.
-        // That means that first block is always candidate for solution,
-        // and we can never end calculation before last column.
-        if (mode == EDLIB_MODE_HW) {
-            lastBlock = max(0, lastBlock);
         }
 
         // If band stops to exist finish
