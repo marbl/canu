bb7e178611b5d52ec8ff922c53854a125129c17a
Brian P. Walenz <thebri@gmail.com>
2018-05-15 20:17:01 -0400

    Actually process inclusively, as the usage text says we do.

--- src/correction/falconsense.C
+++ src/correction/falconsense.C
@@ -499,7 +499,7 @@ main(int argc, char **argv) {
   //
 
   else {
-    for (uint32 ii=idMin; ii<idMax; ii++) {
+    for (uint32 ii=idMin; ii<=idMax; ii++) {
       if ((readList.size() > 0) &&      //  Skip reads not on the read list,
           (readList.count(ii) == 0))    //  if there actually is a read list.
         continue;
