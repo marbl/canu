be146ba2b268e04b02e25d0351d755de9d44e3b7
Brian P. Walenz <thebri@gmail.com>
2018-10-22 12:06:38 -0400

    Update for the last copyright update.

--- addCopyrights-BuildData.pl
+++ addCopyrights-BuildData.pl
@@ -36,6 +36,7 @@ $stoppingCommits{"bbbdcd063560e5f86006ee6b8b96d2d7b80bb750"} = 1;   #  21 NOV 20
 $stoppingCommits{"64459fe33f97f6d23fe036ba1395743d0cdd03e4"} = 1;   #  17 APR 2017
 $stoppingCommits{"9e9bd674b705f89817b07ff30067210c2d180f42"} = 1;   #  14 AUG 2017
 $stoppingCommits{"0fff8a511fd7d74081d94ff9e0f6c0351650ae2e"} = 1;   #  27 FEB 2018 - v1.7
+$stoppingCommits{"fcc3fe19eb635abd735486d215fbf65c56bcf4ee"} = 1;   #  22 OCT 2018 - v1.8
 
 open(F, "< logs") or die "Failed to open 'logs': $!\n";
 
