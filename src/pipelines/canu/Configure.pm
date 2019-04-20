cd20c5861a0f2ac78f7a6caee43149c6baf6a8cb
Sergey Koren <sergek@umd.edu>
2019-04-20 13:38:46 -0400

    Increase minimums for GFA memory (issue #1334)

--- src/pipelines/canu/Configure.pm
+++ src/pipelines/canu/Configure.pm
@@ -833,31 +833,31 @@ sub configureAssembler () {
         setGlobalIfUndef("batMemory", "4-16");        setGlobalIfUndef("batThreads", "2-4");
         setGlobalIfUndef("dbgMemory", "4-16");        setGlobalIfUndef("dbgThreads", "2-4");
 
-        setGlobalIfUndef("gfaMemory", "2-8");         setGlobalIfUndef("gfaThreads", "1-4");
+        setGlobalIfUndef("gfaMemory", "4-16");         setGlobalIfUndef("gfaThreads", "1-4");
 
     } elsif (getGlobal("genomeSize") < adjustGenomeSize("500m")) {
         setGlobalIfUndef("batMemory", "16-64");       setGlobalIfUndef("batThreads", "2-8");
         setGlobalIfUndef("dbgMemory", "8-64");       setGlobalIfUndef("dbgThreads", "2-8");
 
-        setGlobalIfUndef("gfaMemory", "4-8");         setGlobalIfUndef("gfaThreads", "2-8");
+        setGlobalIfUndef("gfaMemory", "8-16");         setGlobalIfUndef("gfaThreads", "2-8");
 
     } elsif (getGlobal("genomeSize") < adjustGenomeSize("2g")) {
         setGlobalIfUndef("batMemory", "32-256");      setGlobalIfUndef("batThreads", "4-16");
         setGlobalIfUndef("dbgMemory", "32-256");      setGlobalIfUndef("dbgThreads", "4-16");
 
-        setGlobalIfUndef("gfaMemory", "8-16");        setGlobalIfUndef("gfaThreads", "4-16");
+        setGlobalIfUndef("gfaMemory", "16-32");        setGlobalIfUndef("gfaThreads", "4-16");
 
     } elsif (getGlobal("genomeSize") < adjustGenomeSize("5g")) {
         setGlobalIfUndef("batMemory", "128-512");     setGlobalIfUndef("batThreads", "8-32");
         setGlobalIfUndef("dbgMemory", "128-512");     setGlobalIfUndef("dbgThreads", "8-32");
 
-        setGlobalIfUndef("gfaMemory", "16-32");       setGlobalIfUndef("gfaThreads", "8-32");
+        setGlobalIfUndef("gfaMemory", "32-64");       setGlobalIfUndef("gfaThreads", "8-32");
 
     } else {
         setGlobalIfUndef("batMemory", "256-1024");    setGlobalIfUndef("batThreads", "16-64");
         setGlobalIfUndef("dbgMemory", "128-1024");    setGlobalIfUndef("dbgThreads", "16-64");
 
-        setGlobalIfUndef("gfaMemory", "32-64");       setGlobalIfUndef("gfaThreads", "16-64");
+        setGlobalIfUndef("gfaMemory", "64-128");       setGlobalIfUndef("gfaThreads", "16-64");
     }
 
 
