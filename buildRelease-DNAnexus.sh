250f80a7bd40c49bdc550e7ea976ee9e7e265922
Brian P. Walenz <thebri@gmail.com>
2019-10-15 05:07:14 -0400

    Rename src/dx-canu/ to pipelines/src/dx-canu/.  Bulid the DNAnexus app outside the src tree.

--- buildRelease-DNAnexus.sh
+++ buildRelease-DNAnexus.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-mkdir -p src/dx-canu/resources/bin/
-
 #  Generate a script to compile Canu using the Holy Build Box.
 
 echo  > build-linux.sh  \#\!/bin/bash
@@ -14,44 +12,87 @@ echo >> build-linux.sh  cd ..
 
 chmod 755 build-linux.sh
 
-echo Build Linux and make tarballs.
+echo ""
+echo "-- Build Linux and make tarballs."
+echo ""
 
+echo "% docker run ..."
 docker run \
   -v `pwd`:/build \
   -t \
   -i \
-  --rm phusion/holy-build-box-64:latest /hbb_exe/activate-exec bash /build/build-linux.sh
+  --rm phusion/holy-build-box-64:latest /hbb_exe/activate-exec bash /build/build-linux.sh \
+> build-linux.sh.out 2>&1
 
 rm -f build-linux.sh
 
-tail -n 10 Linux-amd64.out
+echo ""
+echo "-- Build success?"
+echo ""
+
+tail -n 1 build-linux.sh.out | head -n 1
+tail -n 2 Linux-amd64.out | head -n 1
 
 #  Fetch the Upload Agent and install in our bin/.
 
-if [ ! -e src/dx-canu/resources/bin/ua ] ; then
-  curl -L -R -O https://wiki.dnanexus.com/images/files/dnanexus-upload-agent-1.5.31-linux.tar.gz
+if [ ! -e src/pipelines/dx-canu/resources/bin/ua ] ; then
+  echo ""
+  echo "-- Fetch UploadAgent."
+  echo ""
+
+  curl -L -R -O https://dnanexus-sdk.s3.amazonaws.com/dnanexus-upload-agent-1.5.31-osx.zip
+  curl -L -R -O https://dnanexus-sdk.s3.amazonaws.com/dnanexus-upload-agent-1.5.31-linux.tar.gz
 
   tar zxf dnanexus-upload-agent-1.5.31-linux.tar.gz
 
-  mv dnanexus-upload-agent-1.5.31-linux/ua src/dx-canu/resources/bin/
+  mv dnanexus-upload-agent-1.5.31-linux/ua src/pipelines/dx-canu/resources/bin/
 
-  rm -rf dnanexus-upload-agent-1.5.31-linux.tar.gz
+  #rm -rf dnanexus-upload-agent-1.5.31-linux.tar.gz
   rm -rf dnanexus-upload-agent-1.5.31-linux
 fi
 
-#  Package that up into the DNAnexus app.
+#   Remove the old app.
 
-rm -rf src/dx-canu/resources/usr/bin/
-rm -rf src/dx-canu/resources/usr/lib/
-rm -rf src/dx-canu/resources/usr/share/
+echo ""
+echo "-- Purge previous dx-canu build."
+echo ""
 
-mkdir -p src/dx-canu/resources/usr/bin/
-mkdir -p src/dx-canu/resources/usr/lib/
-mkdir -p src/dx-canu/resources/usr/share/
+echo "% rm -rf dx-canu/"
+rm -rf   dx-canu/
+mkdir -p dx-canu/
+mkdir -p dx-canu/resources/bin/
+mkdir -p dx-canu/resources/usr/bin/
+mkdir -p dx-canu/resources/usr/lib/
+mkdir -p dx-canu/resources/usr/share/
 
-rsync -a Linux-amd64/bin/   src/dx-canu/resources/usr/bin/
-rsync -a Linux-amd64/lib/   src/dx-canu/resources/usr/lib/
-rsync -a Linux-amd64/share/ src/dx-canu/resources/usr/share/
+#rm -rf dx-canu/resources/usr/bin/
+#rm -rf dx-canu/resources/usr/lib/
+#rm -rf dx-canu/resources/usr/share/
+
+#mkdir -p dx-canu/src
+
+#rm  -f dx-canu/dxapp.json
+#rm  -f dx-canu/
+
+#cp -p src/pipelines/dx-canu/dxapp.json                             dx-canu/dxapp.json
+#cp -p src/pipelines/dx-canu/src/canu-job-launcher.sh               dx-canu/src/canu-job-launcher.sh
+#cp -p src/pipelines/dx-canu/resources/bin/dx-get-instance-info.py  dx-canu/src/resources/bin/dx-get-instance-info.py
+
+#if [ ! -e dx-canu/resources/bin/ua ] ; then
+#  cp -p src/pipelines/dx-canu/resources/bin/ua dx-canu/resources/bin/ua
+#fi
+
+#  Package all that up into dx-canu.
+
+echo ""
+echo "-- Package new bits into dx-canu build."
+echo ""
+
+echo "% rsync ..."
+rsync -a src/pipelines/dx-canu/  dx-canu/
+rsync -a Linux-amd64/bin/        dx-canu/resources/usr/bin/
+rsync -a Linux-amd64/lib/        dx-canu/resources/usr/lib/
+rsync -a Linux-amd64/share/      dx-canu/resources/usr/share/
 
 #rm -fr Linux-amd64/obj Linux-amd64.tar Linux-amd64.tar.gz
 #tar -cf Linux-amd64.tar Linux-amd64
@@ -59,7 +100,11 @@ rsync -a Linux-amd64/share/ src/dx-canu/resources/usr/share/
 #dx rm Linux-amd64.tar.gz
 #dx upload Linux-amd64.tar.gz
 
-cd src
+echo ""
+echo "-- Build the DNAnexus app."
+echo ""
+
+echo "% dx build -f dx-canu"
 dx build -f dx-canu
 
 exit 0
