1a3aff38b1cad6ad407ee59ad0d8095083797cc7
Brian P. Walenz <thebri@gmail.com>
2019-11-04 01:18:33 -0500

    Update for the last copyright update.

--- addCopyrights-BuildData.pl
+++ addCopyrights-BuildData.pl
@@ -37,6 +37,7 @@ $stoppingCommits{"64459fe33f97f6d23fe036ba1395743d0cdd03e4"} = 1;   #  17 APR 20
 $stoppingCommits{"9e9bd674b705f89817b07ff30067210c2d180f42"} = 1;   #  14 AUG 2017
 $stoppingCommits{"0fff8a511fd7d74081d94ff9e0f6c0351650ae2e"} = 1;   #  27 FEB 2018 - v1.7
 $stoppingCommits{"fcc3fe19eb635abd735486d215fbf65c56bcf4ee"} = 1;   #  22 OCT 2018 - v1.8
+$stoppingCommits{"438412092d4470c63323f20780252b48ec132d44"} = 1;   #  04 NOV 2019 - v1.9
 
 open(F, "< logs") or die "Failed to open 'logs': $!\n";
 
@@ -68,6 +69,10 @@ while (!eof(F)) {
         $author = "Brian P. Walenz";
     } elsif (m/koren/i) {
         $author = "Sergey Koren";
+    } elsif (m/nurk/i) {
+        $author = "Sergey Nurk";
+    } elsif (m/rhie/i) {
+        $author = "Arang Rhie";
     } else {
         print STDERR "Skipping commit from '$_'\n";
         $author = undef;
