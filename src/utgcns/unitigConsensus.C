7e58304d4a846221b2bd345dfee977e26c7d16dc
Brian P. Walenz <thebri@gmail.com>
2020-01-29 15:04:56 -0500

    Move 'read alignment failure' log to 'algorithm debug' level output.

--- src/utgcns/unitigConsensus.C
+++ src/utgcns/unitigConsensus.C
@@ -862,7 +862,8 @@ unitigConsensus::generatePBDAG(tgTig                     *tig_,
     pass++;
   }
 
-  fprintf(stderr, "For tig %d finished aligning reads.  %d failed, %d passed.\n", _tig->tigID(), fail, pass);
+  if (showAlgorithm())
+    fprintf(stderr, "generatePBDAG()--    read alignment: %d failed, %d passed.\n", fail, pass);
 
   //  Construct the graph from the alignments.  This is not thread safe.
 
