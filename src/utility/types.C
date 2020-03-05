4dcc6e873b89eeddc3f8974f7547e99be93b68d0
Brian P. Walenz <thebri@gmail.com>
2020-03-04 23:57:15 -0500

    Templateify toHex() and toDec().

--- src/utility/types.C
+++ src/utility/types.C
@@ -39,15 +39,18 @@ char   hex[16]     = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b
 char   str[16][64] = { 0 };
 uint32 pos         =   0;
 
+
+
+template<typename uintType>
 char const *
-toHex(uint128 v) {
-  char *ret = str[pos++];
+toHex(uintType v) {
+  char   *ret = str[pos++];
+  uint32  w = sizeof(uintType) * 2;
+  uint32  p = w;
+  uint32  s = 0;
 
   if (pos >= 16)
     pos = 0;
-
-  uint32  p = 32;
-  uint32  s = 0;
 
   while (p > 0) {
     p -= 1;
@@ -55,124 +58,49 @@ toHex(uint128 v) {
     s += 4;
   }
 
-  ret[32] = 0;
+  ret[w] = 0;
 
   return(ret);
 }
 
+template char const *toHex<uint128>(uint128 v);
+template char const *toHex<uint64> (uint64  v);
+template char const *toHex<uint32> (uint32  v);
+template char const *toHex<uint16> (uint16  v);
+
+
+
+template<typename uintType>
 char const *
-toHex(uint64  v) {
-  char *ret = str[pos++];
+toDec(uintType v) {
+  char   *ret = str[pos++];
+  uint32  p   = 64;
+  uint32  x   = 0;
 
   if (pos >= 16)
     pos = 0;
 
-  uint32  p = 16;
-  uint32  s = 0;
-
-  while (p > 0) {
-    p -= 1;
-    ret[p] = hex[ (v >> s) & 0xf ];
-    s += 4;
+  if (v == 0) {
+    ret[0] = '0';
+    ret[1] =  0;
   }
 
-  ret[16] = 0;
+  else {
+    while (v > 0) {             //  Write the number, low-order
+      p -= 1;                   //  digits first, to the end
+      ret[p] = dec[ v % 10 ];   //  of the string.
+      v /= 10;
+    }
 
-  return(ret);
-}
-
-char const *
-toHex(uint32  v) {
-  char *ret = str[pos++];
-
-  if (pos >= 16)
-    pos = 0;
-
-  uint32  p = 8;
-  uint32  s = 0;
-
-  while (p > 0) {
-    p -= 1;
-    ret[p] = hex[ (v >> s) & 0xf ];
-    s += 4;
-  }
-
-  ret[8] = 0;
-
-  return(ret);
-}
-
-
-
-
-
-
-
-//  These are all the same.
-
-char const *
-toDec(uint128 v) {
-  char *ret = str[pos++];
-
-  if (pos >= 16)
-    pos = 0;
-
-  uint32  p = 64;
-
-  while (v > 0) {
-    p -= 1;
-    ret[p] = dec[ v % 10 ];
-    v /= 10;
-  }
-
-  for (uint32 xx=0; p<64; xx++, p++) {
-    ret[xx] = ret[p];
+    for (x=0; p<64; x++, p++)   //  Shift the string so it
+      ret[x] = ret[p];          //  starts at the origin.
+    ret[x] = 0;
   }
 
   return(ret);
 }
 
-char const *
-toDec(uint64  v) {
-  char *ret = str[pos++];
-
-  if (pos >= 16)
-    pos = 0;
-
-  uint32  p = 64;
-
-  while (v > 0) {
-    p -= 1;
-    ret[p] = dec[ v % 10 ];
-    v /= 10;
-  }
-
-  for (uint32 xx=0; p<64; xx++, p++) {
-    ret[xx] = ret[p];
-  }
-
-  return(ret);
-}
-
-char const *
-toDec(uint32  v) {
-  char *ret = str[pos++];
-
-  if (pos >= 16)
-    pos = 0;
-
-  uint32  p = 64;
-
-  while (v > 0) {
-    p -= 1;
-    ret[p] = dec[ v % 10 ];
-    v /= 10;
-  }
-
-  for (uint32 xx=0; p<64; xx++, p++) {
-    ret[xx] = ret[p];
-  }
-
-  return(ret);
-}
-
+template char const *toDec<uint128>(uint128 v);
+template char const *toDec<uint64> (uint64  v);
+template char const *toDec<uint32> (uint32  v);
+template char const *toDec<uint16> (uint16  v);
