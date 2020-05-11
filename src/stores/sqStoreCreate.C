c1e4ac2f1cd44aac373ac2d17f96de39f131e078
Sergey Koren <sergek@umd.edu>
2020-05-11 18:57:56 -0400

    Parse genome size as float to avoid overflowing with genomes >4gb

--- src/stores/sqStoreCreate.C
+++ src/stores/sqStoreCreate.C
@@ -545,7 +545,7 @@ main(int argc, char **argv) {
     }
 
     else if (strcmp(argv[arg], "-genomesize") == 0) {
-      genomeSize = atoi(argv[++arg]);
+      genomeSize = atof(argv[++arg]);
     }
 
     else if (strcmp(argv[arg], "-coverage") == 0) {
