f9237410590a804e2187518927e0b955ed052f31
Brian P. Walenz <thebri@gmail.com>
2020-05-12 06:22:48 -0400

    Don't seed a tig with a read that has no (or just incorrect) incoming best edges.

--- src/bogart/AS_BAT_PopulateUnitig.C
+++ src/bogart/AS_BAT_PopulateUnitig.C
@@ -126,24 +126,80 @@ populateUnitig(TigVector &tigs,
       (OG->isCoverageGap(fi) == true))    //  coverage gap, or lopsided.
     return;
 
-  Unitig *utg = tigs.newUnitig();
-
-  //  Add a first read -- to be 'compatable' with the old code, the first read is added
-  //  reversed, we walk off of its 5' end, flip it, and add the 3' walk.
-
-  utg->addRead(ufNode(fi, RI->readLength(fi), 0));
-
-  //  Add reads as long as there is a path to follow...from the 3' end of the first read.
+  //  Grab the best edges for the candidate seed read.
 
   BestEdgeOverlap  *bestedge5 = OG->getBestEdgeOverlap(fi, false);
   BestEdgeOverlap  *bestedge3 = OG->getBestEdgeOverlap(fi, true);
 
-  assert(bestedge5->ahang() <= 0);  //  Best Edges must be dovetail, which makes this test
-  assert(bestedge5->bhang() <= 0);  //  much simpler.
-  assert(bestedge3->ahang() >= 0);
-  assert(bestedge3->bhang() >= 0);
+  BestEdgeOverlap  *backedge5 = OG->getBestEdgeOverlap(bestedge5->readId(), bestedge5->read3p());
+  BestEdgeOverlap  *backedge3 = OG->getBestEdgeOverlap(bestedge3->readId(), bestedge3->read3p());
 
-  //  Stick on reads on the beginning of the tig.
+  //  If this read has non-mutual best edges don't seed a tig with it.  There
+  //  are (at least) four levels we could test:
+  //    1) One end has a non-mutual best edge.
+  //    2) Both ends have non-mutual best edge.
+  //    3) One end of the read has no incoming best edge.
+  //    4) Both ends have no incoming best edge.
+  //
+  //  On ecoli, the longest (and correct) path is seeded by a read that
+  //  matches cases (1) and (3).
+  //
+  //  We've found one instance, in a human, where (4) occurred and led to a
+  //  misassembly.
+
+  //  This is case (1) and (2).  Case (2) is implemented.  Requiring case (1)
+  //  didn't break ecoli, but it did change the layout slightly.  It 'feels
+  //  like' case (1) might be too strict, but BPW hasn't tested on anything.
+#if 1
+  bool edgeTo5 = ((backedge5->readId() == fi) && (backedge5->read3p() == false));
+  bool edgeTo3 = ((backedge3->readId() == fi) && (backedge3->read3p() ==  true));
+
+  if ((edgeTo5 == false) &&
+      (edgeTo3 == false)) {
+    if (logFileFlagSet(LOG_BUILD_UNITIG))
+      writeLog("tig ------ seed read %7u; non-mutual best edges, not using as a seed.  (edge to: 5' %s 3' %s)\n",
+               fi,
+               (edgeTo5) ? "yes" : "no",
+               (edgeTo3) ? "yes" : "no");
+    return;
+  }
+#endif
+
+  //  This is case (3) and (4).  Case (3) is implemented.  The only argment
+  //  against case (3) is a very very tiny DNA fragment with only two reads
+  //  covering it.  In that case we'd never seed a tig (because both reads
+  //  would have one end uncovered).
+#if 1
+  bool bestEdgeTo5 = false;
+  bool bestEdgeTo3 = false;
+
+  for (uint32 ii=1; ii<RI->numReads()+1; ii++) {
+    BestEdgeOverlap  *e5 = OG->getBestEdgeOverlap(ii, false);
+    BestEdgeOverlap  *e3 = OG->getBestEdgeOverlap(ii,  true);
+
+    if (e5->readId() == fi)   ((e5->read3p() == false) ? bestEdgeTo5 : bestEdgeTo3) = true;
+    if (e3->readId() == fi)   ((e3->read3p() == false) ? bestEdgeTo5 : bestEdgeTo3) = true;
+  }
+
+  if ((bestEdgeTo5 == false) ||
+      (bestEdgeTo3 == false)) {
+    if (logFileFlagSet(LOG_BUILD_UNITIG))
+      writeLog("tig ------ seed read %7u; no best edge to me, not using as a seed.  (edge to: 5' %s 3' %s)\n",
+               fi,
+               (bestEdgeTo5) ? "yes" : "no",
+               (bestEdgeTo3) ? "yes" : "no");
+    return;
+  }
+#endif
+
+  //  Add a first read -- to be 'compatable' with the old code, the first read is added
+  //  reversed, we walk off of its 5' end, flip it, and add the 3' walk.
+
+  Unitig *utg = tigs.newUnitig();
+
+  utg->addRead(ufNode(fi, RI->readLength(fi), 0));
+
+  //  Add reads as long as there is a path to follow...from the 3' end of the first read.
 
   if (bestedge3->readId()) {
     uint32  rid = utg->ufpath.back().ident;
