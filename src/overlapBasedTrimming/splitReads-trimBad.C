cb72f2a637e4f1b81b99b37cea17646466e2ef39
Brian P. Walenz <thebri@gmail.com>
2020-06-19 03:15:45 -0400

    Move logging to a function.

--- src/overlapBasedTrimming/splitReads-trimBad.C
+++ src/overlapBasedTrimming/splitReads-trimBad.C
@@ -25,6 +25,83 @@
 //  Process the list of bad intervals in blist to find a single clear range.
 //  Store the result in w->clrBgn and w->clrEnd.
 
+
+
+void
+logBadInterval(workUnit *w,
+               FILE     *subreadFile) {
+  vector<uint32>   loBad;
+  vector<uint32>   hiBad;
+
+  uint32   spur5 = UINT32_MAX;
+  uint32   spur3 = UINT32_MAX;
+
+  for (uint32 rr=0; rr<w->blist.size(); rr++) {
+    if (w->blist[rr].type == badType_5spur) {
+      assert(spur5 == UINT32_MAX);  //  Should be at most one 5' spur region
+      spur5 = rr;
+    }
+
+    if (w->blist[rr].type == badType_3spur) {
+      assert(spur3 == UINT32_MAX);  //  Should be at most one 3' spur region
+      spur3 = rr;
+    }
+
+    if (w->blist[rr].end == w->clrBgn)
+      loBad.push_back(rr);
+
+    if (w->clrEnd == w->blist[rr].bgn)
+      hiBad.push_back(rr);
+  }
+
+  //  Can't really say much about the number of regions we find.  There could be none (if the
+  //  biggest good region extends to the end of the read), exactly one (what we expect), or more
+  //  than one (if two algorithms agree on a bad region).
+
+  char  *logPtr = w->logMsg;
+
+  sprintf(logPtr, "iid %6u trim %7u %7u",
+          w->id, w->clrBgn, w->clrEnd);
+  while (*logPtr)
+    logPtr++;
+
+  if (w->isOK == false)
+    sprintf(logPtr, " TOO_SHORT");
+  while (*logPtr)
+    logPtr++;
+
+  if (spur5 != UINT32_MAX)
+    sprintf(logPtr, " (5'spur %7u %7u)", w->blist[spur5].bgn, w->blist[spur5].end);
+  while (*logPtr)
+    logPtr++;
+
+  if (spur3 != UINT32_MAX)
+    sprintf(logPtr, " (3'spur %7u %7u)", w->blist[spur3].bgn, w->blist[spur3].end);
+  while (*logPtr)
+    logPtr++;
+
+  for (uint32 xx=0; xx<loBad.size(); xx++) {
+    uint32 x = loBad[xx];
+
+    sprintf(logPtr, " (%s %7u %7u)", w->blist[x].typeName(), w->blist[x].bgn, w->blist[x].end);
+    while (*logPtr)
+      logPtr++;
+  }
+
+  for (uint32 xx=0; xx<hiBad.size(); xx++) {
+    uint32 x = hiBad[xx];
+
+    sprintf(logPtr, " (%s %7u %7u)", w->blist[x].typeName(), w->blist[x].bgn, w->blist[x].end);
+    while (*logPtr)
+      logPtr++;
+  }
+
+  logPtr[0] = '\n';
+  logPtr[1] = 0;
+}
+
+
+
 void
 trimBadInterval(sqStore        *UNUSED(seq),
                 workUnit              *w,
@@ -71,75 +148,7 @@ trimBadInterval(sqStore        *UNUSED(seq),
 
   //  For logging, find the two bordering bad regions
 
-  if (subreadFile) {
-    vector<uint32>   loBad;
-    vector<uint32>   hiBad;
-
-    uint32   spur5 = UINT32_MAX;
-    uint32   spur3 = UINT32_MAX;
-
-    for (uint32 rr=0; rr<w->blist.size(); rr++) {
-      if (w->blist[rr].type == badType_5spur) {
-        assert(spur5 == UINT32_MAX);  //  Should be at most one 5' spur region
-        spur5 = rr;
-      }
-
-      if (w->blist[rr].type == badType_3spur) {
-        assert(spur3 == UINT32_MAX);  //  Should be at most one 3' spur region
-        spur3 = rr;
-      }
-
-      if (w->blist[rr].end == w->clrBgn)
-        loBad.push_back(rr);
-
-      if (w->clrEnd == w->blist[rr].bgn)
-        hiBad.push_back(rr);
-    }
-
-    //  Can't really say much about the number of regions we find.  There could be none (if the
-    //  biggest good region extends to the end of the read), exactly one (what we expect), or more
-    //  than one (if two algorithms agree on a bad region).
-
-    char  *logPtr = w->logMsg;
-
-    sprintf(logPtr, "iid %6u trim %7u %7u",
-            w->id, w->clrBgn, w->clrEnd);
-    while (*logPtr)
-      logPtr++;
-
-    if (w->isOK == false)
-      sprintf(logPtr, " TOO_SHORT");
-    while (*logPtr)
-      logPtr++;
-
-    if (spur5 != UINT32_MAX)
-      sprintf(logPtr, " (5'spur %7u %7u)", w->blist[spur5].bgn, w->blist[spur5].end);
-    while (*logPtr)
-      logPtr++;
-
-    if (spur3 != UINT32_MAX)
-      sprintf(logPtr, " (3'spur %7u %7u)", w->blist[spur3].bgn, w->blist[spur3].end);
-    while (*logPtr)
-      logPtr++;
-
-    for (uint32 xx=0; xx<loBad.size(); xx++) {
-      uint32 x = loBad[xx];
-
-      sprintf(logPtr, " (%s %7u %7u)", w->blist[x].typeName(), w->blist[x].bgn, w->blist[x].end);
-      while (*logPtr)
-        logPtr++;
-    }
-
-    for (uint32 xx=0; xx<hiBad.size(); xx++) {
-      uint32 x = hiBad[xx];
-
-      sprintf(logPtr, " (%s %7u %7u)", w->blist[x].typeName(), w->blist[x].bgn, w->blist[x].end);
-      while (*logPtr)
-        logPtr++;
-    }
-
-    logPtr[0] = '\n';
-    logPtr[1] = 0;
-  }
+  if (subreadFile)
+    logBadInterval(w, subreadFile);
 }
 
