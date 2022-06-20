c33aab5f85536527d165abbb2e8968e28a60e959
Brian P. Walenz <thebri@gmail.com>
2022-06-20 16:29:52 -0400

    Forget loaded reads and the unitigConsensus object after we process an imported tig.

--- src/utgcns/utgcns.C
+++ src/utgcns/utgcns.C
@@ -161,62 +161,69 @@ processImportedTigs(cnsParameters  &params) {
   sqRead_defaultVersion = sqRead_raw | sqRead_normal;
 
   while (tig->importData(importFile, reads, importedLayouts, importedReads) == true) {
+    if ((params.tigBgn <= tig->tigID()) &&
+        (tig->tigID()  <= params.tigEnd)) {
+      fprintf(stdout, "%7u %9u %7u%s", tig->tigID(), tig->length(), tig->numberOfChildren(),
+              (tig->numberOfChildren() == 1) ? "\n" : "");
 
-    if (tig->tigID() < params.tigBgn || tig->tigID() > params.tigEnd) {
-       fprintf(stdout, "Skipping tig %d\n", tig->tigID());
-       continue;
+      //  Stash excess coverage.  Singletons report no logging.
+
+      tgTigStashed S;
+
+      tig->stashContains(params.maxCov, S);
+
+      if (S.nBack > 0)
+        fprintf(stdout, "  %8u %7.2fx %8u %7.2fx  %8u %7.2fx\n",
+                S.nCont, (double)S.bCont / tig->length(),
+                S.nStsh, (double)S.bStsh / tig->length(),
+                S.nBack, (double)S.bBack / tig->length());
+
+      //  Compute!
+
+      tig->_utgcns_verboseLevel = params.verbosity;
+
+      unitigConsensus  *utgcns  = new unitigConsensus(params.seqStore, params.errorRate, params.errorRateMax, params.errorRateMaxID, params.minOverlap, params.minCoverage);
+      bool              success = utgcns->generate(tig, params.algorithm, params.aligner, &reads);
+
+      //  Show the result, if requested.
+
+      if (params.showResult)
+        tig->display(stdout, params.seqStore, 200, 3);
+
+      //  Unstash.
+
+      tig->unstashContains();
+
+      //  Save the result.
+
+      if (params.outResultsFile)   tig->saveToStream(params.outResultsFile);
+      if (params.outLayoutsFile)   tig->dumpLayout(params.outLayoutsFile);
+      if (params.outSeqFileA)      tig->dumpFASTA(params.outSeqFileA);
+      if (params.outSeqFileQ)      tig->dumpFASTQ(params.outSeqFileQ);
+
+      delete utgcns;
+      utgcns = nullptr;
     }
-    //  Log that we're processing.
-
-    if (tig->numberOfChildren() > 1) {
-      fprintf(stdout, "%7u %9u %7u", tig->tigID(), tig->length(), tig->numberOfChildren());
-    } else {
-      fprintf(stdout, "%7u %9u %7u\n", tig->tigID(), tig->length(), tig->numberOfChildren());
+    else {
+      fprintf(stdout, "%7u %9u %7u  skipped.", tig->tigID(), tig->length(), tig->numberOfChildren());
     }
-
-    //  Stash excess coverage.  Singletons report no logging.
-
-    tgTigStashed S;
-
-    tig->stashContains(params.maxCov, S);
-
-    if (S.nBack > 0)
-      fprintf(stdout, "  %8u %7.2fx %8u %7.2fx  %8u %7.2fx\n",
-              S.nCont, (double)S.bCont / tig->length(),
-              S.nStsh, (double)S.bStsh / tig->length(),
-              S.nBack, (double)S.bBack / tig->length());
-
-    //  Compute!
-
-    tig->_utgcns_verboseLevel = params.verbosity;
-
-    unitigConsensus  *utgcns  = new unitigConsensus(params.seqStore, params.errorRate, params.errorRateMax, params.errorRateMaxID, params.minOverlap, params.minCoverage);
-    bool              success = utgcns->generate(tig, params.algorithm, params.aligner, &reads);
-
-    //  Show the result, if requested.
-
-    if (params.showResult)
-      tig->display(stdout, params.seqStore, 200, 3);
-
-    //  Unstash.
-
-    tig->unstashContains();
-
-    //  Save the result.
-
-    if (params.outResultsFile)   tig->saveToStream(params.outResultsFile);
-    if (params.outLayoutsFile)   tig->dumpLayout(params.outLayoutsFile);
-    if (params.outSeqFileA)      tig->dumpFASTA(params.outSeqFileA);
-    if (params.outSeqFileQ)      tig->dumpFASTQ(params.outSeqFileQ);
 
     //  Tidy up for the next tig.
+
+    for (auto it=reads.begin(); it != reads.end(); ++it) {
+      delete it->second;
+    }
+    reads.clear();
 
     delete tig;
     tig = new tgTig();    //  Next loop needs an existing empty layout.
   }
 
+  delete tig;
+
   AS_UTL_closeFile(importedReads);
   AS_UTL_closeFile(importedLayouts);
+
   delete importFile;
 }
 
