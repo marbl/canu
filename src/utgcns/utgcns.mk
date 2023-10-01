3295b9971eb001f93fb895c4316c377f2f1de5e0
Brian P. Walenz <thebri@gmail.com>
2023-10-01 18:42:38 -0400

    Use system boost if found, otherwise use the (ancient) version in canu.

--- src/utgcns/utgcns.mk
+++ src/utgcns/utgcns.mk
@@ -7,9 +7,38 @@ SOURCES  := utgcns.C \
             utgcns-processTigs.C \
             libpbutgcns/AlnGraphBoost.C
 
+
+ifeq (${OSTYPE}, Linux)
+  BOOST ?= libboost
+endif
+
+ifeq (${OSTYPE}, FreeBSD)
+  ifeq ($(wildcard /usr/local/include/boost), /usr/local/include/boost)
+    BOOST ?= /usr/local/include
+  else
+    BOOST ?= libboost
+  endif
+endif
+
+ifeq (${OSTYPE}, Darwin)
+  ifeq      ($(wildcard /opt/homebrew/include/boost), /opt/homebrew/include/boost)
+    BOOST ?= /opt/homebrew/include
+  else ifeq ($(wildcard /usr/local/include/boost),    /usr/local/include/boost)
+    BOOST ?= /usr/local/include
+  else ifeq ($(wildcard ${BREW}/opt/boost/include),   ${BREW}/opt/boost/include)
+    BOOST ?= ${BREW}/opt/boost/include
+  else ifeq ($(wildcard ${PORT}/include/boost),       ${PORT}/include/boost)
+    #  UNTESTED!
+    BOOST ?= ${PORT}/include/boost
+  else
+    BOOST ?= libboost
+  endif
+endif
+
+
 SRC_INCDIRS := ../utility/src ../stores libpbutgcns
 
-EXT_INCDIRS := libboost
+EXT_INCDIRS := ${BOOST}
 
 TGT_LDFLAGS := -L${TARGET_DIR}/lib
 TGT_LDLIBS  := -l${MODULE}
